06fea7c9c78064fede0fcb56d0434a6c1d12d0bb
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-08 20:32:06 -0300

    :rotating_light: Desafio 3
    LINTEEEE

--- middlewares/posts.js
+++ middlewares/posts.js
@@ -10,9 +10,9 @@ module.exports = {
       const token = tokenGenerator();
       res.status(200).json({ token });
     }
-    if (reqEmail === undefined) return next({ message: 'O campo email é obrigatório', statusCode: 400 });
-    if (!reqEmail) return next({ message: 'O email deve ter o formato email@email.com', statusCode: 400 });
-    if (reqPass === undefined) return next({ message: 'O campo password é obrigatório', statusCode: 400 });
-    if (!reqPass) return next({ message: 'O password ter pelo menos 6 caracteres', statusCode: 400 });
+    if (reqEmail === undefined) return next({ message: 'O campo "email" é obrigatório', statusCode: 400 });
+    if (!reqEmail) return next({ message: 'O "email" deve ter o formato "email@email.com"', statusCode: 400 });
+    if (reqPass === undefined) return next({ message: 'O campo "password" é obrigatório', statusCode: 400 });
+    if (!reqPass) return next({ message: 'O "password" ter pelo menos 6 caracteres', statusCode: 400 });
   },
 };
