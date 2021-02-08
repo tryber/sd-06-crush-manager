67d6ad22fc9b64c7efa44fd7982fd72e26658165
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-02-08 16:09:01 -0300

    Solução do requisito 01

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const SUCCESS = 200;
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const readFile = util.promisify(fs.readFile);
-
-// app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+app.use(bodyParser.json());
+
+// Get Method - Requirement 01
 app.get('/crush', async (req, res) => {
   const fileName = path.join(__dirname, 'crush.json');
-  console.log(fileName);
-  const data = await readFile(fileName);
+  const dataCrush = await readFile(fileName);
 
-  res.status(200).send(data.toString('utf-8'));
+  res.status(200).send(JSON.parse(dataCrush));
 });
 
 app.listen(3000);
