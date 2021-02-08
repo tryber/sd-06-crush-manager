9c43e11b6734032c46c0eb1fb5a85bbac9bd1a87
thalesgr <thalesgr@gmail.com>
2021-02-08 16:16:17 -0300

    1 e 2

--- index.js
+++ index.js
@@ -21,7 +21,17 @@ function getCrush() {
 
 app.get('/crush', async (_req, res) => {
   const data = await getCrush();
-  res.status(200).send(data);
+  const treatedData = JSON.parse(data);
+  res.status(200).send(treatedData);
+});
+
+app.get('/crush/:id',async (req, res) => {
+  const id = Number(req.params.id);
+  const data = await getCrush();
+  const treatedData = JSON.parse(data);
+  const user = treatedData.filter((crush) => crush.id === id);
+  if (!user) return res.status(404).send({ message: 'Crush não encontrado' })
+  res.status(200).send(user);
 });
 
 app.listen(3000, () => console.log('servidor online porta 3000'));
