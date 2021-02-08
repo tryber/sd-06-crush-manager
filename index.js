55cb17222c028d79105d754981e2f45a29c54fcb
jederson <jmacedo.ti@gmail.com>
2021-02-08 16:34:37 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 const SUCCESS = 200;
@@ -8,4 +10,11 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-// iniciando o Projeto
+app.use(bodyParser.json());
+
+app.get('/crush', async (req, res) => {
+  const crushData = await fs.readFileSync('./crush.json', 'utf-8');
+  res.status(200).send(crushData);
+});
+
+app.listen(3000);
