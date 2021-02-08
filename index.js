d3f366d74b38f67fea6f38f630bcc369650481ef
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-02-08 18:44:52 -0300

    :construction: Requisito 1

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { readFile } = require('./util/manageFiles');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.use(bodyParser.json());
+app.use((request, _response, next) => {
+  console.log({
+    data: new Date(),
+    method: request.method,
+    route: request.originalUrl,
+  });
+  next();
+});
+
+app.get('/:fileName', async (request, response) => {
+  const { fileName } = request.params;
+  const myFile = await readFile(fileName);
+  response.status(SUCCESS).json(JSON.parse(myFile));
+});
+
+app.listen(port, () => console.log(`Executando na porta ${port}`));
