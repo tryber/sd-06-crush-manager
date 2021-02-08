6ba34ba41022a4d8e17786c55914d4b728ebbf97
Paulo <paulocheloni@gmail.com>
2021-02-08 15:02:43 -0300

    adding first commit

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 
+const routes = require('./routes')
+
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.all(/\//, routes)
