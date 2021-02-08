ed90c063cd463aa56060dc5764fa214fd27ec838
rafMewtwo <ponciano.raf@gmail.com>
2021-02-08 20:16:26 -0300

    requiriment 02

--- utils/managerFiles.js
+++ utils/managerFiles.js
@@ -5,17 +5,17 @@ const path = require('path');
 
 // leitura de um arquivo
 const readFile = async (fileName) => {
-  const file = await fs.readFile(fileName, 'utf-8')
+  const file = await fs.readFile(fileName, 'utf-8');
   return JSON.parse(file);
 };
 
 // escrita de um arquivo, nao carece de um retorno
 const writeFile = async (fileName, content) => {
-  await fs.writeFile(path(__dirname, '..', fileName), content, 'utf-8')
+  await fs.writeFile(path(__dirname, '..', fileName), content, 'utf-8');
   return true;
 };
 
 module.exports = {
   readFile,
-  writeFile
+  writeFile,
 };
