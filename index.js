be20911c03bdf85cf017b6eb4f5f7184db11957c
Paulo Frazão <paulo4187@gmail.com>
2021-02-08 17:44:15 -0300

    challenge 1, get array

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const crush = require('./crush.json');
 
 const app = express();
 const SUCCESS = 200;
-
+const porta = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (_, res) => {
+  if (crush.length > 0) {
+    return res.status(200).json(crush);
+  }
+  return res.status(200).json([]);
+});
+
+app.listen(porta, () => console.log('ON na Porta 3000!'));
