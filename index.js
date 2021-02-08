c85fc4797e0423485611f24e384129741536cb89
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-02-08 20:06:53 -0300

    Requisito 4 feito

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const NOTFOUND = 404;
 const NFmessage = { message: 'Crush não encontrado' };
 const auth = { token: '7mqaVRXJSp886CGr' };
 const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const regexDate = /(\d{2})[/](\d{2})[/](\d{4})/;
 
 function getCrushes() {
   return JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
@@ -45,6 +46,42 @@ app.post('/login', (req, res) => {
     return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
   res.status(SUCCESS).send(auth);
+});
+
+app.post('/crush', (req, res) => {
+  const { name, age, date } = req.body;
+  const { authorization } = req.headers;
+  console.log(req.body);
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization !== auth.token) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+  if (!date || date === '' || !date.datedAt || date.datedAt === '' || !date.rate || date.rate === '') {
+    return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (!regexDate.test(date.datedAt)) {
+    return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (date.rate !== 1 && date.rate !== 2 && date.rate !== 3 && date.rate !== 4 && date.rate !== 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  const crushes = getCrushes();
+  crushes.push(req.body);
+  res.status(201).send(req.body);
 });
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`));
