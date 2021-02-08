33d4082827a5c28b947c6681890b9feed32aaadc
Renan Cunha <renan2610@gmail.com>
2021-02-08 16:34:23 -0300

    Adding requirement 1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (req, res) => {
+  fs.readFile('./crush.json', (err, data) => {
+    // if (err) return console.error(err);
+    // console.log(data.toString());
+    res.status(200).send(data.toString());
+  });
+});
+
+app.listen(port);
