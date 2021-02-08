36085ccd344b2b32937823b1f1252de94a5d8975
phellipe5k <luizinhophe@hotmail.com>
2021-02-08 16:43:46 -0300

    starting project

---
+++ crush.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express.Router();
+
+const validateToken = (req, res, next) => {
+  if (!req.headers.token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (req.headers.token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+};
+
+const readJson = async () => {
+  const data = await fs.readFileSync('crush.json');
+  const json = await JSON.parse(data);
+  console.log(json);
+  return json;
+};
+
+app.get('/', validateToken, async (req, res) => {
+  const data = await readJson();
+  if (data === [] || data === null) return res.status(200).json([]);
+  res.status(200).json(data);
+});
+
+app.get('/search', validateToken, async (req, res) => {
+  const data = await readJson();
+  if (!req.query.q || req.query.q === '') return res.status(200).json(data);
+  const filtered = data.filter((el) => el.name.includes(req.query.q));
+  if (filtered === []) return res.status(200).json([]);
+  return res.status(200).json(filtered);
+});
+
+app.get('/:id', validateToken, async (req, res) => {
+  const data = await readJson();
+  const crush = await data.find((el) => el.id === Number(req.params.id));
+  console.log(crush);
+  if (crush === undefined) return res.status(404).json({ message: 'Crush não encontrado' });
+  res.status(200).json(crush);
+});
+
+app.delete('/:id', validateToken, async (req, res) => {
+  const data = await readJson();
+  const crush = await data.filter((el) => el.id !== Number(req.params.id));
+  const json = JSON.stringify(crush);
+  fs.writeFileSync('crush.json', json);
+  return res.status(200).json({ message: 'Crush deletado com sucesso' });
+});
+
+const validateCreateAndUpdate = ({ name, age, date }) => {
+  const dateValidate = (dates) => {
+    const regex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+    return regex.test(String(dates).toLowerCase());
+  };
+
+  console.log(dateValidate(date.datedAt));
+  if (!name || name === '') return { message: 'O campo "name" é obrigatório' };
+  if (name < 3) return { message: 'O "name" deve ter pelo menos 2 caracteres' };
+  if (!age || age === '') return { message: 'O campo "age" é obrigatório' };
+  if (age < 18) return { message: 'O crush deve ser maior de idade' };
+  if (!dateValidate(date.datedAt)) return { message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' };
+  if (date.rate < 1 && date.rate > 5) return { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
+  if (!date || date === '' || !date.datedAt || !date.rate) return { message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' };
+  return true;
+};
+
+app.post('/:id', async (req, res) => {
+  const crush = validateCreateAndUpdate(req.body);
+  console.log(req.body);
+  if (crush.message) return res.status(400).json(crush);
+  const data = await readJson();
+  const result = data.concat({ id: Number(req.params.id), ...req.body })
+    .sort((a, b) => a.id - b.id);
+  const json = JSON.stringify(result);
+  fs.writeFileSync('crush.json', json);
+  return res.status(201).json({ id: Number(req.params.id), ...req.body });
+});
+
+app.put('/:id', async (req, res) => {
+  const crush = validateCreateAndUpdate(req.body);
+  if (crush.message) return res.status(400).json(crush);
+  const data = await readJson();
+  const filter = data.filter((el) => el.id !== Number(req.params.id));
+  const result = filter.concat({ id: Number(req.params.id), ...req.body })
+    .sort((a, b) => a.id - b.id);
+  const json = JSON.stringify(result);
+  fs.writeFileSync('crush.json', json);
+  return res.status(200).json({ id: Number(req.params.id), ...req.body });
+});
+/*
+{
+  "name": "Keanu Reeves",
+  "age": 56,
+  "date": {
+    "datedAt": "22/10/2019",
+    "rate": 5
+  }
+}
+*/
+module.exports = app;
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const randtoken = require('rand-token');
+const crush = require('./crush.js');
+// const fs = require('fs');
 
 const app = express();
 const SUCCESS = 200;
@@ -7,3 +11,30 @@ const SUCCESS = 200;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.use(bodyParser.json());
+
+app.use('/crush', crush);
+
+// LOGIN
+
+const validateLogin = ({ email, password }) => {
+  const emailValidate = (em) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(em).toLowerCase());
+  };
+  if (!email || email === '') return { message: 'O campo "email" é obrigatório' };
+  if (!emailValidate(email)) return { message: 'O "email" deve ter o formato "email@email.com"' };
+  if (!password || password === '') return { message: 'O campo "password" é obrigatório' };
+  if (password.length < 6) return { message: 'O "password" ter pelo menos 6 caracteres' };
+  const token = randtoken.generate(16);
+  return { token };
+};
+
+app.post('/login', (req, res) => {
+  const validation = validateLogin(req.body);
+  if (validation.message) return res.status(400).json(validation);
+  return res.status(200).json(validation);
+});
+
+app.listen(3000);
