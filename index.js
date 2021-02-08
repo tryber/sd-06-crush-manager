ae9682efb1c4667429052cc42b8da925dfc6b6a2
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-08 15:48:25 -0300

    First commit, opening the PR

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.listen(port, () => console.log(`Listening to the usual port: ${port}`));
