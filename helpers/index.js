b4a590fc3b43b3aabb557624d12c6ab1e544e07a
jederson <jmacedo.ti@gmail.com>
2021-02-08 19:20:18 -0300

    Requisito 3, fixing 3

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 
 const getCrushes = async () => {
-  const arrayCrushes = await fs.readFileSync('./crush.json', 'utf-8');
+  const arrayCrushes = await fs.readFileSync('./crush.json', 'utf-8', (err) => {
+    if (err) throw new Error(err);
+    console.log('File read');
+  });
   return JSON.parse(arrayCrushes);
 };
 
