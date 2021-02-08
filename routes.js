234affa449982db67dc32d73ccee694c93208cc7
LeandroParisi <lepcbelisario@gmail.com>
2021-02-08 17:26:34 -0300

    resetando

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const getAllCrushes = require('./middleware/getAllCrushes.js');
 
 const router = express.Router();
 
-router.get('/crush');
+router.get('/crush', getAllCrushes);
 
 module.exports = router;
--- utils/crushReader.js
+++ utils/crushReader.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const path = require('path');
 
 const filePath = path.basename('../crush.json');
 
 const crushReader = async () => {
-  const file = await fs.readFile(filePath, (error, data) => {
+  const file = await fs.readFile(filePath, 'utf-8', (error, data) => {
     if (error) throw new Error('file could not be read');
     return data;
   });
