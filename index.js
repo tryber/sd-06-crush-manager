af45bd56fdf462d864e98849222d5d4c98ddb903
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-02-08 15:51:59 -0300

    Desafio 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const SUCCESS = 200;
@@ -6,4 +7,13 @@ const SUCCESS = 200;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', (_req, res) => {
+  fs.readFile('./crush.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Não foi possível ler o arquivo. Erro: ${err}`);
+    }
+    res.status(SUCCESS).send(data);
+  });
 });
