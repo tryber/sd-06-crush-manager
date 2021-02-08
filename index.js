cc2210e9e40ab231444e7f9e32728848f926b947
Paulo <paulocheloni@gmail.com>
2021-02-08 16:22:38 -0300

    adding req 1

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const SUCCESS = 200;
 const routes = require('./routes')
 
 
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
 app.all(/\//, routes)
+
+
+
+app.listen(3040, () => console.log('rodando na porta 3040'))
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const router = express();
+const fs = require('fs');
 
+router.get('/crush', (req, res) => {
+  fs.readFile('./crush.json', 'utf8', async (err, data) => {
+    return res.send(data)
+    
+  })
+})
+
+
+
+module.exports = router
