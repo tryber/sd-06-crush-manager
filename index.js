ce33e7b22e9870a1f5a89af8f606e259a9efa942
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-02-08 17:28:44 -0300

    Solução do requisito 02

--- index.js
+++ index.js
@@ -17,12 +17,29 @@ app.get('/', (_request, response) => {
 
 app.use(bodyParser.json());
 
-// Get Method - Requirement 01
-app.get('/crush', async (req, res) => {
+// Ler arquivo com os dados
+const getData = async () => {
   const fileName = path.join(__dirname, 'crush.json');
-  const dataCrush = await readFile(fileName);
+  const data = await readFile(fileName);
+  return JSON.parse(data);
+};
 
-  res.status(200).send(JSON.parse(dataCrush));
+// endpoint GET /crush - Requirement 01
+app.get('/crush', async (req, res) => {
+  const crushes = await getData();
+  res.status(200).send(crushes);
+});
+
+// endpoint GET /crush/:id - Requirement 02
+app.get('/crush/:id', async (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  const crushes = await getData();
+  const crushSelected = crushes.find((crush) => crush.id === id);
+
+  if (!crushSelected) {
+    return res.status(404).json({ message: 'Crush não encontrado' });
+  }
+  res.status(200).json(crushSelected);
 });
 
 app.listen(3000);
