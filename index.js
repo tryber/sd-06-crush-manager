69b142fa1a055d0bf8d8600f34dd8c6ad9457498
alcantara2509 <alcantara2509@gmail.com>
2021-02-08 19:11:43 -0300

    requisito 2

--- index.js
+++ index.js
@@ -9,13 +9,23 @@ const app = express();
 const SUCCESS = 200;
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(SUCCESS).send();
+app.get('/', (_req, res) => {
+  res.status(SUCCESS).send();
 });
 
-app.get('/crush', async (_request, response) => {
+app.get('/crush', async (_req, res) => {
   const data = await readFile('./crush.json', 'utf8');
-  response.status(SUCCESS).send(JSON.parse(data));
+  res.status(SUCCESS).send(JSON.parse(data));
+});
+
+app.get('/crush/:id', async (req, res) => {
+  const data = await readFile('./crush.json', 'utf8');
+  const dataId = req.params.id;
+  console.log(typeof +dataId);
+  if (!JSON.parse(data)[+dataId] || +dataId === 0) {
+    res.status(404).send({ message: 'Crush não encontrado' });
+  }
+  res.status(SUCCESS).send(JSON.parse(data)[+dataId - 1]);
 });
 
 app.listen(3000);
