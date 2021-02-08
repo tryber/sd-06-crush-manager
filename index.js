b7340e7c971144715a9c4a4051cb1780187d2ca4
Cássio Rubens <cassiorubensadv@gmail.com>
2021-02-08 17:11:59 -0300

    FEAT: 1 - Crie o endpoint GET

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const crush = require('./crush.json');//
 
 const app = express();
 const SUCCESS = 200;
+
+const port = 3001;//
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+//
+app.get('/crush', (_request, response) => {
+  if (crush.length > 0) {
+    response.status(SUCCESS).send(crush);
+  } else {
+    response.status(SUCCESS).send([]);
+  }
+});
+
+app.listen(port);
