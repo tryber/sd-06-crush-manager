ec4344a49f4c04cca0b14f51e8f75c86de610554
leoSasuke <leocavachini@hotmail.com>
2021-02-08 19:15:44 -0300

    desafio

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
+const data = require('./crush.json');
+
 const app = express();
+
 const SUCCESS = 200;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+// Requisito 1
+app.get('/crush', (_req, res) => {
+  if (data < 1) return ([]);
+  res.status(SUCCESS).send(data);
+});
+
+app.listen(3000, () => console.log('port working...'));
