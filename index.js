2932a4915e6fff7651f4396aa3704052e28eab27
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-02-08 19:28:20 -0300

    Desafio 1

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+const port = 3000;
 const SUCCESS = 200;
+
+app.use(express.json());
+
+let crush;
+
+fs.readFile('./crush.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  crush = data;
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (_request, response) => {
+  response.status(SUCCESS).send(crush);
+});
+
+app.listen(port);
