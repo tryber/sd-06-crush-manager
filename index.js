ae81e5c6ef8e4f3846dd62271740e324638d9a0e
ricardorosa-dev <ricardorosa@gmail.com>
2021-02-08 15:04:41 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
+const crushes = require('./crush.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (_req, res) => {
+  res.status(200).send(crushes);
+});
+
+app.listen(port, () => console.log('Listening on 3000...'));
