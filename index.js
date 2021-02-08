b6643ae3d2fa2e4b13a24fd858ada5da60cb614d
Isa Koga <isadorakoga710@gmail.com>
2021-02-08 17:02:31 -0300

    :memo: Req 2 + refat. coding

--- index.js
+++ index.js
@@ -4,21 +4,6 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 
-const fs = require('fs').promises;
-
-const arquive = './crush.json';
-
-const getData = async () => {
-  const newData = await fs.readFile(arquive, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Não foi possível ler o arquivo ${arquive}\n Erro: ${err}`);
-      process.exit(1);
-    }
-    return data;
-  });
-  return newData;
-};
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
@@ -26,9 +11,11 @@ app.get('/', (_request, response) => {
 
 app.use(bodyParser.json());
 
-app.get('/crush', async (req, res) => {
-  const response = await getData();
-  res.send(JSON.parse(response));
-});
+const getAllCrush = require('./getAllCrush');
+const getCrushById = require('./getCrushById');
+
+app.get('/crush', getAllCrush);
+
+app.get('/crush/:id', getCrushById);
 
 app.listen('3000');
