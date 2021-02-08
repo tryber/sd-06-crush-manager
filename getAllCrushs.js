cddf18633bd4954ed5504c501fa7552dd2f8fc14
Isa Koga <isadorakoga710@gmail.com>
2021-02-08 18:09:55 -0300

    :rotating_light: Linter fix

--- getAllCrushs.js
+++ getAllCrushs.js
@@ -3,6 +3,6 @@ const { getData } = require('./getData');
 const getAllCrush = async (req, res) => {
   const response = await getData();
   res.send(JSON.parse(response));
-}
+};
 
 module.exports = getAllCrush;
--- getCrushsById.js
+++ getCrushsById.js
@@ -2,16 +2,12 @@ const { getData } = require('./getData');
 
 const getCrushById = async (req, res) => {
   const response = await getData();
-  const id = req.params.id;
-  const selectedCrush = JSON.parse(response).find(crush => crush.id === parseInt(id));
+  const { id } = req.params;
+  const selectedCrush = JSON.parse(response).find((crush) => crush.id === parseInt(id, 1));
   if (!selectedCrush) {
     throw new Error('crush not found');
   }
   res.send(selectedCrush);
-}
-
-app.use(function (err, _req, res, _next) {
-  res.status(404).send(`Algo deu errado! Mensagem: ${err.message}`);
-});
+};
 
 module.exports = getCrushById;
