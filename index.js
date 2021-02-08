65099620d8cd6bdd929a9dd228b8a446253639ac
isabellavjs <isabellavjs@gmail.com>
2021-02-08 20:31:06 -0300

    Reabre PR

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 const port = 5000;
+
 const { getCrush, getCrushId } = require('./getRequest');
 const { handleLogin, addCrush } = require('./postRequest');
 const { handleError } = require('./utils/middlewareError');
