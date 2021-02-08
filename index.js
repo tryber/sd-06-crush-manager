2ea483d50e1db25228c8026d1867985153105ede
HCaetano <hrcgeo@gmail.com>
2021-02-08 19:21:42 -0300

    Move email and password pairs from headers to body

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const readFile = require('./util/readFile');
 const tokenGenerator = require('./util/tokenGenerator');
 
 const app = express();
+app.use(express.json());
 const SUCCESS = 200;
 const port = 3000;
 
@@ -36,8 +37,8 @@ app.get('/crush/:id', (request, response) => {
 });
 
 // challenge 3
-app.get('/login', (request, response) => {
-  const { email, password } = request.headers;
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
   const token = tokenGenerator();
   const emailValidator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -54,7 +55,7 @@ app.get('/login', (request, response) => {
   }
 
   if (password && password.length < 6) {
-    return response.status(400).send({ message: 'O "password" ter pelo menos 6 caracteres' });
+    return response.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
 
   response.status(200).send({ token });
