2393dfa1f9ea444478e792d0a0b27e20d6329f84
Jean Machado <jeanvictor.arq@gmail.com>
2021-02-08 16:47:09 -0300

    desafio 1

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const SUCCESS = 200;
 
+app.use(bodyParser.json());
+
+const CrashAll = require('./src/controllers/CrashAll');
+
+app.use('/crush', CrashAll);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.listen(3000, () => {
+  console.log('API ONLINE... Port 3000');
 });
---
+++ src/controllers/CrashAll.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
+
+const router = express.Router();
+
+const crushs = '../../crush.json';
+
+const readCrushs = async () => {
+  const content = await fs.readFile(path.resolve(__dirname, '.', crushs));
+
+  return JSON.parse(content.toString('utf8'));
+};
+
+router.get('/', async (_req, res) => {
+  const contentCrushs = await readCrushs();
+
+  console.log(contentCrushs);
+
+  if (contentCrushs.length === 0) {
+    return res.status(200).send([]);
+  }
+  return res.status(200).send(contentCrushs);
+});
+
+module.exports = router;
