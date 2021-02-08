724e2e92b2b13971fdf7c119c9536b40f048e544
Cyrano webdev <cyrano@cyranowebdev.com>
2021-02-08 15:34:34 -0300

    Iniciando Crush Manager!

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const crushJson = fs.readFileSync('./crush.json');
+
+const crush = JSON.parse(crushJson);
 
 const app = express();
+
+app.use(bodyParser.json());
+
 const SUCCESS = 200;
+
+app.get('/crush/:id', (req, res) => {
+  const { id } = req.params;
+  const result = (id && id >= 0) && crush.find((person) => person.id === Number(id));
+
+  if (result) return res.status(SUCCESS).send(result);
+
+  res.status(404).send('{ "message": "Crush não encontrado" }');
+});
+
+app.get('/crush', (req, res) => {
+  if (crush.length > 0) {
+    return res.status(SUCCESS).send(crush);
+  }
+  res.status(200).send([]);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.listen(3000);
