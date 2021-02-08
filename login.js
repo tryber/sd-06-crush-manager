f92108201f7d5f839da031b9676cd6a54905bb4d
Marcelo Pessini <marceloppessini@gmail.com>
2021-02-08 17:58:22 -0300

    desafio 3

--- login.js
+++ login.js
@@ -15,7 +15,7 @@ const login = async (request, response) => {
   if (!email) return response.status(400).send({ message: 'O campo "email" é obrigatório' });
   if (!validateEmail(email)) return response.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
   if (!password) return response.status(400).send({ message: 'O campo "password" é obrigatório' });
-  if (!validatePassword(password)) return response.status(400).send({ message: 'A "senha" ter pelo menos 6 caracteres' });
+  if (!validatePassword(password)) return response.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   const token = crypto.randomBytes(8).toString('hex');
   response.send({ token });
 };
