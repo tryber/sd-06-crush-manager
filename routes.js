251daf4c0ec1c8021ac3347928dd8ce87885e1bd
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-08 15:32:58 -0300

    Deletando routes.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const routes = express.Router();
-
-const listcrushes = require('./requests/getRequest');
-
-routes.get('/crush', listcrushes.listcrushes);
-
-module.exports = routes;
