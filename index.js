eed16dd82a841eb439ab170aff94d566189bb21d
rafMewtwo <ponciano.raf@gmail.com>
2021-02-08 18:10:42 -0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const crushs = require('./crush.json');
+
 const app = express();
 const SUCCESS = 200;
+
+const PORT = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (req, res) => res.status(200).send(crushs));
+
+app.listen(PORT, () => console.log('funcional')); 
