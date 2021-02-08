ba002c2343076121d2667d8c3a9ccc4779ae26fc
FlavioSugano <flavio.sugano@gmail.com>
2021-02-08 16:48:46 -0300

    Requisito 1: rota /crush

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(SUCCESS).send();
+  response.status(SUCCESS).send('Olá mundo');
 });
+
+app.get('/crush', (_req, res) => {
+  res.status(SUCCESS).send('rota crush');
+});
+
+app.listen(port, () => console.log('Example app listening on port port!'));
