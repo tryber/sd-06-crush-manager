e550f5c4207a22263ddf05e4b3b51943fe6b60bb
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-02-08 17:53:39 -0300

    feat: add 'crush' endpoint

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const { promisify } = require('util');
+
+const readCrushes = promisify(fs.readFile);
 const express = require('express');
 
 const app = express();
@@ -6,4 +10,28 @@ const SUCCESS = 200;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+/* app.get('/crush', (request, response) => {
+  const fileName = './crush.json';
+  const data = fs.readFile(fileName, (err, rawData) => {
+    if (err) {
+      throw new Error(`Could not read file ${ fileName }\n Error: ${ err }`);
+    }
+    const data = JSON.parse(rawData);
+    if (!data) response.status(200).send([]);
+    response.status(SUCCESS).send(data);
+  });
+}); */
+
+app.get('/crush', (request, response) => {
+  const fileName = './crush.json';
+  readCrushes(fileName)
+    .then((rawData) => JSON.parse(rawData))
+    .then((data) => response.status(SUCCESS).send(data))
+    .catch((error) => console.log(`Could not read file ${fileName}\n Error: ${error}`));
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
 });
