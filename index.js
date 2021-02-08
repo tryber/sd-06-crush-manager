9416a0ab43f74a4ca1104050458e753bef9f3bcf
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-02-08 17:01:18 -0300

    Requisito 2 feito

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const fs = require('fs');
 
 const port = 3000;
-
 const app = express();
 const SUCCESS = 200;
+const NOTFOUND = 404;
+const NFmessage = { message: 'Crush não encontrado' };
+
+let crushes = fs.readFileSync('./crush.json', 'utf8');
+crushes = JSON.parse(crushes);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -12,12 +16,14 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/crush', (_req, res) => {
-  fs.readFile('./crush.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Não foi possível ler o arquivo. Erro: ${err}`);
-    }
-    res.status(SUCCESS).send(JSON.parse(data));
-  });
+  res.status(SUCCESS).send(crushes);
+});
+
+app.get('/crush/:id', (req, res) => {
+  const { id } = req.params;
+  const crush = crushes.find((e) => e.id === parseInt(id, 10));
+  if (!crush) return res.status(NOTFOUND).send(NFmessage);
+  res.status(SUCCESS).send(crush);
 });
 
 app.listen(port, () => console.log(`Aplicação rodando na porta ${port}!`));
