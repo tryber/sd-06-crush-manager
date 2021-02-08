d4387dea68253eb12cdeefd72d5ceb56d34f6c41
Isa Koga <isadorakoga710@gmail.com>
2021-02-08 15:57:05 -0300

    :memo: Req 1:

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
 
+const fs = require('fs').promises;
+
+const arquive = './crush.json';
+
+const data = async () => {
+  const newData = await fs.readFile(arquive, 'utf8', (err, data)  => {
+    if (err) {
+      console.error(`Não foi possível ler o arquivo ${arquive}\n Erro: ${err}`);
+      process.exit(1);
+    }
+   return data;
+  });
+  return newData;
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.use(bodyParser.json());
+
+app.get('/crush', async (req, res) => {
+  const response = await data();
+  res.send(response);
+});
+
+app.listen('3000');
