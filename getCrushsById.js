079f3b06618c38e412d01462def82ac2ea2a3405
Isa Koga <isadorakoga710@gmail.com>
2021-02-08 18:12:55 -0300

    :rotating_light: Radix parameter

--- getCrushsById.js
+++ getCrushsById.js
@@ -3,7 +3,7 @@ const { getData } = require('./getData');
 const getCrushById = async (req, res) => {
   const response = await getData();
   const { id } = req.params;
-  const selectedCrush = JSON.parse(response).find((crush) => crush.id === parseInt(id, 1));
+  const selectedCrush = JSON.parse(response).find((crush) => crush.id === parseInt(id, 2));
   if (!selectedCrush) {
     throw new Error('crush not found');
   }
