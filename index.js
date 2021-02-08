563b753ea358b67aeea01520482b0940418bf462
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-02-08 19:39:43 -0300

    1° desafio retornando da forma correta, testando no github

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const { readFile } = require('./useful/readAndWriteFiles');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', async (_req, res) => {
+  const crush = await readFile('crush');
+  console.log(crush);
+  return res.status(200).send(crush);
+});
+
+app.listen(port, () => console.log('running port', port));
