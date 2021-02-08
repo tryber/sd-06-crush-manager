0a4a98bbf5fdd667a894ba0853030f0cef883d26
alcantara2509 <alcantara2509@gmail.com>
2021-02-08 19:34:41 -0300

    refatorando gets

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express');
-// const path = require('path');
-const fs = require('fs');
-const util = require('util');
-
-const readFile = util.promisify(fs.readFile);
+const { getAllCrushs, getCrushById } = require('./getCrushs');
 
 const app = express();
 const SUCCESS = 200;
@@ -13,19 +9,8 @@ app.get('/', (_req, res) => {
   res.status(SUCCESS).send();
 });
 
-app.get('/crush', async (_req, res) => {
-  const data = await readFile('./crush.json', 'utf8');
-  res.status(SUCCESS).send(JSON.parse(data));
-});
+app.get('/crush', getAllCrushs);
 
-app.get('/crush/:id', async (req, res) => {
-  const data = await readFile('./crush.json', 'utf8');
-  const dataId = req.params.id;
-  console.log(typeof +dataId);
-  if (!JSON.parse(data)[+dataId] || +dataId === 0) {
-    res.status(404).send({ message: 'Crush não encontrado' });
-  }
-  res.status(SUCCESS).send(JSON.parse(data)[+dataId - 1]);
-});
+app.get('/crush/:id', getCrushById);
 
 app.listen(3000);
