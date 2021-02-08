885af5d8d973c123fda4ea05b39bda750257e6dd
Daniel Moura <darthur.moura@gmail.com>
2021-02-08 18:29:07 -0300

    Commit inicial

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+
+
