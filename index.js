fc3bb92765f91ed5f0362e3afdd5ff94ef1414f5
danieleperse <danieleperse3@gmail.com>
2021-02-08 18:02:19 -0300

    desafio 1 finalizado - rota /crush criada

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
 const app = express();
 const SUCCESS = 200;
+const fs = require('fs');
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.json());
+
+const fileName = 'crush.json';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (req, res) => {
+  const readFile = fs.readFileSync(fileName);
+  if (!readFile) {
+    return res.status(200).send([]);
+  }
+  return res.status(200).send(JSON.parse(readFile));
+});
+
+app.listen(3000, () => console.log('listening port 3000'));
