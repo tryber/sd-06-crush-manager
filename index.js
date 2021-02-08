b348f42c1bda31a9738e03a6bc9c16c28d0b1994
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-02-08 19:12:24 -0300

    feat: add id param to 'crush' endpoint

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ app.get('/crush', (request, response) => {
     .catch((error) => console.log(`Could not read file ${fileName}\n Error: ${error}`));
 });
 
+app.get('/crush/:id', async (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  const fileName = './crush.json';
+  const crushes = await readCrushes(fileName)
+    .then((rawData) => JSON.parse(rawData))
+    .then((data) => data)
+    .catch((error) => console.log(`Could not read file ${fileName}\n Error: ${error}`));
+  const retrievedCrush = crushes.find((crush) => crush.id === id);
+  if (!retrievedCrush) response.status(200).json({ message: 'Crush não encontrado' });
+  response.status(SUCCESS).send(retrievedCrush);
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
