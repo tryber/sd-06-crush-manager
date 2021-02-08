303f8adf740cbfcdb88f45a068ada717c39dcd38
AlexanderFeitosa <alexander.feitosa@gmail.com>
2021-02-08 18:24:39 -0300

    :sparkles: Requisito 02

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ app.get('/crush', (_req, res) => {
   if (!crushList || crushList.length === 0) return res.status(SUCCESS).send([]);
 });
 
+app.get('/crush/:id', (req, res) => {
+  const data = fs.readFileSync('./crush.json', 'utf8');
+  const crushList = JSON.parse(data);
+  const { id } = req.params;
+
+  const crushIndex = crushList.findIndex((e) => e.id === parseInt(id, 10));
+
+  if (crushIndex === -1) return res.status(404).send({ message: 'Crush não encontrado' });
+
+  res.status(SUCCESS).send(crushList[crushIndex]);
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
