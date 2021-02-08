24addc47b218b16fe55cb84d288be452d404b2cd
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-02-08 18:09:55 -0300

    1° desafio retornando da forma correta, testando no github

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { readFile, writeFile } = require('./useful/readAndWriteFiles');
 
 const app = express();
 const SUCCESS = 200;
@@ -7,3 +8,11 @@ const SUCCESS = 200;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', async (req, res) => {
+  const crush = await readFile('crush');
+  console.log(crush);
+  return res.status(200).send(crush);
+});
+
+app.listen(3001, () => console.log('running port 3001'));
