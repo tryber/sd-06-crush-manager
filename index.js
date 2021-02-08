6731caafa7937b279175319a458bcab0a7c11579
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-02-08 18:53:37 -0300

    teste ssh 3

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { readFile } = require('./useful/readAndWriteFiles');
 const app = express();
 const SUCCESS = 200;
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
