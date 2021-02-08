398e844afdf4e569f26b385522d1e678e2b49d5b
Marcelo Pessini <marceloppessini@gmail.com>
2021-02-08 16:51:52 -0300

    desafio 2

--- index.js
+++ index.js
@@ -25,4 +25,14 @@ app.get('/crush', async (request, response) => {
   response.status(200).send(JSON.parse(data));
 });
 
+app.get('/crush/:id', async (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  const data = await fileContent(file);
+  const crush = await JSON.parse(data).find((crushs) => crushs.id === id) || false;
+  if (!crush) {
+    return response.status(404).send({ message: 'Crush não encontrado' });
+  }
+  response.status(200).send(crush);
+});
+
 app.listen(3000, () => console.log('Rodando na porta 3000'));
