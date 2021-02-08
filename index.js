bf32d1a53459c5ffa03ea0edf9b60c8c69dc8a0a
Luiz Mello <luizmacmillan@gmail.com>
2021-02-08 16:30:45 -0300

    Requisitos 1 e 2.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { getChrushes, getCrushById } = require('./src/endpoints/gets');
 
 const app = express();
 const SUCCESS = 200;
+const PORT = 3000;
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', getChrushes);
+app.get('/crush/:id', getCrushById);
+
+app.listen(PORT, () => console.log('Voando na Nimbus 3000'));
