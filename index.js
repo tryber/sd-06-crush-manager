84bf26c225ae35588ba3e1a915aac2318d15e44c
Ana Flávia <anaflvia@gmail.com>
2021-02-08 15:21:37 -0300

    iniciando projeto

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const fs = require('fs').promises;
+const rescue = require('express-rescue');
 
 const app = express();
 const SUCCESS = 200;
+const fileName = 'crush.json';
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', rescue(async (req, res) => {
+  const file = await fs.readFile(fileName);
+  res.send(file.toString('utf-8'));
+}));
+
+app.listen(3000, () => console.log('listening on port 3000'));
