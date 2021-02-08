542735aa21542e8005ac03c1602a962d7a926c12
Cyrano webdev <cyrano@cyranowebdev.com>
2021-02-08 16:17:07 -0300

    Req1 aprovado usando path.join

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const fs = require('fs');
-
-const crushJson = fs.readFileSync('./crush.json');
-
-const crush = JSON.parse(crushJson);
 
 const app = express();
 
@@ -12,7 +9,11 @@ app.use(bodyParser.json());
 
 const SUCCESS = 200;
 
+const crushPath = path.join('.', '/', 'crush.json');
+
 app.get('/crush/:id', (req, res) => {
+  const crushJson = fs.readFileSync(crushPath);
+  const crush = JSON.parse(crushJson);
   const { id } = req.params;
   const result = (id && id >= 0) && crush.find((person) => person.id === Number(id));
 
@@ -22,9 +23,11 @@ app.get('/crush/:id', (req, res) => {
 });
 
 app.get('/crush', (req, res) => {
-  if (!crush) return res.status(200).send([]);
+  const crushJson = fs.readFileSync(crushPath);
+  const crush = JSON.parse(crushJson);
+  if (!crush || crush.length === 0) return res.status(200).send([]);
 
-  res.status(SUCCESS).send(crush);
+  if (crush && crush.length > 0) return res.status(SUCCESS).send(crush);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
