2a6c523ac01028f1c0106996d313d8602ada5dbe
kissyla <kissylamottapereira@gmail.com>
2021-02-08 19:02:59 -0300

    requisito 1 certo

--- index.js
+++ index.js
@@ -3,19 +3,27 @@ const fs = require('fs');
 
 const app = express();
 const SUCCESS = 200;
+const NOT_FOUND = 404;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', (req, res) => {
-  fs.readFile('./crush.json', 'utf8', (err, data) => {
-    if (!data) {
-      res.status(SUCCESS).send('[]');
-    }
-    res.status(SUCCESS).send(data);
-  });
+app.get('/crush', (_req, res) => {
+  const file = fs.readFileSync('./crush.json', 'utf8');
+  res.json(JSON.parse(file));
 });
 
-app.listen(3000);
+// async app.get('/crush/:id', (req, res) => {
+//   const file = await fs.readFile('./crush.json', 'utf8');
+//   const dataFiles = JSON.parse(file);
+//   const { id } = req.params;
+//   const crush = dataFiles.filter((elemt) => {
+//     elemt.id === id;
+//   });
+//   if(!crush) res.status(NOT_FOUND);
+//   res.status(SUCCESS).send(crush)
+// });
+
+app.listen(3000, () => { console.log('porta: 3000 ativa'); });
