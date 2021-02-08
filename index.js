cc7d1d4bde8302419119f97eb4c07202b342cd73
ricardorosa-dev <ricardorosa@gmail.com>
2021-02-08 18:12:11 -0300

    requisito 2

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const app = express();
 const SUCCESS = 200;
 const port = 3000;
 
+const crushes = JSON.parse(fs.readFileSync('./crush.json'));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
 app.get('/crush', (_req, res) => {
-  const crushes = fs.readFileSync('./crush.json');
   if (crushes.length > 0) {
-    res.status(200).send(JSON.parse(crushes));
+    res.status(200).send(crushes);
   } else {
     res.status(200).send([]);
   }
+});
+
+app.get('/crush/:id', (req, res) => {
+  const crush = crushes.find((c) => c.id === parseInt(req.params.id, 10));
+  if (!crush) return res.status(404).send({ message: 'Crush não encontrado' });
+  res.status(200).send(crush);
 });
 
 app.listen(port, () => console.log('Listening on 3000...'));
