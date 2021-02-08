e78eacf4771f980c862d2696d26b0ae479e7ebe7
Marxneves <marxkhristie@gmail.com>
2021-02-08 20:38:08 -0300

    Iniciando projeto requisito 1 e 2

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const express = require('express');
+const crypto = require('crypto');
+
+module.exports = () => (crypto.randomBytes(8).toString('hex'));
 
 const app = express();
 const SUCCESS = 200;
@@ -7,3 +11,30 @@ const SUCCESS = 200;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (_request, response) => {
+  fs.readFile('./crush.json', (err, data) => {
+    if (err) throw new Error('Error');
+    response.status(SUCCESS).sendend(JSON.parse(data));
+  });
+});
+
+app.get('/crush/:id', (request, response) => {
+  const id = request.params.id - 1;
+
+  fs.readFile('./crush.json', (err, data) => {
+    if (err) throw new Error('Error');
+
+    if (!JSON.parse(data)[id]) {
+      response.status(404).send({ message: 'Crush não encontrado' });
+    }
+
+    response.status(SUCCESS).send(JSON.parse(data)[id]);
+  });
+});
+
+app.post('/login', (_request, _response) => {
+
+});
+
+app.listen(3000);
