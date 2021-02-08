ae0cb8beb9a8cd05f19762a75f98cb38bed5c958
thalesgr <thalesgr@gmail.com>
2021-02-08 15:41:17 -0300

    inicial

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
+const util = require('util');
 
 const app = express();
 const SUCCESS = 200;
@@ -7,3 +10,18 @@ const SUCCESS = 200;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.use(bodyParser.json());
+
+const readFile = util.promisify(fs.readFile);
+
+function getCrush() {
+  return readFile('./crush.json', 'utf-8');
+}
+
+app.get('/crush', async (_req, res) => {
+  const data = await getCrush();
+  res.status(200).send(data);
+});
+
+app.listen(3000, () => console.log('servidor online porta 3000'));
