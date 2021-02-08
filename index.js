53bc9ea65a7259dc5f1d015619ba4ecf77dc7bec
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2021-02-08 15:21:30 -0300

    file data and first request to data

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const crushRegistered = require('./data');
 
 const app = express();
 const SUCCESS = 200;
+
+app.get('/crush', (_req, res) => {
+  res.json(crushRegistered);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
