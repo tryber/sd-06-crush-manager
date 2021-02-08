1f0c5ff8d58e6f869409a7297c7977f3330cbadc
phellipe5k <luizinhophe@hotmail.com>
2021-02-08 17:47:35 -0300

    refresh

--- crush.js
+++ crush.js
@@ -51,7 +51,7 @@ const validateCreateAndUpdate = ({ name, age, date }) => {
   };
   console.log(name, age);
   if (!name || name === '') return { message: 'O campo "name" é obrigatório' };
-  if (name.length < 3) return { message: 'O "name" deve ter pelo menos 2 caracteres' };
+  if (name.length < 3) return { message: 'O "name" deve ter pelo menos 3 caracteres' };
   if (!age || age === '') return { message: 'O campo "age" é obrigatório' };
   if (age < 18) return { message: 'O crush deve ser maior de idade' };
   if (!dateValidate(date.datedAt)) return { message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' };
