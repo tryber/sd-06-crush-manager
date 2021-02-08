acaccb1769f936cc99bc5b5cfc16e1a98d56383c
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-08 20:05:53 -0300

    First item, returns the crush json but pattern may be wrong

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 const SUCCESS = 200;
-const port = 3000;
+const port = 3333;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.listen(port, () => console.log(`Listening to the usual port: ${port}`));
+app.use('/', routes);
+
+app.listen(port, () => console.log(`listening to port ${port}`));
