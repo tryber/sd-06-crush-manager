2c60d0c0bf01fb26c7dcc7f72ec65ade9d0d34a0
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-08 20:06:39 -0300

    Pattern may be correct now

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ main();
 
 routes.get('/crush', async (req, res) => {
   const file = await readFile('crush');
-  res.status(200).json(file);
+  res.status(200).json(JSON.parse(file));
 });
 
 module.exports = routes;
