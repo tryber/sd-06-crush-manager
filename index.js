8b07437d6caf997c850eeb00f78fda50b6857334
Jean Paulo Silva Vasconcelos <jean.vasconcelos6@gmail.com>
2021-02-08 11:30:11 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const SUCCESS = 200;
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(SUCCESS).send();
+});
