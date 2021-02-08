4257126f2547e6fbcc7365eb06e6f5180f43147a
danieleperse <danieleperse3@gmail.com>
2021-02-08 19:31:37 -0300

    desafio 2 ok - busca por id do crush funcionando

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 const fs = require('fs');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
-// app.use(bodyParser.json());
-
+app.use(bodyParser.json());
 const fileName = 'crush.json';
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+// desafio 1
 app.get('/crush', (req, res) => {
-  const readFile = fs.readFileSync(fileName);
-  if (!readFile) {
+  const file = fs.readFileSync(fileName);
+  if (!file) {
     return res.status(200).send([]);
   }
-  return res.status(200).send(JSON.parse(readFile));
+  return res.status(200).send(JSON.parse(file));
+});
+
+// desafio 2
+app.get('/crush/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const file = fs.readFileSync(fileName);
+  const crushes = JSON.parse(file);
+  let crush = null;
+
+  if (id > 0) {
+    crush = crushes.find((item) => id === item.id);
+  }
+
+  if (!crush) {
+    return res.status(404).send({ message: 'Crush não encontrado' });
+  }
+  return res.status(200).send(crush);
 });
 
 app.listen(3000, () => console.log('listening port 3000'));
