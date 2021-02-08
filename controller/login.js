d3f8a8934c693766578dd60c7ffab3f83a90e5fe
DehSartoreto <fixsartoreto@gmail.com>
2021-02-08 17:40:52 -0300

    Req 3 msg erro

--- controller/login.js
+++ controller/login.js
@@ -3,11 +3,11 @@ const { tokenGenerator, validateEmail, validatePassword } = require('../services
 const login = (req, res) => {
   const { email, password } = req.body;
 
-  if (!email) return res.status(400).json({ message: 'O campo email é obrigatório' });
-  if (!validateEmail(email)) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!validateEmail(email)) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
 
-  if (!password) return res.status(400).json({ message: 'O campo password é obrigatório' });
-  if (!validatePassword(password)) return res.status(400).json({ message: 'O password deve ter pelo menos 6 caracteres' });
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (!validatePassword(password)) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
 
   res.status(200).json({ token: tokenGenerator() });
 };
