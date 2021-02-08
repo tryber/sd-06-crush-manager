c2e97d74068ee264add48a32ed813ef77aa83c94
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-02-08 17:08:07 -0300

    Requisito 1 refeito

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const SUCCESS = 200;
 const NOTFOUND = 404;
 const NFmessage = { message: 'Crush não encontrado' };
 
-let crushes = fs.readFileSync('./crush.json', 'utf8');
-crushes = JSON.parse(crushes);
+function getCrushes() {
+  return JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -16,12 +17,12 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/crush', (_req, res) => {
-  res.status(SUCCESS).send(crushes);
+  res.status(SUCCESS).send(getCrushes());
 });
 
 app.get('/crush/:id', (req, res) => {
   const { id } = req.params;
-  const crush = crushes.find((e) => e.id === parseInt(id, 10));
+  const crush = getCrushes().find((e) => e.id === parseInt(id, 10));
   if (!crush) return res.status(NOTFOUND).send(NFmessage);
   res.status(SUCCESS).send(crush);
 });
