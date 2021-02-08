8c60ab8bfe75a9a906f13e2f390947bfb239ea35
DehSartoreto <fixsartoreto@gmail.com>
2021-02-08 15:50:35 -0300

    Requisito 1, criando /crush

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 const SUCCESS = 200;
@@ -8,3 +10,21 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+app.use(bodyParser.json());
+
+const getCrushes = async () => {
+  const crushList = await fs.readFileSync('./crush.json', 'utf-8', (err) => {
+    if (err) throw new Error(err);
+    console.log('File read');
+  });
+  return JSON.parse(crushList);
+};
+
+app.get('/crush', async (_req, res) => {
+  const crushData = await getCrushes();
+  res.status(200).send(crushData);
+});
+
+app.listen(3000, () => {
+  console.log('WORKING UHUL');
+});
