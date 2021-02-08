e4bf4da2f5c0904c2413ffd4727fb06afd936ee7
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-02-08 17:12:45 -0300

    add crush by id

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const { request } = require('http');
 
 const app = express();
 const SUCCESS = 200;
@@ -12,13 +13,23 @@ app.get('/', (_request, response) => {
 app.get('/crush', (_request, response) => {
   const readData = fs.readFileSync('crush.json');
   if (readData) {
-    const dataJson = JSON.parse(readData)
+    const dataJson = JSON.parse(readData);
     response.status(200).send(dataJson);
   } else {
     response.status(200).send(JSON.parse([]));
   }
+});
 
-  
+app.get('/crush/:id', (request, response) => {
+  const { id } = request.params;
+  const readData = fs.readFileSync('crush.json');
+  const dataJson = JSON.parse(readData);
+  const dataFiltered = dataJson.filter(item => item.id === +id);
+  if (dataFiltered.length !== 0) {
+    response.status(200).send(dataFiltered);
+  } else {
+    response.status(404).send(JSON.parse('{"message": "Crush não encontrado"}'));
+  }
 });
 
 app.listen(3000);
