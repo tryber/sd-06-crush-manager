760df586383753c77d788779d1455294ef5bcbee
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-02-08 17:49:55 -0300

    Requirement 2

--- index.js
+++ index.js
@@ -13,13 +13,23 @@ const read = (file) => {
   }
 };
 
+const dataCrush = read('crush.json');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
 app.get('/crush', (req, res) => {
-  res.status(SUCCESS).json(read('crush.json'));
+  res.status(SUCCESS).json(dataCrush);
+});
+
+app.get('/crush/:id', (req, res) => {
+  if (dataCrush[req.params.id - 1]) {
+    res.status(SUCCESS).json(dataCrush[req.params.id - 1]);
+  } else {
+    res.status(404).json({ "message": "Crush não encontrado" });
+  }
 });
 
 app.listen(3000);
