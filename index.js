8889a2dbc32631815cd86fb781115934ca17ac1d
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-02-08 17:48:10 -0300

    Requirement 01 - push 01

--- index.js
+++ index.js
@@ -3,7 +3,22 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 
+const port = 3000;
+const crushes = require('./crush.json');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.use(express.json());
+
+app.get('/crushes', (_req, res) => {
+  if (crushes.length > 0) {
+    res.status(200).send(crushes);
+  } else {
+    res.status(200).send([]);
+  }
+});
+
+app.listen(port, () => console.log('Running Project Crush Manager!'));
