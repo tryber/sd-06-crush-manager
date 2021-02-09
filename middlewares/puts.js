9b19e67e8f4ecee39a28c5b78ed64dbc4ed4ec80
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-09 16:24:01 -0300

    :art: Desafio 5
    corrigido o status de 201 para 200

--- middlewares/puts.js
+++ middlewares/puts.js
@@ -10,7 +10,7 @@ module.exports = {
     const { name, age, date } = req.body;
     if (!name) return next({ statusCode: 400, message: 'O campo "name" é obrigatório' });
     if (!age) return next({ statusCode: 400, message: 'O campo "age" é obrigatório' });
-    if (!date || !date.datedAt || !date.rate) return next({ statusCode: 400, message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    if (!date || !date.datedAt || date.rate !== 0) return next({ statusCode: 400, message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
 
     const trueName = validateName(name);
     if (!trueName) return next({ statusCode: 400, message: 'O "name" deve ter pelo menos 3 caracteres' });
@@ -19,13 +19,13 @@ module.exports = {
     const trueDate = validateDate(date.datedAt);
     if (!trueDate) return next({ statusCode: 400, message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
     const trueRate = validateRate(date.rate);
-    if (!trueRate && date.rate !== 0) return next({ statusCode: 400, message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    if (!trueRate && date.rate === 0) return next({ statusCode: 400, message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
 
     const newFile = await file.map((crush) => ((crush.id === +id)
       ? { ...bodyCrush, id: +id }
       : crush));
     console.log(bodyCrush);
-    res.status(201).json({ ...bodyCrush, id: +id });
+    res.status(200).json({ ...bodyCrush, id: +id });
     return writingFile(newFile);
   },
 };
