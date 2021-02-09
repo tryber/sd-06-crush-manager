e5a3ba3263c22020bd4fd36ad9f4b29ba260513a
CristianoNunes <cristianonunes99@gmail.com>
2021-02-09 19:52:15 -0300

    Desafio 04 - Correcao01

--- index.js
+++ index.js
@@ -20,11 +20,22 @@ const {
 
 const app = express();
 const SUCCESS = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
 const fileName = path.join(__dirname, 'crush.json');
-
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
+app.use(bodyParser.json());
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(SUCCESS).send();
+});
+
+// Ler os dados do arquivo
 const getData = async () => {
   const data = await readFile(fileName);
   return JSON.parse(data);
@@ -36,8 +47,6 @@ const setData = async (crushes, newCrush) => {
   const newArrayCrushesFormated = JSON.stringify(newArrayCrushes, null, '\t');
   await writeFile(fileName, newArrayCrushesFormated, 'utf-8');
 };
-
-app.use(bodyParser.json());
 
 // Desafio 01 - endpoint GET /crush
 app.get('/crush', async (req, res) => {
@@ -51,29 +60,29 @@ app.get('/crush/:id', async (req, res) => {
   const crushes = await getData();
   const crushSelected = crushes.find((crush) => crush.id === id);
   if (!crushSelected) {
-    return res.status(404).json({ message: 'Crush não encontrado' });
+    return res.status(NOT_FOUND).json({ message: 'Crush não encontrado' });
   }
-  res.status(200).json(crushSelected);
+  res.status(SUCCESS).json(crushSelected);
 });
 
 // Desafio 03 - endpoint POST /login
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
   if (!email || email === '') {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!checkEmail(email)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(BAD_REQUEST).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password || password.toString() === '') {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "password" é obrigatório' });
   }
   if (!checkPasswordCont(password)) {
-    return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+    return res.status(BAD_REQUEST).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
 
   const token = createToken();
-  res.status(200).json({ token });
+  res.status(SUCCESS).json({ token });
 });
 
 // Desafio 04 - endpoint POST /crush
@@ -81,43 +90,38 @@ app.post('/crush', async (req, res) => {
   const { token } = req.headers;
   const { name, age, date } = req.body;
   if (!token || token === '') {
-    return res.status(401).json({ message: 'Token não encontrado' });
+    return res.status(UNAUTHORIZED).json({ message: 'Token não encontrado' });
   }
   if (!verifyToken(token)) {
-    return res.status(401).json({ message: 'Token inválido' });
+    return res.status(UNAUTHORIZED).json({ message: 'Token inválido' });
   }
   if (!checkCrushName(name)) {
-    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "name" é obrigatório' });
   }
   if (!checkCrushNameLength(name)) {
-    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    return res.status(BAD_REQUEST).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   if (!checkAgeCrush(age)) {
-    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "age" é obrigatório' });
   }
   if (!checkAgeOlder(age)) {
-    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+    return res.status(BAD_REQUEST).json({ message: 'O crush deve ser maior de idade' });
   }
   if (!checkDateRate(date.datedAt, date.rate)) {
-    return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   }
   if (!formatDate(date.datedAt)) {
-    return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   }
   if (!checkRate(date.rate)) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    return res.status(BAD_REQUEST).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
   const crushes = await getData();
   const id = getNextId(crushes);
   const newCrush = { name, age, id, date };
   setData(crushes, newCrush);
-  res.status(201).json(newCrush);
-});
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(SUCCESS).send();
+  res.status(CREATED).json(newCrush);
 });
 
 app.listen(3000);
