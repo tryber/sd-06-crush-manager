8d4f4aea99c49463e97e0990aae88db8149df596
FlavioSugano <flavio.sugano@gmail.com>
2021-02-09 19:21:32 -0300

    Movendo a rota '/' p/ o arquivo routes.js

--- getAllCrushes.js
+++ getAllCrushes.js
@@ -1,15 +1,14 @@
-// const fs = require('fs').promises;
-// // const util = require('util');
+const fs = require('fs').promises;
 
-// // https://nodejs.org/api/util.html#util_util_promisify_original
-// const readFilePromise = fs.readFile;
+// https://nodejs.org/api/util.html#util_util_promisify_original
+const readFilePromise = fs.readFile;
 
-// const getAllCrushes = async (_req, res) => {
-//   const file = 'crush.json';
+const getAllCrushes = async (_req, res) => {
+  const file = 'crush.json';
 
-//   readFilePromise(file)
-//     .then((content) => res.status(200).send(JSON.parse(content)))
-//     .catch((error) => console.log(error));
-// };
+  readFilePromise(file)
+    .then((content) => res.status(200).send(JSON.parse(content)))
+    .catch((error) => console.log(error));
+};
 
-// module.exports = { getAllCrushes };
+module.exports = { getAllCrushes };
--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const express = require('express');
-const fs = require('fs').promises;
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
-const readFilePromise = fs.readFile;
-// app.use(routes);
+app.use(routes);
 
-const SUCCESS = 200;
 const port = 3000;
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(SUCCESS).send('Olá mundo');
-});
-
-app.get('/crush', async (_req, res) => {
-  const file = 'crush.json';
-
-  readFilePromise(file)
-    .then((content) => res.status(200).send(JSON.parse(content)))
-    .catch((error) => console.log(error));
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
