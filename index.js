3d763460aa4b2806f6da78f755c35fd2145f6ec2
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-09 10:48:44 -0300

    acertando requisito 2 devolvendo 400 ao invés de 404

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const SUCCESS = 200;
 const NOTFOUND = 400;
 const PORT = 3000;
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// const regexDate = /(\d{2})[/](\d{2})[/](\d{4})/;
 const notFoundCrush = { message: 'Crush não encontrado' };
 const requiredEmail = { message: 'O campo "email" é obrigatório' };
 const emailInvalid = { message: 'O "email" deve ter o formato "email@email.com"' };
@@ -13,18 +14,19 @@ const requiredPassword = { message: 'O campo "password" é obrigatório' };
 const invalidPassword = { message: 'A "senha" deve ter pelo menos 6 caracteres' };
 const token = { token: '7mqaVRXJSp886CGr' };
 
+const readerFile = () => {
+  // path.resolve traz resolução de caminhos
+  const file = JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
+  return file;
+};
+
+// quero que todas as requisições devolvam um json
+app.use(express.json()); // aqui já incorporou as funções do body-parse
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
-
-function readerFile() {
-  // path.resolve traz resolução de caminhos
-  return JSON.parse(fs.readFileSync('./crush.json', 'utf8'));
-}
-
-// quero que todas as requisições devolvam um json
-app.use(express.json()); // aqui já incorporou as funções do body-parse
 
 // Requisito 1
 app.get('/crush', (_request, response) => {
@@ -37,7 +39,7 @@ app.get('/crush/:id', (request, response) => {
   const { id } = request.params;
   const crushes = readerFile();
   const crush = crushes.find((element) => element.id === parseInt(id, 10));
-  if (!crush) return response.status(NOTFOUND).send(notFoundCrush);
+  if (!crush) return response.status(404).send(notFoundCrush);
   response.status(SUCCESS).send(crush);
 });
 
@@ -50,5 +52,28 @@ app.post('/login', (request, response) => {
   if (password.length < 6) return response.status(NOTFOUND).send(invalidPassword);
   response.status(SUCCESS).send(token);
 });
+
+// Requisito 4
+const released = (request, response, next) => {
+  const { release } = request.headers;
+  if (!release) return response.status(401).send({ message: 'Token não encontrado' });
+  if (release !== token.token) return response.status(401).send({ message: 'Token inválido' });
+  next();
+};
+
+app.use(released);
+
+// const validationInfo = (name, age, date) => {
+//   let message = '';
+//   if (!name) message = 'O campo "name" é obrigatório';
+//   if (name.length < 3) message = 'O "name" deve ter pelo menos 3 caracteres';
+//   if (!age) message = 'O campo "name" é obrigatório';
+//   if (age < 18) message = 'O crush deve ser maior de idade';
+// if (!regexDate.test(date.datedAt)) message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
+// };
+
+// app.post('/crush', (request, response) => {
+
+// }
 
 app.listen(PORT, () => console.log(`Em execução ${PORT}`));
