12f522c5964ce30227654d6fb590488d80aa9ff1
Cyrano webdev <cyrano@cyranowebdev.com>
2021-02-09 14:54:23 -0300

    Req5 incompleto.

--- crushRouter.js
+++ crushRouter.js
@@ -67,4 +67,39 @@ router.post('/', (req, res) => {
   res.status(CREATED).send(req.body);
 });
 
+router.put('/:id', (req, res) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(Unauthorized).send({ message: 'Token não encontrado' });
+  if (authorization && authorization.length !== 16) return res.status(Unauthorized).send({ message: 'Token inválido' });
+
+  const { name, age, date } = req.body;
+  if (!name || name === '') return res.status(badRequest).send({ message: 'O campo "name" é obrigatório' });
+  if (name && name.length < 3) return res.status(badRequest).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+
+  if (!age || age === null || !Number.isInteger(age)) return res.status(badRequest).send({ message: 'O campo "age" é obrigatório' });
+  if (age && Number.isInteger(age) && age < 18) return res.status(badRequest).send({ message: 'O crush deve ser maior de idade' });
+
+  if (!date || date === null
+    || !date.datedAt || date.datedAt === null || !date.rate || date.rate === null) {
+    return res.status(badRequest)
+      .send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  const reg = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  const validDatedAt = reg.test(String(date.datedAt).toLowerCase());
+  if (!validDatedAt) return res.status(badRequest).send({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  if (!Number.isInteger(date.rate) || date.rate < 1 || date.rate > 5) {
+    return res.status(badRequest)
+      .send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  const { id } = req.params;
+  const crushIndex = req.crushes.findIndex((person) => person.id === Number(id));
+
+  const updatedCrush = { ...req.body, id: Number(id) };
+  if (crushIndex >= 0) {
+    req.crushes[crushIndex] = updatedCrush;
+  }
+  res.status(SUCCESS).send(updatedCrush);
+});
+
 module.exports = router;
