b02b2f3e8022d7ef98cbfb452e0f2245d4b32bf1
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-09 16:06:18 -0300

    Second item, gets correct crush with id param

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ routes.get('/crush', async (req, res) => {
   res.status(200).json(JSON.parse(file));
 });
 
+routes.get('/crush/:id', async (req, res) => {
+  const crushId = parseInt(req.params.id, 10);
+  const fullFile = await readFile('crush');
+  const idFile = JSON.parse(fullFile).find((crush) => crush.id === crushId);
+  res.status(200).json(idFile);
+});
+
 module.exports = routes;
