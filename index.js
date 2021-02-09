b4066907712a2801a1ec04a43758e3075b5a5dcd
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-09 12:50:44 -0300

    Requisito 4

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const SUCCESS = 200;
 const NOTFOUND = 400;
 const PORT = 3000;
 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-// const regexDate = /(\d{2})[/](\d{2})[/](\d{4})/;
+const regexDate = /(\d{2})[/](\d{2})[/](\d{4})/;
 const notFoundCrush = { message: 'Crush não encontrado' };
 const requiredEmail = { message: 'O campo "email" é obrigatório' };
 const emailInvalid = { message: 'O "email" deve ter o formato "email@email.com"' };
@@ -63,17 +63,26 @@ const released = (request, response, next) => {
 
 app.use(released);
 
-// const validationInfo = (name, age, date) => {
-//   let message = '';
-//   if (!name) message = 'O campo "name" é obrigatório';
-//   if (name.length < 3) message = 'O "name" deve ter pelo menos 3 caracteres';
-//   if (!age) message = 'O campo "name" é obrigatório';
-//   if (age < 18) message = 'O crush deve ser maior de idade';
-// if (!regexDate.test(date.datedAt)) message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
-// };
+const validationInfo = (name, age, date) => {
+  let message = '';
+  if (!name) message = 'O campo "name" é obrigatório';
+  if (name.length < 3) message = 'O "name" deve ter pelo menos 3 caracteres';
+  if (!age) message = 'O campo "name" é obrigatório';
+  if (age < 18) message = 'O crush deve ser maior de idade';
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0))message = 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios';
+  if (!regexDate.test(date.datedAt)) message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
+  if (date.rate % 1 !== 0 || date.rate < 1 || date.rate > 5) message = 'O campo "rate" deve ser um inteiro de 1 à 5';
+  return message;
+};
 
-// app.post('/crush', (request, response) => {
-
-// }
+app.post('/crush', (req, res) => {
+  const { name, age, date } = req.body;
+  const message = validationInfo(name, age, date);
+  if (message !== '') return res.status(400).json({ message });
+  const crushes = readerFile();
+  const crush = { age, date, id: crushes.length + 1, name };
+  crushes.push(crush);
+  res.status(201).send(crush);
+});
 
 app.listen(PORT, () => console.log(`Em execução ${PORT}`));
