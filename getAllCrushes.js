a02235c1dfa78c12473578bbf1caae04238d2f75
FlavioSugano <flavio.sugano@gmail.com>
2021-02-08 23:51:36 -0300

    Requisito: alteração no modo de usar as funções como promises

--- getAllCrushes.js
+++ getAllCrushes.js
@@ -1,14 +1,10 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
-const util = require('util');
-
-// https://nodejs.org/api/util.html#util_util_promisify_original
-const readFilePromise = util.promisify(fs.readFile);
 
 const getAllCrushes = async (_req, res) => {
   const file = path.join(__dirname, 'crush.json');
 
-  const allCrushes = await readFilePromise(file);
+  const allCrushes = await fs.readFile(file);
 
   res.status(200);
   return res.send(JSON.parse(allCrushes));
