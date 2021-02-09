0f3ab95e3c597d2269105316bb2b01183c1df4d8
DehSartoreto <fixsartoreto@gmail.com>
2021-02-09 14:15:18 -0300

    req 5 fixed

--- controller/validateCrush.js
+++ controller/validateCrush.js
@@ -12,7 +12,7 @@ const validateCrush = (req, res, next) => {
   if (!age || age === '') return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
 
-  if (!date || !date.datedAt || !date.rate) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   if (!checkDate(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
 
