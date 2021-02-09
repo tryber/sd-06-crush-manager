dad47ecc3b72c4ee75ea0c3506bb72638390606a
Paulo Frazão <paulo4187@gmail.com>
2021-02-09 16:59:38 -0300

    challenge 2, find for route params.id

--- index.js
+++ index.js
@@ -28,4 +28,12 @@ app.get('/crush', async (_, res) => {
   const data = await getData('crush.json');
   res.status(200).send(data);
 });
+
+app.get('/crush/:id', async (req, res) => {
+  const data = await getData('crush.json');
+  const findCrushForId = data.filter((crush) => crush.id === +req.params.id);
+  if (findCrushForId.length > 0) return res.status(200).json(findCrushForId[0]);
+  return res.status(404).json({ message: 'Crush não encontrado' });
+});
+
 app.listen(porta, () => console.log('ON 3000!'));
