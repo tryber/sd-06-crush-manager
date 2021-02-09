ee6d21d8e5018ff24fd48ee48820f1f69f794ecc
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-02-09 15:36:00 -0300

    Checking 3rd requirement again

--- routes/requisito4.js
+++ routes/requisito4.js
@@ -1,35 +1,11 @@
-const crypto = require('crypto');
+const { writeFile } = require('../utils/manageFiles');
 
 const SUCCESS = 200;
 const ERROR = 400;
 
-const loginControl = (request, response, next) => {
-  const { email, password } = request.body;
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (!email) {
-    response.status(ERROR).json({ message: 'O campo "email" é obrigatório' });
-  } else if (!emailRegex.test(email) && email.length > 1) {
-    response.status(ERROR).json({
-      message: 'O "email" deve ter o formato "email@email.com"',
-    });
-  } else if (!password) {
-    response.status(ERROR).json({
-      message: 'O campo "password" é obrigatório',
-    });
-  } else if (password) {
-    if (password.length < 6) {
-      response.status(ERROR).json({
-        message: 'O "password" ter pelo menos 6 caracteres',
-      });
-    }
-  }
-
-  const token = crypto.randomBytes(8).toString('hex');
-  const generatedToken = { token };
-
-  response.status(SUCCESS).json(generatedToken);
-  next();
+const addCrush = (_request, _response, _next) => {
+  console.log(SUCCESS, ERROR);
+  writeFile();
 };
 
-module.exports = loginControl;
+module.exports = addCrush;
