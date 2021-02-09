30077d5d62af914f002ddb62dea8803250260cc9
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-02-09 14:25:22 -0300

    chushRouter mudando nomes das constantes

--- chushRouter.js
+++ chushRouter.js
@@ -5,16 +5,21 @@ const { readFile } = require('./useful/readAndWriteFiles');
 const chushRouter = express.Router();
 
 chushRouter.get('/', async (_req, res) => {
-  const crush = await readFile('crush');
-  return res.status(200).send(crush);
+  const crushTxt = await readFile('crush');
+  return res.status(200).send(crushTxt);
 });
 
 chushRouter.get('/:id', async (req, res) => {
-  const crush = await readFile('crush').then((chushTxt) => JSON.parse(chushTxt));
+  const crushArray = await readFile('crush').then((chushTxt) => JSON.parse(chushTxt));
   const { id } = req.params;
-  const crushId = crush.find((objCrush) => objCrush.id === +id);
-  if (!crushId) return res.status(404).send({ message: 'Crush não encontrado' });
-  return res.status(200).send(crushId);
+  const crushObj = crushArray.find((objCrush) => objCrush.id === +id);
+  if (!crushObj) return res.status(404).send({ message: 'Crush não encontrado' });
+  return res.status(200).send(crushObj);
+});
+
+chushRouter.post('/', async (_req, res) => {
+  const crushArray = await readFile('crush').then((chushTxt) => JSON.parse(chushTxt));
+  return res.status(200).send(crushArray);
 });
 
 module.exports = chushRouter;
