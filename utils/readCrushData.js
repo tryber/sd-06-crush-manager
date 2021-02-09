61cf0e86cc815f021d7c193e684c7cb41d7ef8b9
isabellavjs <isabellavjs@gmail.com>
2021-02-09 16:08:09 -0300

    Correction requirement 1

--- utils/readCrushData.js
+++ utils/readCrushData.js
@@ -2,15 +2,21 @@ const fs = require('fs').promises;
 
 const crushData = 'crush.json';
 
-const crush = fs.readFile(crushData, 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Não foi possível ler o arquivo ${crushData}\n Erro: ${err}`);
-    process.exit(1);
-  }
-  return data;
-});
+function readCrush() {
+  const crush = fs.readFile(crushData, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Não foi possível ler o arquivo ${crushData}\n Erro: ${err}`);
+      process.exit(1);
+    }
+    return data;
+  });
+  return crush;
+}
 
-const parsedData = async () => JSON.parse(await crush);
+const parsedData = async () => {
+  const data = await readCrush();
+  return JSON.parse(data);
+};
 
 module.exports = {
   parsedData,
