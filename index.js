a4d5650f65eda6a2e980cbfb188c6f818eeeec54
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-02-09 11:35:24 -0300

    Requirement 01 - push 10

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { readMyFile } = require('./src/utils/middlewares');
 
 const app = express();
 const SUCCESS = 200;
-
 const port = 3000;
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
