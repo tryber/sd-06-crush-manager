44d8757581860eb98b45b9339ac5c1a62fca68e4
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-02-09 18:46:34 -0300

    requisito 4

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const fileName = 'crush.json';
 const app = express();
 
 const SUCCESS = 200;
+const DONNE = 201;
 const FAILURE = 404;
 const BADREQUEST = 400;
+const ERROR = 401;
 
 app.use(express.json());
 
@@ -67,4 +69,45 @@ app.post('/login', (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
 
   return res.json({ token });
+});
+
+app.post('/crush', async (req, res) => {
+  const { name, age, date } = req.body;
+  const { authorization } = req.headers;
+  const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+
+  const file = await fs.readFile(fileName, 'utf-8');
+  const parsedJson = JSON.parse(file);
+  const newCrush = [ ...parsedJson, { id: parsedJson.length + 1, ...req.body } ];
+
+  if (!authorization) {
+    return res.status(ERROR).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length < 16) {
+    return res.status(ERROR).json({ message: 'Token inválido' });
+  }
+  if (!name || name === '') {
+    return res.status(BADREQUEST).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(BADREQUEST).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age || age === '') {
+    return res.status(BADREQUEST).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(BADREQUEST).json({ message: 'O crush deve ser maior de idade' });
+  }
+  if (date === undefined || !date || date.datedAt === '' || date.datedAt === undefined || date.rate === '' || date.rate === undefined) {
+    return res.status(BADREQUEST).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (regexDate.test(date.datedAt) === false) {
+    return res.status(BADREQUEST).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (date.rate < 1 || date.rate > 5) {
+    return res.status(BADREQUEST).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  await fs.writeFile(fileName, JSON.stringify(newCrush));
+  return res.status(DONNE).json({ id: parsedJson.length + 1, ...req.body });
 });
