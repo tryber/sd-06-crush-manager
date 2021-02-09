bcb627079577ff52ff7de17eedda255b3f697c65
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-09 18:51:44 -0300

    Only send token as response

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ routes.post('/login', (req, res) => {
   if (passwordValidation === '' || passwordValidation === undefined) res.status(400).json({ message: 'O campo "password  " é obrigatório' });
   if (emailValidation === false) res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com' });
   if (passwordValidation === false) res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
-  res.status(200).send({ emailValidation, passwordValidation, token });
+  res.status(200).send({ token });
 });
 
 module.exports = routes;
