3b5c321923a6a030674a65aa5318da9d82fa325c
Paulo <paulocheloni@gmail.com>
2021-02-09 13:17:27 -0300

    adding req 3

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const SUCCESS = 200;
 
-const routes = require('./routes')
+const routes = require('./routes');
 
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.all(/\//, routes)
+app.all(/\//, routes);
 
-
-
-app.listen(3000, () => console.log('rodando na porta 3000'))
+app.listen(3000, () => console.log('rodando na porta 3000'));
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,31 @@
 const express = require('express');
+
 const router = express();
 const fs = require('fs');
 
+const handleLogin = require('./helpers/handleLogin');
 
 router.get('/crush', (_req, res) => {
-   const file = fs.readFileSync('./crush.json', 'utf8');  
-   res.json(JSON.parse(file));
-})
-
-router.get('/crush/:id', (req, res) => {
-    fs.readFile('./crush.json', 'utf8', (_err, jsonData) => {
-      const {id} = req.params
-      const data = JSON.parse(jsonData);
-      const myCrush = data[id-1]
-      console.log(myCrush, 'crushes', id, 'id', data, 'data')
-      try {
-        if (myCrush.id == id)  res.status(200).json(myCrush)
-      } catch (err) {
-        res.json(404, {message: 'Crush não encontrado'})
-      }
-
-  });
-    
+  const file = fs.readFileSync('./crush.json', 'utf8');
+  res.json(JSON.parse(file));
 });
 
-module.exports = router
+router.get('/crush/:id', (req, res) => {
+  fs.readFile('./crush.json', 'utf8', (_err, jsonData) => {
+    const { id } = req.params;
+    const data = JSON.parse(jsonData);
+    const myCrush = data[id - 1];
+    console.log(myCrush, 'crushes', id, 'id', data, 'data');
+    try {
+      if (myCrush.id == id) res.status(200).json(myCrush);
+    } catch (err) {
+      res.json(404, { message: 'Crush não encontrado' });
+    }
+  });
+});
+
+router.post('/login', async (req, res) => {
+  await handleLogin(req, res);
+});
+
+module.exports = router;
