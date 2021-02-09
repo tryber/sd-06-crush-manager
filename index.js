a32503b631c0197f8e4077ce2729b1f32a4cd481
ricardorosa-dev <ricardorosa@gmail.com>
2021-02-09 05:02:07 -0300

    teste

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const { gen } = require('n-digit-token');
+const fileCrush = require('./crush.json');
 
 const app = express();
 const SUCCESS = 200;
 const port = 3000;
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -25,6 +29,19 @@ app.get('/crush/:id', (req, res) => {
   const crush = crushes.find((c) => c.id === parseInt(req.params.id, 10));
   if (!crush) return res.status(404).send({ message: 'Crush não encontrado' });
   res.status(200).send(crush);
+});
+
+app.post('/login', (req, res) => {
+  const myToken = gen(16);
+
+  // const data = JSON.stringify(req.body);
+  // fileCrush.push(data);
+  // console.log(fileCrush);
+  // // console.log(file_crush);
+  // fs.writeFileSync('crush.json', fileCrush);
+  console.log(fileCrush);
+
+  res.send({ token: myToken });
 });
 
 app.listen(port, () => console.log('Listening on 3000...'));
