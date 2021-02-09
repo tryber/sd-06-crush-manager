6c187ca51cfe148f58cc7313b1ce57e027a81fde
jederson <jmacedo.ti@gmail.com>
2021-02-09 16:43:39 -0300

    Requisito 5 fixing 1

--- middlewares/updateCrush.js
+++ middlewares/updateCrush.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const { getCrushes } = require('../helpers');
 
 const updateArrayCrushes = async (crush, id) => {
--- middlewares/validateCrush.js
+++ middlewares/validateCrush.js
@@ -10,7 +10,7 @@ const validateCrush = (req, res, next) => {
   if (!age || age === '') return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   if (validateAge(age)) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
 
-  if (!date || !date.datedAt || !date.rate) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   if (!validateDatePattern(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
 
