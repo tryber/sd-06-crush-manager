4a42ea88049dc257d13b679aeafbf1b230ab502c
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-02-09 11:25:55 -0300

    Requirement 01 - push 09

--- src/utils/middlewares.js
+++ src/utils/middlewares.js
@@ -3,11 +3,7 @@ const { readFile } = require('./manageFiles');
 const readMyFile = async (req, res) => {
   const { fileName } = req.params;
   const myCrushes = await readFile(fileName);
-  if (myCrushes.length > 0) {
-    res.status(200).json((myCrushes));
-  } else {
-    res.status(200).json([]);
-  }
+  res.status(200).json((myCrushes));
 };
 
 module.exports = {
