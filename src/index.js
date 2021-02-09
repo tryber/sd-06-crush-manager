e2506574a8a35643a16c0dfa6b59fed370ab8f60
Júlia Pôrto <juportob@gmail.com>
2021-02-09 16:04:27 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ app.get('/', (_request, response) => {
 
 app.use(bodyParser.json());
 app.listen(3000, () => console.log('running'));
+const { readFile } = require('./utils/manageFiles');
 
 // ------- Requisito 1 --------
-
-const { readFile } = require('./utils/manageFiles');
 
 app.get('/crush', async (_request, response) => {
   const crushes = await readFile('crush');
@@ -24,5 +23,19 @@ app.get('/crush', async (_request, response) => {
   } catch (error) {
     console.log(error);
     response.status(200).json([]);
+  }
+});
+
+// ------- Requisito 2 --------
+
+app.get('/crush/:id', async (request, response) => {
+  const { id } = request.params;
+  const myCrushes = await readFile('crush');
+  const element = JSON.parse(myCrushes).find((e) => e.id === parseInt(id, 10));
+
+  if (element) {
+    response.status(200).json(element);
+  } else {
+    response.status(404).json({ message: 'Crush não encontrado' });
   }
 });
