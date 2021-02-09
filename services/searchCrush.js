21a8243f50066871324627d6df755d4fd90c6d1b
Alvaro Neto <alvaroshn@gmail.com>
2021-02-09 16:42:08 -0300

    Outra tentativa req 07 busca

--- services/searchCrush.js
+++ services/searchCrush.js
@@ -6,8 +6,8 @@ const searchCrush = async (request, response) => {
   // console.log(queryTerm);
   const data = await readFile();
   if (!request.query.q || request.query.q === '') return response.status(SUCCESS).send(data);
-  const queryTerm = request.query.q;
-  const query = data.filter((crush) => crush.name.includes(queryTerm));
+  // const queryTerm = request.query.q;
+  const query = data.filter((crush) => crush.name.includes(request.query.q));
   console.log(query);
   response.status(SUCCESS).send(query);
 };
