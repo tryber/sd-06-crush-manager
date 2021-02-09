ec7ef6a3df6f014f8c31be81283be7189539e332
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-02-09 20:12:15 -0300

    another try file path

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/crush', async (_request, response) => {
-  const readData = await readFile('./crush.json');
+  const readData = await readFile('crush.json');
   if (readData) {
     const dataJson = JSON.parse(readData);
     response.status(200).json(dataJson);
@@ -27,13 +27,13 @@ app.get('/crush', async (_request, response) => {
 
 app.get('/crush/:id', async (request, response) => {
   const { id } = request.params;
-  const readData = await readFile('./crush.json');
+  const readData = await readFile('crush.json');
   const dataJson = await JSON.parse(readData);
   const dataFiltered = dataJson.filter((item) => item.id === +id);
   if (dataFiltered.length === 0) {
     response.status(404).json({ message: 'Crush não encontrado' });
   }
-  response.status(200).json(dataFiltered);
+  response.status(200).send(dataFiltered);
 });
 
 app.post('/login', (request, response) => {
@@ -61,11 +61,11 @@ app.post('/crush', async (request, response) => {
   if (!date.datedAt.test(/^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/)) return response.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   if (date.rate < 1 || date.rate > 5) return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
 
-  const readData = await readFile('./crush.json');
+  const readData = await readFile('crush.json');
   const dataJson = await JSON.parse(readData);
   const newCrush = { id: dataJson.length + 1, ...request.body };
   dataJson.push(newCrush);
-  await writeFile('./crush.json', JSON.stringify(dataJson));
+  await writeFile('crush.json', JSON.stringify(dataJson));
   response.status(201).json(newCrush);
 });
 
@@ -86,12 +86,12 @@ app.put('/crush/:id', async (request, response) => {
   if (!date.datedAt.test(/^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/)) return response.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   if (date.rate < 1 || date.rate > 5) return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
 
-  const readData = await readFile('./crush.json');
+  const readData = await readFile('crush.json');
   const dataJson = await JSON.parse(readData);
   const newData = dataJson.filter((item) => item.id !== +id);
   const itemModified = { id, ...request.body };
   newData.push(itemModified);
-  await writeFile('./crush.json', JSON.stringify(newData));
+  await writeFile('crush.json', JSON.stringify(newData));
   response.status(200).json(itemModified);
 });
 
@@ -104,7 +104,7 @@ app.delete('/crush/:id', async (request, response) => {
   const readData = await fs.readFile('./crush.json');
   const dataJson = await JSON.parse(readData);
   const newData = dataJson.filter((item) => item.id !== +id);
-  await writeFile('./crush.json', JSON.stringify(newData));
+  await writeFile('crush.json', JSON.stringify(newData));
   response.status(200).json({ message: 'Crush deletado com sucesso' });
 });
 
