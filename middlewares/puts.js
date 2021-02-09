a1509cec54c2c7a2f97fcd7de73b83c59e459219
Gui-Alucard <guilhermeanv7822@gmail.com>
2021-02-09 18:11:29 -0300

    :zap: Desafio 5
    Validações passado localmente

--- middlewares/puts.js
+++ middlewares/puts.js
@@ -4,25 +4,26 @@ const { validateName, validateAge, validateDate } = require('../services/validat
 module.exports = {
   async putCrushId(req, res, next) {
     const file = await readFiles();
-    const { params: { id }, body: bodyCrush } = req;
+    const { params: { id }, body: { name, age, date } } = req;
     if (!id) return next({ message: 'Crush não encontrado', statusCode: 404 });
 
-    if (!bodyCrush.name || bodyCrush.name === '') return next({ statusCode: 400, message: 'O campo "name" é obrigatório' });
-    if (!bodyCrush.age || bodyCrush.age === '') return next({ statusCode: 400, message: 'O campo "age" é obrigatório' });
-    if (!bodyCrush.date && !bodyCrush.date.datedAt && !bodyCrush.date.rate && bodyCrush.date.rate !== 0) return next({ statusCode: 400, message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    if (!name || name === '') return next({ statusCode: 400, message: 'O campo "name" é obrigatório' });
+    if (!age || age === '') return next({ statusCode: 400, message: 'O campo "age" é obrigatório' });
+    if (!date || date.datedAt === undefined || date.rate === undefined) return next({ statusCode: 400, message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
 
-    const trueName = validateName(bodyCrush.name);
+    const trueName = validateName(name);
     if (!trueName) return next({ statusCode: 400, message: 'O "name" deve ter pelo menos 3 caracteres' });
-    const trueAge = validateAge(bodyCrush.age);
+    const trueAge = validateAge(age);
     if (!trueAge) return next({ statusCode: 400, message: 'O crush deve ser maior de idade' });
-    const trueDate = validateDate(bodyCrush.date.datedAt);
+    const trueDate = validateDate(date.datedAt);
     if (!trueDate) return next({ statusCode: 400, message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
-    if (bodyCrush.date.rate <= 0 || bodyCrush.date.rate >= 6) return next({ statusCode: 400, message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    if (date.rate <= 0 || date.rate >= 6) return next({ statusCode: 400, message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
 
     const newFile = await file.map((crush) => ((crush.id === +id)
-      ? { ...bodyCrush, id: +id }
+      ? { name, age, date, id: +id }
       : crush));
-    res.status(200).json({ ...bodyCrush, id: +id });
+    console.log(date.datedAt);
+    res.status(200).json({ name, age, date, id: +id });
     return writingFile(newFile);
   },
 };
