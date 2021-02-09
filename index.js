cda77fc24ee437ab18ac030c61b0a149f4bc4dc9
leoSasuke <leocavachini@hotmail.com>
2021-02-09 16:16:50 -0300

    desafio1-2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
-const data = require('./crush.json');
+const fs = require('fs').promises;
 
 const app = express();
 
 const SUCCESS = 200;
 const port = 3000;
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -13,12 +15,30 @@ app.get('/', (_request, response) => {
 });
 
 // Requisito 1
-app.get('/crush', (_req, res) => {
-  if (data.length === 0) {
+app.get('/crush', async (_req, res) => {
+  const data = await fs.readFile('./crush.json');
+
+  if (data.length < 1) {
     res.status(SUCCESS).send([]);
-  } else {
-    res.status(SUCCESS).send(data);
   }
+
+  res.status(SUCCESS).send(JSON.parse(data));
+});
+// requisito 2
+app.get('/crush/:id', async (req, res) => {
+  const data = await fs.readFile('./crush.json');
+
+  const { id } = req.params;
+
+  const dataJSON = JSON.parse(data);
+
+  const result = dataJSON.find((el) => el.id === Number(id));
+
+  if (result === undefined) {
+    res.status(404).send({ message: 'Crush não encontrado' });
+  }
+
+  res.status(SUCCESS).send(result);
 });
 
 app.listen(port, () => console.log('working...'));
