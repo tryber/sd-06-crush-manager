d6e40b869a6761f0239b4873648a7e8a66055de1
Cássio Rubens <cassiorubensadv@gmail.com>
2021-02-09 20:52:31 -0300

    FEAT: Crie o endpoint POST /login

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { readFile } = require('./utils/manageFiles');//
 const app = express();
 const SUCCESS = 200;
 const port = 3000;//
-const Erro404 = 404;
+const Erro404 = 404;//
+const Erro400 = 400;//
 
 app.use(bodyParser.json());//
 
@@ -30,4 +31,23 @@ app.get('/crush/:id', async (request, response) => {
   response.status(SUCCESS).json(crushId);
 });
 
-app.listen(port);
+// 3 - Crie o endpoint POST /login
+const smsMandatoryEmail = { message: 'O campo "email" é obrigatório' };
+const mandatoryEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const smsEmailNull = { message: 'O "email" deve ter o formato "email@email.com"' };
+const smsMandatoryPassword = { message: 'O campo "password" é obrigatório' };
+const smsPasswordNull = { message: 'A "senha" deve ter pelo menos 6 caracteres' };
+const token = { token: '7mqaVRXJSp886CGr' };
+
+app.post('/login', (request, response) => {
+  const { email, password } = request.body;
+  if (!email || email === '') return response.status(Erro400).send(smsMandatoryEmail);
+  if (!mandatoryEmail.test(email)) return response.status(Erro400).send(smsEmailNull);
+  if (!password || password === '') return response.status(Erro400).send(smsMandatoryPassword);
+  if (password.toString().length < 6) {
+    return response.status(Erro400).send(smsPasswordNull);
+  }
+  response.status(SUCCESS).send(token);
+});
+
+app.listen(port, () => console.log(`Start http://localhost:${port}`));
