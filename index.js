5d4eae8227843ddbaf4e5e3764cbeacb28a8cf1e
Paulo Frazão <paulo4187@gmail.com>
2021-02-09 14:51:00 -0300

    test req1

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const crush = require('./crush.json');
+const fs = require('fs');
+// const crush = require('./crush.json');
+const util = require('util');
 
 const app = express();
 const SUCCESS = 200;
 const porta = 3000;
+const chupa = util.promisify(fs.readFile);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', (_, res) => {
-  if (crush.length > 0) {
-    return res.status(200).json(crush);
-  }
-  return res.status(200).json([]);
-});
+async function getApi() {
+  const data = await chupa('crush.json');
+  return JSON.parse(data);
+}
 
-app.listen(porta, () => console.log('ON na Porta 3000!'));
+app.get('/crush', async (_, res) => {
+  const data = await getApi();
+  res.status(200).json(data);
+});
+app.listen(porta, () => console.log('ON 3000!'));
