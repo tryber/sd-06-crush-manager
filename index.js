84c4929ffe31b49d9cfa44b533d8a45e70a99bde
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-02-09 17:17:53 -0300

    start commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 
-// não remova esse endpoint, e para o avaliador funcionar
+// não remova esse endpoint, e para o avaliador funcionar.
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
