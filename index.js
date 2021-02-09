6b71993de189a21cfef85b9b9b489d8cee3e745c
Ana Flávia <anaflvia@gmail.com>
2021-02-09 14:44:42 -0300

    requisito 5

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.post('/crush', async (req, res) => {
   const { name, age, date } = newCrush;
   const { authorization } = req.headers;
   const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+
   if (!authorization) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
@@ -99,6 +100,47 @@ app.post('/crush', async (req, res) => {
   fileJson.push(newCrush);
   await fs.writeFile(fileName, JSON.stringify(fileJson));
   return res.status(201).json(newCrush);
+});
+
+app.put('/crush/:id', async (req, res) => {
+  const file = await fs.readFile(fileName, 'utf-8');
+  let fileJson = JSON.parse(file);
+  const { id } = req.params;
+  const { name, age, date } = req.body;
+  const { authorization } = req.headers;
+  const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length < 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+  if (date === undefined || !date || date.datedAt === '' || date.datedAt === undefined || date.rate === '' || date.rate === undefined) {
+    return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (regexDate.test(date.datedAt) === false) {
+    return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (date.rate < 1 || date.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  const crushEdited = { name, age, date, id: +id };
+  fileJson = fileJson.map((crush) => (crush.id === +id ? crushEdited : crush));
+  await fs.writeFile(fileName, JSON.stringify(fileJson));
+  return res.status(200).json(crushEdited);
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
