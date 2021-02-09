bc7f883323ab6c4f3611c3f019faa33640767c7e
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-02-09 15:25:29 -0300

    Updating 3rd requirement

--- routes/requisito3.js
+++ routes/requisito3.js
@@ -7,20 +7,22 @@ const loginControl = (request, response, next) => {
   const { email, password } = request.body;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (email.length < 1) {
+  if (!email) {
     response.status(ERROR).json({ message: 'O campo "email" é obrigatório' });
   } else if (!emailRegex.test(email) && email.length > 1) {
     response.status(ERROR).json({
       message: 'O "email" deve ter o formato "email@email.com"',
     });
-  } else if (password < 111111 && password !== '') {
-    response.status(ERROR).json({
-      message: 'O "password" ter pelo menos 6 caracteres',
-    });
-  } else if (password === '') {
+  } else if (!password) {
     response.status(ERROR).json({
       message: 'O campo "password" é obrigatório',
     });
+  } else if (password) {
+    if (password.length < 6) {
+      response.status(ERROR).json({
+        message: 'O "password" ter pelo menos 6 caracteres',
+      });
+    }
   }
 
   const token = crypto.randomBytes(8).toString('hex');
