3182b6bc53c45de349e85821f7b087c477e4f4cc
Leandro <leandro.feitoza8018@gmail.com>
2021-02-09 16:45:20 -0300

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
+const readFile = require('./manageFiles');
 
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', async (req, res) => {
+  const result = await readFile();
+  if (result === []) return res.status(200).send('[]');
+  res.status(SUCCESS).send(result);
+});
+
+app.listen(port, () => console.log(`Server ouvindo na porta ${port}`));
