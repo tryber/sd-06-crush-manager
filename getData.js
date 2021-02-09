476e445c154c15262e75e504c38ed69fcde35737
Isa Koga <isadorakoga710@gmail.com>
2021-02-09 14:06:08 -0300

    :rotating_light: Fix a bug

--- getData.js
+++ getData.js
@@ -6,7 +6,6 @@ const getData = async () => {
   const newData = await fs.readFile(arquive, 'utf8', (err, data) => {
     if (err) {
       console.error(`Não foi possível ler o arquivo ${arquive}\n Erro: ${err}`);
-      // process.exit(1);
     }
     return data;
   });
