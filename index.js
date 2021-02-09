3c2f0731e428a0b4649562a3a7d5c056a5a48f30
danieleperse <danieleperse3@gmail.com>
2021-02-09 10:02:50 -0300

    desafio 3 ok - o token foi gerado e login validado

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const SUCCESS = 200;
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const randtoken = require('rand-token');
 
 app.use(bodyParser.json());
 const fileName = 'crush.json';
@@ -36,6 +37,28 @@ app.get('/crush/:id', (req, res) => {
     return res.status(404).send({ message: 'Crush não encontrado' });
   }
   return res.status(200).send(crush);
+});
+
+// desafio 3
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+
+  if (!email || email === '') {
+    return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!regex.test(email)) {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password || password === '') {
+    return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  }
+  if ((password.toString()).length < 6) {
+    return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+
+  const token = randtoken.generate(16);
+  return res.status(200).send({ token: `${token}` });
 });
 
 app.listen(3000, () => console.log('listening port 3000'));
