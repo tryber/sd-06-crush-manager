268ab587ae2cbef606209692ad9ce1fe842ac9e3
Alvaro Neto <alvaroshn@gmail.com>
2021-02-09 16:34:20 -0300

    Requisito 07 rodando localmente mas nao passando no teste

--- services/searchCrush.js
+++ services/searchCrush.js
@@ -8,6 +8,7 @@ const searchCrush = async (request, response) => {
   if (!request.query.q || request.query.q === '') return response.status(SUCCESS).send(data);
   const queryTerm = request.query.q;
   const query = data.filter((crush) => crush.name.includes(queryTerm));
+  console.log(query);
   response.status(SUCCESS).send(query);
 };
 
