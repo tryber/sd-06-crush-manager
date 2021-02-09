456f7e5156329c840629517cd2037b80a9881f19
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-02-09 13:16:19 -0300

    3° colocando status 400 nas requisiçoes erradas

--- loginRouter.js
+++ loginRouter.js
@@ -7,12 +7,12 @@ const loginRouter = express.Router();
 
 loginRouter.use((req, res, next) => {
   const { email, password } = req.body;
-  if (!email) return res.json({ message: 'O campo "email" é obrigatório' });
-  if (checkEmail(email)) return res.json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  if (!password) return res.json({ message: 'O campo "password" é obrigatório' });
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (checkEmail(email)) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   const passwordString = password.toString();
   // console.log(passwordString.length);
-  if (passwordString.length < 6) return res.json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  if (passwordString.length < 6) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   next();
 });
 
