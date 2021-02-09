0ac35187add243c5e17b2d4fe343819e458639ca
danieleperse <danieleperse3@gmail.com>
2021-02-09 19:26:44 -0300

    evoluçao dia 2 projeto

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
-const fs = require('fs');
+const fs = require('fs').promises;
 const bodyParser = require('body-parser');
 const randtoken = require('rand-token');
 
@@ -14,8 +14,8 @@ app.get('/', (_request, response) => {
 });
 
 // desafio 1
-app.get('/crush', (req, res) => {
-  const file = fs.readFileSync(fileName);
+app.get('/crush', async (req, res) => {
+  const file = await fs.readFile(fileName);
   if (!file) {
     return res.status(200).send([]);
   }
@@ -23,10 +23,10 @@ app.get('/crush', (req, res) => {
 });
 
 // desafio 2
-app.get('/crush/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const file = fs.readFileSync(fileName);
+app.get('/crush/:id', async (req, res) => {
+  const file = await fs.readFile(fileName);
   const crushes = JSON.parse(file);
+  const id = Number(req.params.id);
   let crush = null;
 
   if (id > 0) {
@@ -53,12 +53,59 @@ app.post('/login', (req, res) => {
   if (!password || password === '') {
     return res.status(400).send({ message: 'O campo "password" é obrigatório' });
   }
-  if ((password.toString()).length < 6) {
+  if (password.toString().length < 6) {
     return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
 
   const token = randtoken.generate(16);
   return res.status(200).send({ token: `${token}` });
+});
+
+// validação token
+const tokenValidation = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: 'Token não encontrado' });
+  }
+  if (req.headers.authorization.length < 16) {
+    return res.status(401).send({ message: 'Token inválido' });
+  }
+  next();
+};
+
+// desafio 4
+app.post('/crush', tokenValidation, async (req, res) => {
+  const file = await fs.readFile(fileName);
+  const crushes = JSON.parse(file);
+  console.log(crushes);
+  const { name, age, date } = req.body;
+
+  const regex = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+
+  if (!name || name === '') return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  if (!age || age === '' || !Number.isInteger(age)) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  if (!date || !date.datedAt || !date.rate || date === '' || date.datedAt === '' || date.rate === '') {
+    return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (!Number.isInteger(date.rate) || date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  if (regex.test(date.datedAt) === false) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+
+  const newCrush = {
+    name,
+    age,
+    id: file.length + 1,
+    date: {
+      datedAt: date.datedAt,
+      rate: date.rate,
+    },
+  };
+  // const addArray = crushes.push(newCrush);
+  // console.log(newCrush);
+  // const jsonCrush = JSON.stringify(newCrush);
+
+  const addCrush = fs.writeFile('crush.json', newCrush);
+  return res.status(201).json(addCrush);
 });
 
 app.listen(3000, () => console.log('listening port 3000'));
