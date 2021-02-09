184e4dd1b70137e8e54423cdeb79a7cd813f94f2
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-09 16:45:06 -0300

    Different routes order

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 const routes = require('express').Router();
 const { readFile } = require('../utils/fileFunctions');
 
-routes.get('/crush', async (req, res) => {
-  const file = await readFile('crush');
-  res.status(200).json(JSON.parse(file));
-});
-
 routes.get('/crush/:id', async (req, res) => {
   const crushId = parseInt(req.params.id, 10);
   const fullFile = await readFile('crush');
   const idFile = JSON.parse(fullFile).find((crush) => crush.id === crushId);
   if (idFile !== undefined) res.status(200).json(idFile);
   res.status(404).json({ message: 'Crush não encontrado' });
+});
+
+routes.get('/crush', async (req, res) => {
+  const file = await readFile('crush');
+  res.status(200).json(JSON.parse(file));
 });
 
 routes.post('/login', async (req, res) => {
---
+++ utils/loginFunctions.js
@@ -0,0 +1,7 @@
+export const validateEmail = (email) => {
+
+};
+
+export const validatePassword = (password0) => {
+
+};
