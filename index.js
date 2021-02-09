01e4caec81515746fea301d4c21e4728edc135a5
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-09 15:19:26 -0300

    Limpando os if/else do requisito 4

--- index.js
+++ index.js
@@ -66,21 +66,15 @@ app.use(released);
 
 const validationInfo = (name, age, date) => {
   let message = '';
-  if (!name) {
-    message = 'O campo "name" é obrigatório';
-  } else if (name.length < 3) {
-    message = 'O "name" deve ter pelo menos 3 caracteres';
-  }
-  if (!age) {
-    message = 'O campo "age" é obrigatório';
-  } else if (age < 18) {
-    message = 'O crush deve ser maior de idade';
-  }
+  if (!name) message = 'O campo "name" é obrigatório';
+  if (name.length < 3) message = 'O "name" deve ter pelo menos 3 caracteres';
+  if (!age) message = 'O campo "age" é obrigatório';
+  if (age < 18) message = 'O crush deve ser maior de idade';
   if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
     message = 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios';
-  } else if (!regexDate.test(date.datedAt)) {
-    message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
-  } else if (date.rate % 1 !== 0 || date.rate < 1 || date.rate > 5) {
+  }
+  if (!regexDate.test(date.datedAt)) message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
+  if (date.rate % 1 !== 0 || date.rate < 1 || date.rate > 5) {
     message = 'O campo "rate" deve ser um inteiro de 1 à 5';
   }
   return message;
@@ -100,7 +94,7 @@ app.post('/crush', (request, response) => {
 app.put('/crush/:id', (request, response) => {
   const { name, age, date } = request.body;
   let { id } = request.params;
-  // test pede id seja inteiro tem converter
+  // test exige que o  id seja inteiro tem converter
   id = parseInt(id, 10);
   const message = validationInfo(name, age, date);
   if (message !== '') return response.status(400).json({ message });
