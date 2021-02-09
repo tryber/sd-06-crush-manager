9ab9312fde0dd985cef96f194328ac2d646a5bfc
Cássio Rubens <cassiorubensadv@gmail.com>
2021-02-09 19:44:43 -0300

    FEAT: Crie o endpoint GET /crush/:id

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { readFile } = require('./utils/manageFiles');//
 const app = express();
 const SUCCESS = 200;
 const port = 3000;//
+const Erro404 = 404;
 
 app.use(bodyParser.json());//
 
@@ -14,16 +15,19 @@ app.get('/', (_request, response) => {
 });
 //
 
+// 1 - Crie o endpoint GET /crush
 app.get('/crush', async (_request, response) => {
-  // const { crush } = _request.params;
   const crushs = await readFile();
-  // response.status(SUCCESS).json(crushs);
-  // if (crushs.length > 0) {
-  //   response.status(SUCCESS).send(crushs);
-  // } else {
-  //   response.status(SUCCESS).send([]);
-  // }
   response.status(SUCCESS).json(crushs);
+});
+
+// 2 - Crie o endpoint GET /crush/:id
+app.get('/crush/:id', async (request, response) => {
+  const crushs = await readFile();
+  const id = parseInt(request.params.id, 10);
+  const crushId = crushs.find((crush) => crush.id === id);
+  if (!crushId) response.status(Erro404).json({ message: 'Crush não encontrado' });
+  response.status(SUCCESS).json(crushId);
 });
 
 app.listen(port);
