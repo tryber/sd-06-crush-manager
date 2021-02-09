bcc6c1e03bad2c39cbc8ec84011230b12ee08e44
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-02-09 19:01:28 -0300

    req 4 correction

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -9,7 +9,7 @@ const passwordValidate = (password) => {
 };
 
 const dataValidate = (data) => {
-  const regex = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/gm;
+  const regex = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/gm;
   return !regex.test(data);
 };
 
