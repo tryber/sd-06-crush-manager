48dd546e72b94a95e06542223d596265d9a27e83
Paulo Frazão <paulo4187@gmail.com>
2021-02-09 15:29:39 -0300

    update challenge 1, length list

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const fs = require('fs');
-// const crush = require('./crush.json');
-const util = require('util');
+const fs = require('fs').promises;
 
 const app = express();
 const SUCCESS = 200;
 const porta = 3000;
-const chupa = util.promisify(fs.readFile);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-async function getApi() {
-  const data = await chupa('crush.json');
-  return JSON.parse(data);
+async function getData(arq) {
+  const data = await fs.readFile(arq);
+  return data;
 }
 
+// function readFilePromise(fileName) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(fileName, (err, content) => {
+//       if (err) return reject(err);
+//       resolve(content);
+//     });
+//   });
+// }
+
 app.get('/crush', async (_, res) => {
-  const data = await getApi();
-  res.status(200).json(data);
+  const data = await getData('crush.json');
+  res.status(200).send(data);
 });
 app.listen(porta, () => console.log('ON 3000!'));
