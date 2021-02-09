4af3defe24855be7efced122c84421f633267a5c
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-02-09 16:07:17 -0300

    Refactoring...

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 const express = require('express');
 const fs = require('fs');
+const crypto = require('crypto');
 
 const app = express();
 const SUCCESS = 200;
 
-const read = (file) => {
-  try {
-    const data = fs.readFileSync(file, 'utf8');
-    return JSON.parse(data);
-  } catch (err) {
-    console.error(`Error: ${err}`);
-  }
+app.use(express.json());
+
+const read = async () => {
+  const data = await fs.readFileSync('crush.json', 'utf8');
+  return JSON.parse(data);
 };
 
-const dataCrush = read('crush.json');
+const valEmail = (email) => {
+  const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.com$/i;
+  return emailRegex.test(email);
+};
+
+const valPassword = (password) => {
+  if (!password) {
+    return false;
+  }
+  const validate = password.toString();
+  return validate.length >= 6;
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', (req, res) => {
-  res.status(SUCCESS).json(dataCrush);
+app.get('/crush', async (req, res) => {
+  const dataCrush = await read();
+  res.status(SUCCESS).send(dataCrush);
 });
 
-app.get('/crush/:id', (req, res) => {
+app.get('/crush/:id', async (req, res) => {
+  const dataCrush = await read();
   if (dataCrush[req.params.id - 1]) {
     res.status(SUCCESS).json(dataCrush[req.params.id - 1]);
   } else {
-    res.status(404).json({ "message": "Crush não encontrado" });
+    res.status(404).json({ message: 'Crush não encontrado' });
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  } else if (!valEmail(email)) {
+    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  } else if (!valPassword(password)) {
+    res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+  if (valEmail(email) && valPassword(password)) {
+    const token = crypto.randomBytes(8).toString('hex');
+    res.status(SUCCESS).json({ token });
   }
 });
 
