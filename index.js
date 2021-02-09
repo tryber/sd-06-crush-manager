98cf865abc093908f169efc221157560bfaffd50
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-09 15:32:08 -0300

    Time que esta ganahando não se mexe. Firula só depois do 80%

--- index.js
+++ index.js
@@ -66,15 +66,21 @@ app.use(released);
 
 const validationInfo = (name, age, date) => {
   let message = '';
-  if (!name) message = 'O campo "name" é obrigatório';
-  if (name.length < 3) message = 'O "name" deve ter pelo menos 3 caracteres';
-  if (!age) message = 'O campo "age" é obrigatório';
-  if (age < 18) message = 'O crush deve ser maior de idade';
+  if (!name) {
+    message = 'O campo "name" é obrigatório';
+  } else if (name.length < 3) {
+    message = 'O "name" deve ter pelo menos 3 caracteres';
+  }
+  if (!age) {
+    message = 'O campo "age" é obrigatório';
+  } else if (age < 18) {
+    message = 'O crush deve ser maior de idade';
+  }
   if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
     message = 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios';
-  }
-  if (!regexDate.test(date.datedAt)) message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
-  if (date.rate % 1 !== 0 || date.rate < 1 || date.rate > 5) {
+  } else if (!regexDate.test(date.datedAt)) {
+    message = 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"';
+  } else if (date.rate % 1 !== 0 || date.rate < 1 || date.rate > 5) {
     message = 'O campo "rate" deve ser um inteiro de 1 à 5';
   }
   return message;
