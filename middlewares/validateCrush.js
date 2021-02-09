faa2e3ba3ac13a4f1e2afeb8486105d5177b6d13
jederson <jmacedo.ti@gmail.com>
2021-02-09 15:36:28 -0300

    Requisito 4 validate date

--- middlewares/validateCrush.js
+++ middlewares/validateCrush.js
@@ -12,7 +12,7 @@ const validateCrush = (req, res, next) => {
 
   if (!date || !date.datedAt || !date.rate) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  if (validateDatePattern(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  if (!validateDatePattern(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
 
   return next();
 };
