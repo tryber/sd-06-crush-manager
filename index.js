b73e5f32734b30d4e3681632b0327a15e185a89f
Leandro <leandro.feitoza8018@gmail.com>
2021-02-09 18:05:43 -0300

    Corrigido o retorno da tratativa de erro

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/crush/:id', async (req, res) => {
   const { id } = req.params;
   const result = await readFile();
   const filteredID = result.find((crush) => crush.id === parseInt(id, 10));
-  if (filteredID === undefined) return res.status(404).json({ message: 'Crush não encontrado' });
+  if (filteredID === undefined) return res.status(404).send({ message: 'Crush não encontrado' });
   res.status(200).send(filteredID);
 });
 
