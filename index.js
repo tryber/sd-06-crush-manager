621e5693a50a225cbcf9737bf7e1c374b835710a
Anderson R <misterregisslv@gmail.com>
2021-02-09 19:49:27 -0300

    refatoraçao

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const pathJoin = require('path');
 const app = express();
 const SUCCESS = 200;
 
-const lerArquivo = async (arquivo) => {
-  const conteudoArquivo = await fs.readFile(path(pathJoin.join(__dirname, arquivo)), 'utf-8');
-  return conteudoArquivo;
-};
-
 app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+const lerArquivo = async (arquivo) => {
+  const conteudoArquivo = await fs.readFile(path(pathJoin.join(__dirname, arquivo)), 'utf-8');
+  return conteudoArquivo;
+};
 
 app.get('/crush', async (__request, response) => {
   response.status(SUCCESS).send(await lerArquivo('/crush.json'));
