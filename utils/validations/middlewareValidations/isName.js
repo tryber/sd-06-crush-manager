2975eb47ac673d7fabfb31a64356636b06e61967
rafMewtwo <ponciano.raf@gmail.com>
2021-02-10 20:40:24 -0300

    req4x8

--- utils/validations/middlewareValidations/isName.js
+++ utils/validations/middlewareValidations/isName.js
@@ -1,10 +1,10 @@
 const isName = (req, res, next) => {
   const nameBody = req.body.name;
   if (nameBody === null || nameBody === '') {
-    return res.status(400).json({ message: 'O campo \"name\" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   }
   if (nameBody.length < 3) {
-    return res.status(400).json({ message: 'O \"name\" deve ter pelo menos 3 caracteres' });
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   next();
 };
