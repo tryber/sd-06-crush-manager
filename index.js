2ed28c017f3f22622a9bc74013ab397f14bc0744
Rodrigo Leite <rods.leite27@gmail.com>
2021-02-10 09:10:18 -0300

    fixing req 1 - 6

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ const PORT = 3000;
 
 app.use(middlewares.logger);
 
-const tokenValidator = (req, res, next) => {
-  const { Authorization } = req.headers;
-  if (!Authorization) return res.status(401).json({ message: 'Token não encontrado' });
-  if (Authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
-  next();
-};
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
@@ -30,7 +23,7 @@ app.get('/crush',
     res.status(200).json(JSON.parse(file));
   });
 // req 7
-app.get('/crush/search', tokenValidator,
+app.get('/crush/search',
   async (req, res) => {
     const { name } = req.query;
     const file = JSON.parse(await utils.readFile());
@@ -50,68 +43,97 @@ app.get('/crush/:id',
 app.post('/login',
   async (req, res) => {
     const token = await utils.token();
-    const { email, senha } = req.body;
+    const { email, password } = req.body;
     const validator = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
     const isValid = validator.test(String(email).toLowerCase());
     if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     if (!isValid) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-    if (!senha) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-    if (senha.length < 6) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+    if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    if (password.length < 6) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
     return res.status(200).json({ token });
   });
-app.post('/crush', tokenValidator,
+app.post('/crush',
   async (req, res) => {
-    const { name, age, dateNow, rate } = req.body;
-    const date = { datedAt: dateNow, rate };
+    const { name, age, date } = req.body;
+    const { authorization } = req.headers;
+
+    // validação token
+    if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+    if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+    // name authentication
+    if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    if (name.length < 4) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+
+    // age authentication
+    if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+    if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+
+    // date authentication
+    if (!date) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    if (!date.datedAt || !date.rate) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    const dateValidator = (dates) => {
+      const regex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
+      return regex.test(String(dates).toLocaleLowerCase());
+    };
+    if (!dateValidator(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+
+    // escrever arquivo
     const file = JSON.parse(await utils.readFile());
     const id = file.length + 1;
-    const dateValidator = (dates) => {
-      const regex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
-      return regex.test(String(dates).toLocaleLowerCase());
-    };
-    // name authentication
-    if (!name) return res.status(400).json({ messag: 'O campo "name" é obrigatório' });
-    if (name.length < 4) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-    // age authentication
-    if (!age || age === '') return res.status(400).json({ messag: 'O campo "age" é obrigatório' });
-    if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
-    // date authentication
-    if (!date || date === '' || !date.datedAt || date.rate === undefined) return ({ message: 'O campo date é obrigatório e datedAt e rate não podem ser vazios' });
-    if (rate > 5 || rate < 1) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-    if (!dateValidator(dateNow)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
     file.push({ id, name, age, date });
-    res.status(201).json(file);
+    // utils.writeFile(JSON.stringify(file));
+    utils.writeFile(file);
+    return res.status(201).json({ id, name, age, date });
   });
 
-app.put('/crush/:id', tokenValidator,
+app.put('/crush/:id',
   async (req, res) => {
-    const { id } = req.params;
-    const { name, age, dateNow, rate } = req.body;
-    const date = { datedAt: dateNow, rate };
-    const file = JSON.parse(await utils.readFile());
+    const { name, age, date } = req.body;
+    const { authorization } = req.headers;
+    const id = +req.params.id;
+    // validação token
+    if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+    if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+    // name authentication
+    if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+    if (name.length < 3) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+
+    // age authentication
+    if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+    if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+
+    // date authentication
+    if (!date) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    if (!date.datedAt || !date.rate) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
     const dateValidator = (dates) => {
       const regex = /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/;
       return regex.test(String(dates).toLocaleLowerCase());
     };
-    // name authentication
-    if (!name) return res.status(400).json({ messag: 'O campo "name" é obrigatório' });
-    if (name.length < 4) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-    // age authentication
-    if (!age || age === '') return res.status(400).json({ messag: 'O campo "age" é obrigatório' });
-    if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
-    // date authentication
-    if (!date || date === '' || !date.datedAt || date.rate === undefined) return ({ message: 'O campo date é obrigatório e datedAt e rate não podem ser vazios' });
-    if (rate > 5 || rate < 1) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-    if (!dateValidator(dateNow)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+    if (!dateValidator(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+    // escrever arquivo
+    const file = JSON.parse(await utils.readFile());
     const personIndex = file.findIndex((person) => person.id === id);
-    res.status(200).json(file[personIndex] = { id, name, age, date, rate });
+    const newCrush = { id, name, age, date };
+    file[personIndex] = newCrush;
+    utils.writeFile(file);
+    return res.status(200).json(file[personIndex]);
   });
 
-app.delete('/crush/:id', tokenValidator,
+app.delete('/crush/:id',
   async (req, res) => {
-    const { id } = req.params;
+    // validação token
+    const { authorization } = req.headers;
+    if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
+    if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
+    const id = +req.params.id;
     const file = JSON.parse(await utils.readFile());
     file.splice(id - 1, 1);
+    utils.writeFile(file);
     res.status(200).json({ message: 'Crush deletado com sucesso' });
   });
 
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 const readFile = require('./readFile.js');
 const token = require('./token.js');
+const writeFile = require('./writeFile.js');
 
 module.exports = {
   readFile,
   token,
+  writeFile,
 };
