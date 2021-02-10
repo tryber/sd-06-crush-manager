89e6b57e34a2690304f19a48e95aba2aa70e15e3
Júlia Pôrto <juportob@gmail.com>
2021-02-10 18:32:18 -0300

    Requisito 4

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.get('/', (_request, response) => {
 
 app.use(bodyParser.json());
 app.listen(3000, () => console.log('running'));
-const { readFile } = require('./src/utils/manageFiles');
-const { emailValid, passwordValid } = require('./src/utils/validations');
+const { readFile, writeFile } = require('./src/utils/manageFiles');
+const { emailValid, passwordValid, dateValid } = require('./src/utils/validations');
+const { response } = require('express');
 
 // ------- Requisito 1 --------
 
@@ -54,4 +55,35 @@ app.post('/login', (request, response) => {
   if (passwordValid(password)) return response.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
 
   return response.status(200).json({ token });
+});
+
+// ------- Requisito 4 --------
+
+app.post('/crush', async (request, _resolve) => {
+  const token = request.headers.authorization;
+  const { name, age, date } = request.body;
+
+  if (!token) return response.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return response.status(401).json({ message: 'Token inválido' });
+
+  if (!name) return response.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length <= 3) return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+
+  if (!age || age === '') return response.status(400).json({ essage: 'O campo "age" é obrigatório' });
+  if (age < 18) return response.status(400).json({ message: 'O crush deve ser maior de idade' });
+
+  if (!date || date === '' || !date.datedAt || date.rate === undefined) {
+    return response.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (date.rate < 1 || date.rate > 5) return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  if (dateValid(date.datedAt)) return response.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+
+  const crushes = await readFile('crush');
+  const parsedCrushes = JSON.parse(crushes);
+  const crushesObject = { id: parsedCrushes.length + 1, ...request.body };
+  const crushesData = crushes.concat(crushesObject);
+
+  await writeFile(JSON.stringify(crushesData));
+
+  return response.status(201).json(crushesObject);
 });
