4dfa852a9071e506e4cd6aae73547c3692af1c63
Tiago Berwanger <berwangertiago@gmail.com>
2021-02-10 13:26:25 -0300

    Requisito 1 - ajusting 2

--- src/middleware/readCrushes.js
+++ src/middleware/readCrushes.js
@@ -2,7 +2,9 @@ const { readFile } = require('../utils/managefile');
 
 const readCrushes = async (_req, res) => {
   const crushes = await readFile('crush');
-  res.status(200).json(JSON.parse(crushes));
+  const data = (JSON.parse(crushes));
+  console.log(Array.isArray(data));
+  res.status(200).json(data);
 };
 
 module.exports = readCrushes;
