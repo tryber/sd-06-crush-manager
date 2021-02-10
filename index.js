c30755ad67291ad436702ee206c54644a86b7243
RenatoCostaDev <renatotrybe@gmail.com>
2021-02-10 15:25:01 -0300

    Requirement-2

--- index.js
+++ index.js
@@ -22,3 +22,16 @@ app.get('/crush', async (_req, res) => {
 });
 
 app.listen(3000, () => console.log(`Using port: ${port}`));
+
+// Requisito-2 obs: id da url vem formato string, por isso o Number(id)
+// res com o crush doo id da rota ou { "message": "Crush não encontrado" } e o status 404
+app.get('/crush/:id', async (req, res) => {
+  const crushes = await fs.readFile(crushArray);
+  const crushesJson = JSON.parse(crushes);
+  const { id } = req.params;
+  const crush = crushesJson.find((crushJson) => crushJson.id === Number(id));
+  if (!crush) {
+    return res.status(404).json({ message: 'Crush não encontrado' });
+  }
+  return res.status(200).json(crush);
+});
