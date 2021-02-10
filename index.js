1cc34cfd805d8f91446b1720ce8de247046a7db3
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-02-09 22:23:05 -0300

    test2

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/crush', async (request, response) => {
   if (!age || age === '') return response.status(400).send({ message: 'O campo "age" é obrigatório' });
   if (age < 18) return response.status(400).send({ message: 'O crush deve ser maior de idade' });
 
-  if (!date || !date.datedAt || !date.rate) return response.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  if (!date || !date.datedAt || (!date.rate || date.rate !== 0)) return response.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   if (!((/^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/).test(date.datedAt))) return response.status(400).send({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
 
   if (date.rate < 1 || date.rate > 5) return response.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
