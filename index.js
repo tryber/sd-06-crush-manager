854e007b031a94af91740b3176ef20eed927ff08
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-02-09 23:52:47 -0300

    test9

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const util = require('util');
+// const util = require('util');
 const bodyParser = require('body-parser');
 
 const app = express();
