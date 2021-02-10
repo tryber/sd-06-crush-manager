90b40ab9dfe638e6eeffe7436032f59c08cd7908
leoSasuke <leocavachini@hotmail.com>
2021-02-10 11:45:20 -0300

    desafio3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const fs = require('fs').promises;
 
+const crypto = require('crypto');
+
 const app = express();
 
 const SUCCESS = 200;
@@ -9,6 +11,15 @@ const port = 3000;
 
 app.use(express.json());
 
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex'); // 16 digitos aleatorios 8bytes
+}
+function validEmail(email) {
+  return /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{3,8})?$/.test(email); // validação emial.
+}
+function validPassword(password) {
+  return /[0-9]{6}/.test(password);
+}
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
@@ -40,5 +51,16 @@ app.get('/crush/:id', async (req, res) => {
 
   res.status(SUCCESS).send(result);
 });
+// desafio 3
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
+  if (email === undefined) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (validEmail(email) === false) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  if (password === undefined) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (validPassword(password) === false) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+
+  const token = generateToken();
+  return res.send({ token });
+});
 app.listen(port, () => console.log('working...'));
