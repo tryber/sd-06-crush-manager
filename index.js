8874215300e6b6ab283829f37179b71aa2e22dcc
Paulo Frazão <paulo4187@gmail.com>
2021-02-10 14:25:21 -0300

    test branch 3

--- index.js
+++ index.js
@@ -69,58 +69,58 @@ app.post('/login', (req, res) => {
 //   fs.writeFile(pathArq, newData);
 // }
 
-app.post('/crush', async (req, res) => {
-  const { authorization } = req.headers;
-  const regexToken = /^[a-zA-Z0-9]*$/i;
-  const regexDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
+// app.post('/crush', async (req, res) => {
+//   const { authorization } = req.headers;
+//   const regexToken = /^[a-zA-Z0-9]*$/i;
+//   const regexDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
 
-  // const data = await getData('crush.json');
-  // const newArr = data.concat(bodyData);
-  // console.log(data);
-  // writingNewCrush('./crush.json', JSON.stringify(newArr));
+//   // const data = await getData('crush.json');
+//   // const newArr = data.concat(bodyData);
+//   // console.log(data);
+//   // writingNewCrush('./crush.json', JSON.stringify(newArr));
 
-  if (!authorization) {
-    return res.status(401).json({
-      message: 'Token não encontrado',
-    });
-  }
-  if (!regexToken.test(authorization) || authorization.length !== 16) {
-    return res.status(401).json({
-      message: 'Token inválido',
-    });
-  }
-  if (!req.body.name || req.body.name === '') {
-    res.status(400).json({
-      message: 'O campo "name" é obrigatório',
-    });
-  }
-  if (req.body.name.length < 3) {
-    res.status(400).json({
-      message: 'O "name" deve ter pelo menos 3 caracteres',
-    });
-  }
-  if (!req.body.age || req.body.age === '') {
-    res.status(400).json({
-      message: 'O campo "age" é obrigatório',
-    });
-  }
-  if (req.body.age < 18) {
-    res.status(400).json({
-      message: 'O crush deve ser maior de idade',
-    });
-  }
+//   if (!authorization) {
+//     return res.status(401).json({
+//       message: 'Token não encontrado',
+//     });
+//   }
+//   if (!regexToken.test(authorization) || authorization.length !== 16) {
+//     return res.status(401).json({
+//       message: 'Token inválido',
+//     });
+//   }
+//   if (!req.body.name || req.body.name === '') {
+//     res.status(400).json({
+//       message: 'O campo "name" é obrigatório',
+//     });
+//   }
+//   if (req.body.name.length < 3) {
+//     res.status(400).json({
+//       message: 'O "name" deve ter pelo menos 3 caracteres',
+//     });
+//   }
+//   if (!req.body.age || req.body.age === '') {
+//     res.status(400).json({
+//       message: 'O campo "age" é obrigatório',
+//     });
+//   }
+//   if (req.body.age < 18) {
+//     res.status(400).json({
+//       message: 'O crush deve ser maior de idade',
+//     });
+//   }
 
-  if (!regexDate.test(req.body.date.datedAt)) {
-    res.status(400).json({
-      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
-    });
-  }
-  if (req.body.date.rate < 1 || req.body.date.rate > 5) {
-    res.status(400).json({
-      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
-    });
-  }
-  res.status(201).json(req.body);
-});
+//   if (!regexDate.test(req.body.date.datedAt)) {
+//     res.status(400).json({
+//       message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+//     });
+//   }
+//   if (req.body.date.rate < 1 || req.body.date.rate > 5) {
+//     res.status(400).json({
+//       message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+//     });
+//   }
+//   res.status(201).json(req.body);
+// });
 
 app.listen(door, () => console.log('ON --- PORT --- 3000!'));
