cbfec9cf044f5651163acb26f73e938ff6cb48d9
rafMewtwo <ponciano.raf@gmail.com>
2021-02-10 19:33:18 -0300

    istoken com next

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { readFile } = require('./utils/managerFiles');
 const { generateToken } = require('./utils/generateToken');
 const { isEmail } = require('./utils/validations/isEmail');
 const { isPassword } = require('./utils/validations/isPassword');
+const { isToken } = require('./utils/validations/isToken');
 
 const app = express();
 // preciso da linha 6 para o express ler variaveis do tipo json
@@ -20,7 +21,7 @@ app.get('/', (_request, response) => {
 // requeriment 1 / Crie o endpoint GET /crush
 app.get('/crush', async (req, res) => {
   const crushs = await readFile('crush.json');
-  res.status(SUCCESS).send(crushs);
+  return res.status(SUCCESS).send(crushs);
 });
 // end requeriment 1
 // requeriment 2 / Crie o endpoint GET /crush/:id
@@ -32,7 +33,6 @@ app.get('/crush/:id', async (req, res) => {
     return res.status(SUCCESS).send(result[0]);
   }
   // duvida (?);
-  // const crushnotfound = { menssage: 'Crush não encontrado' };
   return res.status(NOTFOUND).json({ message: 'Crush não encontrado' });
 });
 // end requeriment 2
@@ -44,17 +44,19 @@ app.post('/login', (req, res) => {
   const resultEmail = isEmail(email);
   const resultPassword = isPassword(password);
 
-  if (resultEmail[0] !== 200) res.status(resultEmail[0]).json({ message: resultEmail[1] });
-  if (resultPassword[0] !== 200) res.status(resultPassword[0]).json({ message: resultPassword[1] });
+  if (resultEmail[0] !== 200) return res.status(resultEmail[0]).json({ message: resultEmail[1] });
+  if (resultPassword[0] !== 200) return res.status(resultPassword[0]).json({ message: resultPassword[1] });
 
-  res.status(SUCCESS).json({ token: resultToken });
+  return res.status(SUCCESS).json({ token: resultToken });
 });
 // end requeriment 3
 // requeriment 4 / Crie o endpoint POST /crush
-app.post('/crush', (req, res) => {
-  const token = req.headers.authentication;
-  console.log(token);
-  res.status(SUCCESS).json({ token: token });
+app.post('/crush', isToken, (req, res) => {
+  const newCrush = req.body;
+
+  isToken(req, res);
+
+  return res.status(SUCCESS).json({ message: "token validado" });
 });
 // end requeriment 4
 
