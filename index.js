6cd5716b2eb746209a528d1bc738be039b8b36e9
bynho25 <pablo.trybe@gmail.com>
2021-02-10 00:51:13 -0300

    Starting the project

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const listAllCrushs = require('./requests/listAllCrushs');
 
 const app = express();
 const SUCCESS = 200;
+const PORT = 3000;
+app.use(bodyParser.json());
+
+// --------------Requirement 1 --------------
+app.get('/crush', async (req, res) => {
+  const crushs = await listAllCrushs();
+  try {
+    res.status(200).json(JSON.parse(crushs));
+  } catch (err) {
+    console.log(err);
+    res.status(200).json([]);
+  }
+});
+
+// ---------------Requirement 2 -------------------
+app.get('/crush/:id', async (req, res) => {
+  const { id } = req.params;
+  const crush = await listAllCrushs();
+  const crushID = JSON.parse(crush).find((elem) => elem.id === parseInt(id, 10));
+
+  if (!crushID) res.status(404).json({ message: 'Crush não encontrado' });
+  res.status(200).json(crushID);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.listen(PORT, () => console.log('Executando'));
---
+++ requests/listAllCrushs.js
@@ -0,0 +1,9 @@
+const fs = require('fs').promises;
+const path = require('path').resolve;
+
+const listAllCrushs = async () => {
+  const file = await fs.readFile(path(__dirname, '..', 'crush.json'), 'utf-8');
+  return file;
+};
+
+module.exports = listAllCrushs;
