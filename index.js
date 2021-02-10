bb60a9c94a5587650cc3222778f1f76c90514a52
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2021-02-10 10:18:09 -0300

    modified verification crush id

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const fs = require('fs').promises;
 
-const crushes = 'crush.json';
+const crushData = 'crush.json';
 const crushResgistered = require('./crush.json');
 
 const app = express();
 const SUCCESS = 200;
+const NOTFOUND = 404;
 
 app.use(express.json());
 app.listen(3000, () => console.log('Executando'));
 
 app.get('/crush', async (_req, res) => {
-  const read = await fs.readFile(crushes);
-  if (read < 1) {
+  const readCrush = await fs.readFile(crushData);
+  if (readCrush < 1) {
     return res.status(SUCCESS).json(crushResgistered);
   }
-  return res.status(SUCCESS).json(JSON.parse(read));
+  return res.status(SUCCESS).json(JSON.parse(readCrush));
 });
 
 app.get('/crush/:id', async (req, res) => {
-  const read = await fs.readFile(crushes, 'utf-8');
-  const crushesJson = JSON.parse(read);
+  const readCrush = await fs.readFile(crushData, 'utf-8');
+  const crushDataJson = JSON.parse(readCrush);
   const { id } = req.params;
-  const verifyCrushId = crushesJson.find((crush) => crush.id === parseInt(id, 10));
+  const verifyCrushId = crushDataJson.find((crush) => crush.id === parseInt(id, 10));
   console.log(verifyCrushId);
-  if (verifyCrushId < 1) {
-    return res.status(404).json({ message: 'Crush não encontrado' });
+  if (!verifyCrushId) {
+    return res.status(NOTFOUND).send({ message: 'Crush não encontrado' });
   }
   return res.status(SUCCESS).send(verifyCrushId);
 });
