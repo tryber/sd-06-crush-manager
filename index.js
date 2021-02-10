546f9a71542c7d2f31d955ad99f8e66d21c922c7
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-02-10 20:25:39 -0300

    req 6

--- index.js
+++ index.js
@@ -201,4 +201,65 @@ app.put('/crush/:id', async (req, res) => {
   return res.status(404).send({ message: 'Crush não encontrado' });
 });
 
+// reqiosito 6
+
+app.delete('/crush/:id', async (req, res) => {
+  const crushes = await readFile('crush');
+  const token = req.headers.authorization;
+  const { name, age, date } = req.body;
+  const id = parseInt(req.params.id, 10);
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  if (!name) {
+    return res.status(400).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+  if (name.length < 4) {
+    return res.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+  if (!age || age === '') {
+    return res.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (age < 18) {
+    return res.status(400).json({
+      message: 'O crush deve ser maior de idade',
+    });
+  }
+  if (!date || date === '' || !date.datedAt || date.rate === undefined) {
+    return res.status(400).json({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }
+  if (date.rate > 5 || date.rate < 1) {
+    return res.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  if (dataValidate(date.datedAt)) {
+    return res.status(400).json({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  const crushesArray = JSON.parse(crushes);
+  const element = crushesArray.find((e) => e.id === id);
+
+  if (element) {
+    const elementIndex = crushesArray.findIndex((e) => e.id === id);
+    crushesArray.splice(elementIndex, 1);
+    writeFile('crush', JSON.stringify(crushesArray));
+    return res.status(200).json({ message: 'Crush deletado com sucesso' });
+  }
+  return res.status(404).send({ message: 'Crush não encontrado' });
+});
+
 app.listen(3000, () => console.log('running'));
