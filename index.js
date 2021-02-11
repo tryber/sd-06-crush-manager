b90e87e7a7de392e7277b1d212e4c5703b4668cc
Júlia Pôrto <juportob@gmail.com>
2021-02-11 17:00:28 -0300

    Requisito 4

--- index.js
+++ index.js
@@ -87,52 +87,57 @@ app.post('/crush', async (request, response) => {
 
 // ------- Requisito 5 --------
 
-app.put('/crush/:id', async (request, response) => {
-  const { name, age, date } = request.body;
-  const token = request.headers.authorization;
+// app.put('/crush/:id', async (request, response) => {
+//   const { name, age, date } = request.body;
+//   const token = request.headers.authorization;
 
-  if (!token) return response.status(401).json({ message: 'Token não encontrado' });
-  if (token.length !== 16) return response.status(401).json({ message: 'Token inválido' });
+//   if (!token) return response.status(401).json({ message: 'Token não encontrado' });
+//   if (token.length !== 16) return response.status(401).json({ message: 'Token inválido' });
 
-  if (!name) return response.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.length <= 3) return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+//   if (!name) return response.status(400).json({ message: 'O campo "name" é obrigatório' });
+//   if (name.length <= 3) return response.status(400)
+// .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
 
-  if (!age || age === '') return response.status(400).json({ message: 'O campo "age" é obrigatório' });
-  if (age < 18) return response.status(400).json({ message: 'O crush deve ser maior de idade' });
+//   if (!age || age === '') return response.status(400)
+// .json({ message: 'O campo "age" é obrigatório' });
+//   if (age < 18) return response.status(400).json({ message: 'O crush deve ser maior de idade' });
 
-  if (!date || date === '' || !date.datedAt || date.rate === undefined) {
-    return response.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
-  }
-  if (date.rate < 1 || date.rate > 5) return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  if (dateValid(date.datedAt)) return response.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+//   if (!date || date === '' || !date.datedAt || date.rate === undefined) {
+//     return response.status(400)
+// .json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+//   }
+//   if (date.rate < 1 || date.rate > 5) return response.status(400)
+// .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+//   if (dateValid(date.datedAt)) return response.status(400)
+//  .json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
 
-  const crushes = await readFile('crush');
-  const parsedCrushes = JSON.parse(crushes);
-  const crushId = parseInt(request.params.id, 10);
-  const crushesWithoutId = parsedCrushes.filter((crush) => crush.id !== crushId);
-  const crushEdited = ({ name, age, id: crushId, date });
-  crushesWithoutId.concat(crushEdited);
+//   const crushes = await readFile('crush');
+//   const parsedCrushes = JSON.parse(crushes);
+//   const crushId = parseInt(request.params.id, 10);
+//   const crushesWithoutId = parsedCrushes.filter((crush) => crush.id !== crushId);
+//   const crushEdited = ({ name, age, id: crushId, date });
+//   crushesWithoutId.concat(crushEdited);
 
-  await writeFile('crush', JSON.stringify(crushesWithoutId));
+//   await writeFile('crush', JSON.stringify(crushesWithoutId));
 
-  response.status(200).send(crushEdited);
-});
+//   response.status(200).send(crushEdited);
+// });
 
-// ------- Requisito 6 --------
+// // ------- Requisito 6 --------
 
-app.delete('/crush/:id', async (request, response) => {
-  const token = request.headers.authorization;
+// app.delete('/crush/:id', async (request, response) => {
+//   const token = request.headers.authorization;
 
-  if (!token) return response.status(401).json({ message: 'Token não encontrado' });
-  if (token.length !== 16) return response.status(401).json({ message: 'Token inválido' });
+//   if (!token) return response.status(401).json({ message: 'Token não encontrado' });
+//   if (token.length !== 16) return response.status(401).json({ message: 'Token inválido' });
 
-  const crushes = await readFile('crush');
-  const parsedCrushes = JSON.parse(crushes);
-  const crushId = parseInt(request.params.id, 10);
-  const crushToDelete = parsedCrushes.find((crush) => crush.id === crushId);
-  const newCrushesArray = parsedCrushes.filter((crush) => crush !== crushToDelete);
+//   const crushes = await readFile('crush');
+//   const parsedCrushes = JSON.parse(crushes);
+//   const crushId = parseInt(request.params.id, 10);
+//   const crushToDelete = parsedCrushes.find((crush) => crush.id === crushId);
+//   const newCrushesArray = parsedCrushes.filter((crush) => crush !== crushToDelete);
 
-  writeFile('crush', JSON.stringify(newCrushesArray));
+//   writeFile('crush', JSON.stringify(newCrushesArray));
 
-  response.status(200).json({ message: 'Crush deletado com sucesso' });
-});
+//   response.status(200).json({ message: 'Crush deletado com sucesso' });
+// });
