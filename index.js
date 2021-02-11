574afc7fc5a17c92af7bfb3a7b32a9d51ccb16b0
RenatoCostaDev <renatotrybe@gmail.com>
2021-02-11 10:39:59 -0300

    Requirement-4

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs').promises;
-const crypto = require('crypto');
-const { isEmail, isSize } = require('./utils');
+const { isEmail, isSize, receiveNewToken, isADate } = require('./utils');
 
 const app = express();
 const SUCCESS = 200;
@@ -29,6 +28,7 @@ app.get('/crush', async (_req, res) => {
 app.get('/crush/:id', async (req, res) => {
   const crushes = await fs.readFile(crushArray);
   const crushesJson = JSON.parse(crushes);
+  console.log(crushesJson);
   const { id } = req.params;
   const crush = crushesJson.find((crushJson) => crushJson.id === Number(id));
   if (!crush) {
@@ -55,8 +55,55 @@ app.post('/login', (req, res) => {
   if (isSize(password) < 6) {
     return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
-  // Zambelli plantão 10/02
-  const token = crypto.randomBytes(8).toString('hex');
+  const token = receiveNewToken();
   return res.status(200).json({ token });
 });
-app.listen(3000, () => console.log(`Using port: ${port}`));
+
+// Requisito-4
+// res deve adicionar um novo crush ao seu arquivo com token, name com 3 caracteres, age >= 18anos,
+// chave datedAt deve ser uma data no formato dd/mm/aaaa e obrigatório, rate de 1 a 5
+// e error tratado;
+app.post('/crush', async (req, res) => { // fazer função mid authorization
+  const { authorization } = req.headers;
+  // console.log(authorization);
+  const crushes = await fs.readFile(crushArray, 'utf-8');
+  const crushesJson = JSON.parse(crushes);
+  // postCrush
+  const newIdCrush = crushesJson.length + 1;
+  const postCrush = { ...req.body, id: newIdCrush };
+  const { name, age, date } = postCrush;
+  // token
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+  if (!date || date.datedAt === '' || date.rate === '' || date === undefined || date.datedAt === undefined || date.rate === undefined) {
+    return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (!isADate(date.datedAt)) {
+    return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (date.rate < 1 || date.rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  crushesJson.push(postCrush);
+  await fs.writeFile(crushArray, JSON.stringify(crushesJson));
+  return res.status(201).json(postCrush);
+});
+app.listen(3000, () => console.log(`Using port: ${port}`));// authorization 78afe01ee29f9cb9 (Header)
