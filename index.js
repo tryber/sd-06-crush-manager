d4f0d486c5adfe7e9d9bf36e6fda379088a409c3
kissyla <kissylamottapereira@gmail.com>
2021-02-10 22:54:47 -0300

    reorganizando rotas

--- index.js
+++ index.js
@@ -14,6 +14,39 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+app.get('/crush/:id', (req, res) => {
+  const file = fs.readFileSync('./crush.json', 'utf8');
+  const dataFiles = JSON.parse(file);
+  const { id } = req.params;
+  const crushes = dataFiles.filter((crush) => crush.id === parseInt(id, 10));
+  if (crushes.length !== 1) {
+    return res.status(404).send({ message: 'Crush não encontrado' });
+  }
+  return res.status(SUCCESS).send(crushes[0]);
+});
+
+app.get('/crush', (_req, res) => {
+  const file = fs.readFileSync('./crush.json', 'utf8');
+  res.json(JSON.parse(file));
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password || password === '') {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+  res.status(SUCCESS).json(token);
+});
+
 function tokenAutorization(req, res, next) {
   const { authorization } = req.headers;
   if (!authorization) {
@@ -64,35 +97,6 @@ function crushValidation(name, age, date) {
 //   res.status(SUCCESS).json(matchCrush);
 // });
 
-app.get('/crush/:id', (req, res) => {
-  const file = fs.readFileSync('./crush.json', 'utf8');
-  const dataFiles = JSON.parse(file);
-  const { id } = req.params;
-  const crushes = dataFiles.filter((crush) => crush.id === parseInt(id, 10));
-  if (crushes.length !== 1) {
-    return res.status(404).send({ message: 'Crush não encontrado' });
-  }
-  return res.status(SUCCESS).send(crushes[0]);
-});
-
-app.get('/crush', (_req, res) => {
-  const file = fs.readFileSync('./crush.json', 'utf8');
-  res.json(JSON.parse(file));
-});
-
-app.post('/crush', (req, res) => {
-  const { name, age, date } = req.body;
-  const invalidCrush = crushValidation(name, age, date);
-  if (invalidCrush) {
-    return res.status(400).json({ message: invalidCrush });
-  }
-  const cruchesFile = fs.readFileSync('./crush.json', 'utf8');
-  const crushes = JSON.parse(cruchesFile);
-  const newCrush = { name, age, id: crushes.length + 1, date };
-  crushes.push(newCrush);
-  res.status(201).json(newCrush);
-});
-
 app.put('/crush/:id', (req, res) => {
   const { name, age, date } = req.body;
   const invalidCrush = crushValidation(name, age, date);
@@ -114,24 +118,22 @@ app.delete('/crush/:id', (req, res) => {
   const crushes = JSON.parse(cruchesFile);
   const crushId = crushes.filter((crush) => crush.id === parseInt(id, 10));
   crushes.splice(crushId, 1, crushes);
+  fs.writeFileSync('./crush.json', 'utf8', (crushes));
   res.status(SUCCESS).json({ message: 'Crush deletado com sucesso' });
 });
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  if (!email || email === '') {
-    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+app.post('/crush', (req, res) => {
+  const { name, age, date } = req.body;
+  const invalidCrush = crushValidation(name, age, date);
+  if (invalidCrush) {
+    return res.status(400).json({ message: invalidCrush });
   }
-  if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  if (!password || password === '') {
-    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
-  }
-  res.status(SUCCESS).json(token);
+  const cruchesFile = fs.readFileSync('./crush.json', 'utf8');
+  const crushes = JSON.parse(cruchesFile);
+  const newCrush = { name, age, id: crushes.length + 1, date };
+  crushes.push(newCrush);
+  fs.writeFileSync('./crush.json', 'utf8', (crushes));
+  res.status(201).json(newCrush);
 });
 
 app.listen(3000, () => { console.log('porta: 3000 ativa'); });
