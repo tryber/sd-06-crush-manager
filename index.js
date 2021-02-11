e31ba801644d67e3b960071f0b64cd851ca11f99
Usuario <mguimaraes.carneiro@gmail.com>
2021-02-11 10:39:54 -0300

    Starting Project Crush Manager

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const SUCCESS = 200;
 
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+// first commit
