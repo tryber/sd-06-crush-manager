a8859d7c57976282b2c6b4a6f63599a65ce9200b
isabellavjs <isabellavjs@gmail.com>
2021-02-11 19:08:21 -0300

    Req 5

--- putRequest.js
+++ putRequest.js
@@ -8,14 +8,18 @@ const { modifyFile } = require('./utils/writeCrushData');
 app.use(bodyParser.json());
 
 async function editCrush(request, response) {
-  const { id } = request.params.id;
+  const { id } = request.params;
   const data = await parsedData();
   const crushId = data.findIndex((crush) => crush.id === parseInt(id, 10));
-  const { name, age, date } = request.body;
 
-  const updateCrush = { id: parseInt(id, 10), name, age, date };
-  if (crushId > 0) modifyFile(updateCrush);
-  console.log(updateCrush);
+  const { name, age, date } = request.body;
+  const parsedId = parseInt(id, 10);
+  const updateCrush = { id: parsedId, name, age, date };
+  const newCrushArray = data.concat({ name, age, id, date });
+  if (parsedId > 0) modifyFile(newCrushArray);
+  // console.log(typeof parsedId);
+  console.log(crushId);
+  // console.log(updateCrush);
 
   response.status(200).json(updateCrush);
 }
