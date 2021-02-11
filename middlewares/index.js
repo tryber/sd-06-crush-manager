8c8c29fb4f877fd86e9b142c25d5c65596fcf25b
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-02-11 16:27:24 -0300

    Desafio 3 - POST/login

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,13 +3,11 @@ const { readCrushFile, isValidEmail, isValidPassword } = require('../utils');
 
 const SUCCESS = 200;
 
-const standardResponse = (_request, response) => {
-  response.status(SUCCESS).send();
-};
+const standardResponse = (_request, response) => response.status(SUCCESS).send();
 
 const getCrushes = async (_req, res) => {
   const allCrushes = await readCrushFile();
-  res.status(SUCCESS).send(allCrushes);
+  return res.status(SUCCESS).send(allCrushes);
 };
 
 const getCrush = async (req, res) => {
@@ -17,28 +15,29 @@ const getCrush = async (req, res) => {
   const { id } = req.params;
   const crush = allCrushes.find((each) => each.id === +id);
   if (crush) {
-    res.status(SUCCESS).send(crush);
-  } else {
-    const message = 'Crush não encontrado';
-    res.status(404).send({ message });
+    return res.status(SUCCESS).send(crush);
   }
+  const message = 'Crush não encontrado';
+  return res.status(404).send({ message });
 };
 
 const getToken = async (req, res) => {
   const { email, password } = req.body;
-  if (!email) res.status(400).send({ message: 'O campo "email" é obrigatório' });
-  if (!password) res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
 
   if (isValidEmail(email)) {
     if (isValidPassword(password)) {
       const token = crypto.randomBytes(8).toString('hex');
-      res.status(200).send({ token });
-    } else {
-      res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+      return res.status(200).send({ token });
     }
-  } else {
-    res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+    return res.status(400).send({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
+  return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+};
+
+const addCrush = () => {
+
 };
 
 module.exports = {
@@ -46,4 +45,5 @@ module.exports = {
   getCrush,
   standardResponse,
   getToken,
+  addCrush,
 };
