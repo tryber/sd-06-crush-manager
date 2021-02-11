e1e1cc2929bf45603c191851f8b08cd5c54b369d
Isa Koga <isadorakoga710@gmail.com>
2021-02-11 14:47:53 -0400

    :rotating_light: Fix linter

--- editCrush.js
+++ editCrush.js
@@ -9,13 +9,13 @@ const updateCrush = async (crush, id) => {
   fs.writeFile('./crush.json', JSON.stringify(updateList), (err) => {
     if (err) throw new Error(err);
   });
-}
+};
 
 const editCrush = async (req, res) => {
   const id = parseInt(req.params.id, 10);
-  const editedCrush = {id, ...req.body };
+  const editedCrush = { id, ...req.body };
   updateCrush(editedCrush, id);
   res.status(200).json(editedCrush);
-}
+};
 
 module.exports = { editCrush };
