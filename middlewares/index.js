dd0aa7c7baf089d400876f73fa4d0d7210396406
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-02-11 19:54:57 -0300

    Desafio 4 - POST/crush

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,11 @@
 const crypto = require('crypto');
-const { readCrushFile, isValidEmail, isValidPassword } = require('../utils');
+const { readCrushFile, writeCrushFile, isValidEmail, isValidPassword, isValidDate, isValidRate } = require('../utils');
 
-const SUCCESS = 200;
-
-const standardResponse = (_request, response) => response.status(SUCCESS).send();
+const standardResponse = (_request, response) => response.status(200).send();
 
 const getCrushes = async (_req, res) => {
   const allCrushes = await readCrushFile();
-  return res.status(SUCCESS).send(allCrushes);
+  return res.status(200).send(allCrushes);
 };
 
 const getCrush = async (req, res) => {
@@ -15,7 +13,7 @@ const getCrush = async (req, res) => {
   const { id } = req.params;
   const crush = allCrushes.find((each) => each.id === +id);
   if (crush) {
-    return res.status(SUCCESS).send(crush);
+    return res.status(200).send(crush);
   }
   const message = 'Crush não encontrado';
   return res.status(404).send({ message });
@@ -36,8 +34,35 @@ const getToken = async (req, res) => {
   return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
 };
 
-const addCrush = () => {
+const addCrush = async (req, res) => {
+  const { authorization } = req.headers;
+  const { name, age, date } = req.body;
 
+  if (!authorization) return res.status(401).send({ message: 'Token não encontrado' });
+  if (authorization.length !== 16) return res.status(401).send({ message: 'Token inválido' });
+
+  if (!name) return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+  if (!age) return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+  if (name.length < 3) return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  if (age < 18) return res.status(400).send({ message: 'O crush deve ser maior de idade' });
+
+  if (date) {
+    const { datedAt, rate } = date;
+    if (!datedAt || !rate) return res.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    if (!isValidDate(datedAt)) return res.status(400).send({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+    if (!isValidRate(rate)) return res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  } else {
+    return res.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+
+  // const newId = crypto.randomBytes(4).toString('hex');
+
+  const allCrushes = await readCrushFile();
+  const newId = allCrushes.length + 1;
+  allCrushes.push({ name, age, id: newId, date });
+  console.log(allCrushes);
+  writeCrushFile(JSON.stringify(allCrushes));
+  return res.status(201).send({ id: newId, name, age, date });
 };
 
 module.exports = {
--- utils/index.js
+++ utils/index.js
@@ -7,12 +7,23 @@ const readCrushFile = async () => {
   return allCrushes;
 };
 
+const writeCrushFile = async (newData) => {
+  await fs.writeFile(`${path()}/crush.json`, newData, 'utf-8', (err) => console.log(err));
+};
+
 const isValidEmail = (email) => /\S+@\S+\.\S+/.test(email);
 
 const isValidPassword = (password) => (password.toString().length >= 6);
+
+const isValidDate = (date) => /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/.test(date);
+
+const isValidRate = (rate) => /^([1-5])$/.test(rate);
 
 module.exports = {
   readCrushFile,
   isValidEmail,
   isValidPassword,
+  isValidDate,
+  isValidRate,
+  writeCrushFile,
 };
