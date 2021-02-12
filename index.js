de54e9b441537a7a71ccee7bd67102f915a836b1
Vinicius Buzato <viniciusbuzato@gmail.com>
2021-02-12 00:35:33 -0300

    Desafio 4 - POST/crush - 31 testes passados

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { standardResponse, getCrushes, getCrush, getToken, addCrush } = require('./middlewares');
+const { standardResponse, getCrushes, getCrush, getToken, addCrush, updateCrush } = require('./middlewares');
 
 const app = express();
 const port = 3000;
@@ -11,8 +11,9 @@ app.use(bodyParser.json());
 app.get('/', standardResponse);
 
 app.get('/crush', getCrushes);
-app.get('/crush/:id', getCrush);
-app.post('/login', getToken);
 app.post('/crush', addCrush);
+app.get('/crush/:id', getCrush);
+app.put('/crush/:id', updateCrush);
+app.post('/login', getToken);
 
 app.listen(port);
--- middlewares/index.js
+++ middlewares/index.js
@@ -34,35 +34,62 @@ const getToken = async (req, res) => {
   return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
 };
 
-const addCrush = async (req, res) => {
-  const { authorization } = req.headers;
-  const { name, age, date } = req.body;
+const verifyToken = (token, res) => {
+  if (!token) return res.status(401).send({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).send({ message: 'Token inválido' });
+};
 
-  if (!authorization) return res.status(401).send({ message: 'Token não encontrado' });
-  if (authorization.length !== 16) return res.status(401).send({ message: 'Token inválido' });
-
+const verifyCrushPackage = (name, age, date, res) => {
+  if (name && name.length < 3) return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  if (age < 18) return res.status(400).send({ message: 'O crush deve ser maior de idade' });
   if (!name) return res.status(400).send({ message: 'O campo "name" é obrigatório' });
   if (!age) return res.status(400).send({ message: 'O campo "age" é obrigatório' });
-  if (name.length < 3) return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  if (age < 18) return res.status(400).send({ message: 'O crush deve ser maior de idade' });
 
   if (date) {
     const { datedAt, rate } = date;
-    if (!datedAt || !rate) return res.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+    if (!datedAt || (!rate && rate !== 0)) return res.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
     if (!isValidDate(datedAt)) return res.status(400).send({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
     if (!isValidRate(rate)) return res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   } else {
     return res.status(400).send({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   }
+};
 
-  // const newId = crypto.randomBytes(4).toString('hex');
+const addCrush = async (req, res) => {
+  const { name, age, date } = req.body;
+  const { authorization } = req.headers;
+
+  verifyToken(authorization, res);
+
+  verifyCrushPackage(name, age, date, res);
 
   const allCrushes = await readCrushFile();
   const newId = allCrushes.length + 1;
-  allCrushes.push({ name, age, id: newId, date });
+  allCrushes.push({ name, age, id: +newId, date });
   console.log(allCrushes);
-  writeCrushFile(JSON.stringify(allCrushes));
-  return res.status(201).send({ id: newId, name, age, date });
+  writeCrushFile(allCrushes);
+  return res.status(201).send({ id: +newId, name, age, date });
+};
+
+const updateCrush = async (req, res) => {
+  const id = +req.params.id;
+  const { name, age, date } = req.body;
+  const { authorization } = req.headers;
+
+  verifyToken(authorization, res);
+
+  verifyCrushPackage(name, age, date, res);
+
+  const allCrushes = await readCrushFile();
+  const newCrushesList = allCrushes.map((crush) => {
+    if (crush.id === id) {
+      return { name, age, id, date };
+    }
+    return crush;
+  });
+  console.log(newCrushesList);
+  writeCrushFile(newCrushesList);
+  return res.status(200).send({ id, name, age, date });
 };
 
 module.exports = {
@@ -71,4 +98,5 @@ module.exports = {
   standardResponse,
   getToken,
   addCrush,
+  updateCrush,
 };
