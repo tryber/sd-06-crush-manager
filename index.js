7be1e9e6c6b8ca0f014dcb9eb882fc83fe39a085
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-02-12 15:03:05 -0300

    organize code

--- index.js
+++ index.js
@@ -23,83 +23,12 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.route('/crush')
-  .get(async (request, response) => {
-    const fileName = './crush.json';
-    const rawData = await fs.readFile(fileName);
-    const crushes = JSON.parse(rawData);
-    response.status(SUCCESS).json(crushes);
-  })
-  .post(authenticate, async (request, response) => {
-    const requestBody = request.body;
-
-    if (!checkRequestField(requestBody, 'name')) {
-      return response.status(400).json(
-        {
-          message: 'O campo "name" é obrigatório',
-        },
-      );
-    }
-
-    if (!validateName(requestBody.name)) {
-      return response.status(400).json(
-        {
-          message: 'O "name" deve ter pelo menos 3 caracteres',
-        },
-      );
-    }
-
-    if (!checkRequestField(requestBody, 'age')) {
-      return response.status(400).json(
-        {
-          message: 'O campo "age" é obrigatório',
-        },
-      );
-    }
-
-    if (!validateAge(requestBody.age)) {
-      return response.status(400).json(
-        {
-          message: 'O crush deve ser maior de idade',
-        },
-      );
-    }
-
-    if (!datingValidation(requestBody.date)) {
-      return response.status(400).json(
-        {
-          message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
-        },
-      );
-    }
-
-    if (!validateDate(requestBody.date.datedAt)) {
-      return response.status(400).json(
-        {
-          message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
-        },
-      );
-    }
-
-    if (!rateValidation(requestBody.date.rate)) {
-      return response.status(400).json(
-        {
-          message: 'O campo "rate" deve ser um inteiro de 1 à 5',
-        },
-      );
-    }
-
-    const fileName = './crush.json';
-    const rawData = await fs.readFile(fileName);
-    const crushes = JSON.parse(rawData);
-    const lastIndex = crushes.length - 1;
-    const lastId = crushes[lastIndex].id;
-    const nextId = lastId + 1;
-    const newCrush = { ...requestBody, id: nextId };
-    crushes.push(newCrush);
-    await fs.writeFile(fileName, JSON.stringify(crushes));
-    response.status(201).json(newCrush);
-  });
+app.get('/crush', async (request, response) => {
+  const fileName = './crush.json';
+  const rawData = await fs.readFile(fileName);
+  const crushes = JSON.parse(rawData);
+  response.status(SUCCESS).json(crushes);
+});
 
 app.get('/crush/:id', async (request, response) => {
   const id = parseInt(request.params.id, 10);
@@ -145,7 +74,7 @@ app.post('/login', (request, response) => {
   );
 });
 
-/* app.post('/crush', authenticate, async (request, response) => {
+app.post('/crush', authenticate, async (request, response) => {
   const requestBody = request.body;
 
   if (!checkRequestField(requestBody, 'name')) {
@@ -214,7 +143,7 @@ app.post('/login', (request, response) => {
   crushes.push(newCrush);
   await fs.writeFile(fileName, JSON.stringify(crushes));
   response.status(201).json(newCrush);
-}); */
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
