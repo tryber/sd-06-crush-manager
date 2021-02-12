eaa44f878981563d6163c796f6885bfd2411fa48
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-02-12 14:45:48 -0300

    fix: correct typo

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,7 +4,7 @@ module.exports = function authenticate(request, response, next) {
   const requestHeader = request.headers;
 
   if (!checkRequestField(requestHeader, 'authorization')) {
-    return response.status(401).send(
+    return response.status(401).json(
       {
         message: 'Token não encontrado',
       },
@@ -12,7 +12,7 @@ module.exports = function authenticate(request, response, next) {
   }
 
   if (!validateToken(requestHeader.authorization)) {
-    return response.status(401).send(
+    return response.status(401).json(
       {
         message: 'Token inválido',
       },
