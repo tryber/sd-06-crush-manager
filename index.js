c3cd6d1b1ed5b48e42dba9bd0524aef32260056d
afornagieri <fornagieri.artur@gmail.com>
2021-02-13 12:07:16 -0300

    Primeiro requisito

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const { response } = require('express');
 const express = require('express');
+const crushJson = require('./crush.json');
 
 const app = express();
 const SUCCESS = 200;
@@ -6,4 +8,14 @@ const SUCCESS = 200;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+
+app.get('/crush', (req, res) => {
+  res.statusCode === 200 ? res.send(crushJson) : [];
+});
+
+
+app.listen(3000, () => {
+  console.log('ouvindo a porta 3001');
 });
