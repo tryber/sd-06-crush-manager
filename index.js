a764b7a4cdb16dcbc0975a8e67474cfebaf1288f
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-02-13 19:06:37 -0300

    feat: finish modify crush route

--- index.js
+++ index.js
@@ -145,6 +145,81 @@ app.post('/crush', authenticate, async (request, response) => {
   response.status(201).json(newCrush);
 });
 
+app.put('/crush/:id', authenticate, async (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  const requestBody = request.body;
+
+  if (!checkRequestField(requestBody, 'name')) {
+    return response.status(400).json(
+      {
+        message: 'O campo "name" é obrigatório',
+      },
+    );
+  }
+
+  if (!validateName(requestBody.name)) {
+    return response.status(400).json(
+      {
+        message: 'O "name" deve ter pelo menos 3 caracteres',
+      },
+    );
+  }
+
+  if (!checkRequestField(requestBody, 'age')) {
+    return response.status(400).json(
+      {
+        message: 'O campo "age" é obrigatório',
+      },
+    );
+  }
+
+  if (!validateAge(requestBody.age)) {
+    return response.status(400).json(
+      {
+        message: 'O crush deve ser maior de idade',
+      },
+    );
+  }
+
+  if (!datingValidation(requestBody.date)) {
+    return response.status(400).json(
+      {
+        message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+      },
+    );
+  }
+
+  if (!validateDate(requestBody.date.datedAt)) {
+    return response.status(400).json(
+      {
+        message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+      },
+    );
+  }
+
+  if (!rateValidation(requestBody.date.rate)) {
+    return response.status(400).json(
+      {
+        message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+      },
+    );
+  }
+
+  const fileName = './crush.json';
+  const rawData = await fs.readFile(fileName);
+  const crushes = JSON.parse(rawData);
+  const retrievedCrush = crushes.find((crush) => crush.id === id);
+  if (!retrievedCrush) response.status(404).json({ message: 'Crush não encontrado' });
+  const newCrushes = crushes.map((crush) => {
+    if (crush.id === id) {
+      return requestBody;
+    }
+    return crush;
+  });
+  await fs.writeFile(fileName, JSON.stringify(newCrushes));
+  response.status(201).json(requestBody);
+});
+
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
 });
