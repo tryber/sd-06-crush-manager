a1ee17530a9bdc1d2e33baced02b65941423aecb
afornagieri <fornagieri.artur@gmail.com>
2021-02-13 13:47:39 -0300

    Req 1 fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { response } = require('express');
+// const { response } = require('express');
 const express = require('express');
 const crushJson = require('./crush.json');
+// const token = require('./tokenMUITOSEGURO_E_COMPLEXO.json');
 
 const app = express();
 const SUCCESS = 200;
@@ -10,12 +11,28 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-
 app.get('/crush', (req, res) => {
-  res.statusCode === 200 ? res.send(crushJson) : [];
+  if (res.statusCode === 200) {
+    res.send(JSON.parse(crushJson));
+  }
+  return [];
 });
 
+// app.get('/crush/:id', (req, res) => {
+//   const { ParamID } = req.params;
+//   const checkIfTheUserExists = crushJson.some((id) => ParamID === id);
+//   if (checkIfTheUserExists) {
+//     crushJson.find((id) => id === ParamID);
+//   }
+//   res.send({
+//     message: 'Crush não encontrado',
+//   });
+// });
+
+// app.post('/crush/:id', (req, res) => {
+
+// });
 
 app.listen(3000, () => {
-  console.log('ouvindo a porta 3001');
+  console.log('ouvindo a porta 3000');
 });
