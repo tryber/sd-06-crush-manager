135deac3810f7d86b128217f51e039c8dcb16419
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-14 06:53:33 -0300

    [Estrutura inicial] app.listen() Para o servidor escutar as requisições

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const SUCCESS = 200;
 // body-parser para leituras de arquivos json (vindos, por exemplo, do body)
 app.use(bodyParser.json());
 
+// "A função app.listen() do Express inicia um socket UNIX e escuta as conexões
+// em um caminho fornecido." [https://pt.stackoverflow.com/questions/228944/qual-a-fun%C3%A7%C3%A3o-do-app-listen-no-express]
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Roz, sweet dreams are made... on PORT ${PORT} <3`));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
