6a2d9e5b94dad7a69ff60201e6fe75eb94aa6b45
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-14 06:39:10 -0300

    [Estrutura inicial] acrescenta o body-parser o body-parser lê os arquivos json recebidos.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
 const SUCCESS = 200;
+
+// app.use(); garante que todas as requisições passarão por ele
+// body-parser para leituras de arquivos json (vindos, por exemplo, do body)
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
