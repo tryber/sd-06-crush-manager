4c067336c76eae384ce9f62f7dcca2f4dcf1be38
afornagieri <fornagieri.artur@gmail.com>
2021-02-14 21:02:42 -0300

    Requisitos 1,2,3 - Testando se passa no avaliador.

--- index.js
+++ index.js
@@ -1,37 +1,66 @@
-// const { response } = require('express');
 const express = require('express');
-const crushJson = require('./crush.json');
-// const token = require('./tokenMUITOSEGURO_E_COMPLEXO.json');
+const fs = require('fs');
+const crypto = require('crypto');
 
 const app = express();
+app.use(express.json());
+
 const SUCCESS = 200;
 
-// não remova esse endpoint, e para o avaliador funcionar
+// Necessário para o avaliador
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', (req, res) => {
-  if (res.statusCode === 200) {
-    res.send(JSON.parse(crushJson));
+app.get('/crush', async (req, res) => {
+  if (res.statusCode === SUCCESS) {
+    res.send(JSON.parse(fs.readFileSync('./crush.json')));
   }
-  return [];
 });
 
-// app.get('/crush/:id', (req, res) => {
-//   const { ParamID } = req.params;
-//   const checkIfTheUserExists = crushJson.some((id) => ParamID === id);
-//   if (checkIfTheUserExists) {
-//     crushJson.find((id) => id === ParamID);
-//   }
-//   res.send({
-//     message: 'Crush não encontrado',
-//   });
-// });
+app.get('/crush/:id', async (req, res) => {
+  const { id: paramID } = req.params;
+  const file = await fs.readFileSync('./crush.json');
+  const checkIfTheUserExists = file.some((id) => id === Number(paramID));
 
-// app.post('/crush/:id', (req, res) => {
+  if (!checkIfTheUserExists) res.status(404).json({ message: 'Crush não encontrado' });
 
-// });
+  const response = JSON.parse(file).find((people) => people.id === Number(paramID));
+  res.status(200).json(response);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
+
+  if (!email) {
+    return res.status(404).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!emailFormat.test(email)) {
+    return res.status(404).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    return res.status(404).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(404).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+
+  return new Promise((fulfill, reject) => {
+    crypto.randomBytes(8, (error, buf) => {
+      if (error) {
+        reject(error);
+      } else {
+        const GeneratedToken = buf.toString('hex');
+        fulfill(GeneratedToken);
+        res.status(200).json({ token: GeneratedToken });
+      }
+    });
+  });
+});
 
 app.listen(3000, () => {
   console.log('ouvindo a porta 3000');
