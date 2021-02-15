954864673a4ba74c292606686da4184185bb021a
Ana Higo <anahigo.arq@gmail.com>
2021-02-15 19:07:14 -0300

    Desafio 1

--- putRequest.js
+++ putRequest.js
@@ -1,13 +1,5 @@
-const fs = require('fs').promises;
-const path = require('path');
+// module.exports = {
+//   async edit(request, response) {
 
-const readCrushFile = async () => {
-  const content = await fs.readFile(path.resolve(__dirname, '.', 'crush.json'));
-  return JSON.parse(content.toString('utf-8'));
-};
-
-module.exports = {
-  async edit(request, response) {
-
-  },
-};
+//   },
+// };
