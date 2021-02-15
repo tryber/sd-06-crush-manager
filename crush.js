e8297fa45e23e909b607e20312df9e3f0dae328b
FlavioSugano <flavio.sugano@gmail.com>
2021-02-15 15:33:15 -0300

    Requisito 2

--- crush.js
+++ crush.js
@@ -10,7 +10,21 @@ const getAllCrushes = async (_req, res) => {
 };
 
 const getCrushById = async (req, res) => {
+  const { id } = req.params;
 
+  readFilePromise(file)
+    .then((content) => {
+      res.status(200);
+
+      return JSON.parse(content);
+    })
+    .then((json) => {
+      const selectedCrush = json.find((crush) => Number(crush.id) === Number(id));
+
+      if (!selectedCrush) return res.status(404).send({ message: 'Crush não encontrado' });
+      return res.send(selectedCrush);
+    })
+    .catch((error) => console.log(error));
 };
 
 module.exports = {
--- routes.js
+++ routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const routes = express.Router();
-const { getAllCrushes } = require('./crush');
+const { getAllCrushes, getCrushById } = require('./crush');
 
 routes.get('/', (_request, response) => {
   response.status(200).send('Crush Manager - Rota /');
 });
 
 routes.get('/crush', getAllCrushes);
+
+routes.get('/crush/:id', getCrushById);
 
 module.exports = routes;
