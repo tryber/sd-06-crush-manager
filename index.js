58c8190a484368db2d0379aa778bb826ca9c427c
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-02-15 13:38:39 -0300

    requisito 5

--- index.js
+++ index.js
@@ -98,10 +98,10 @@ app.post('/crush', async (req, res) => {
   if (age < 18) {
     return res.status(BADREQUEST).json({ message: 'O crush deve ser maior de idade' });
   }
-  if (date === undefined || !date || date.datedAt === '' || date.datedAt === undefined || date.rate === '' || date.rate === undefined) {
+  if (!date || date.datedAt === '' || !date.datedAt || date.rate === '' || !date.rate) {
     return res.status(BADREQUEST).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   }
-  if (regexDate.test(date.datedAt) === false) {
+  if (!regexDate.test(date.datedAt)) {
     return res.status(BADREQUEST).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   }
   if (date.rate < 1 || date.rate > 5) {
@@ -110,4 +110,47 @@ app.post('/crush', async (req, res) => {
 
   await fs.writeFile(fileName, JSON.stringify(newCrush));
   return res.status(DONNE).json({ id: parsedJson.length + 1, ...req.body });
+});
+
+app.put('/crush/:id', async (req, res) => {
+  const { name, age, date } = req.body;
+  const { authorization } = req.headers;
+  const { id } = req.params;
+  const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+
+  const file = await fs.readFile(fileName, 'utf-8');
+  const parsedJson = JSON.parse(file);
+
+  if (!authorization) {
+    return res.status(ERROR).json({ message: 'Token não encontrado' });
+  }
+  if (authorization.length < 16) {
+    return res.status(ERROR).json({ message: 'Token inválido' });
+  }
+  if (!name || name === '') {
+    return res.status(BADREQUEST).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(BADREQUEST).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  if (!age || age === '') {
+    return res.status(BADREQUEST).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(BADREQUEST).json({ message: 'O crush deve ser maior de idade' });
+  }
+  if (!date || date.datedAt === '' || !date.datedAt || date.rate === '' || date.rate === undefined) {
+    return res.status(BADREQUEST).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  }
+  if (!regexDate.test(date.datedAt)) {
+    return res.status(BADREQUEST).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (date.rate < 1 || date.rate > 5) {
+    return res.status(BADREQUEST).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  const edit = { id: +id, name, age, date };
+  const editedCrush = [...parsedJson, edit]
+  await fs.writeFile(fileName, JSON.stringify(editedCrush));
+  return res.status(SUCCESS).json(edit);
 });
