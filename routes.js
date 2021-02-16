0e9becef8d3df1447bcc6c4d0046c39a0bd18a4b
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-15 22:17:39 -0300

    cria função para obter crush pelo id

--- routes.js
+++ routes.js
@@ -3,13 +3,24 @@ const express = require('express');
 const routes = express.Router();
 const fs = require('fs');
 
+const contatinhos = fs.readFileSync('./crush.json');
 // vai rotear os endpoints que se quer acessar
 // POST, GET, PUT, DELETE
 routes.get('/crush', (_request, response) => response.status(200).send(contatinhos));
 
+// Requisito 2 -> GET /crush/:id
+
+routes.get('/crush/:id', (request, response) => {
+  const { id } = request.params;
+
+  const listaContatinhos = JSON.parse(contatinhos);
+  const neo = listaContatinhos.filter((contatinho) => contatinho.id === parseInt(id, 10));
+
+  if (neo.length) {
+    return response.status(200).json(neo);
   }
 
-  return response.status(200).send(contatinhos);
+  return response.status(404).json({ message: 'Crush não encontrado' });
 });
 
 module.exports = routes;
