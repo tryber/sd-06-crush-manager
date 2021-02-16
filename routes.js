e0381aa8a681655443da16de4552b5c63e5129f3
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-15 22:16:53 -0300

    altera função em /crush

--- routes.js
+++ routes.js
@@ -5,10 +5,8 @@ const fs = require('fs');
 
 // vai rotear os endpoints que se quer acessar
 // POST, GET, PUT, DELETE
-routes.get('/crush', (_request, response) => {
-  let contatinhos = fs.readFileSync('./crush.json');
-  if (!contatinhos) {
-    contatinhos = [];
+routes.get('/crush', (_request, response) => response.status(200).send(contatinhos));
+
   }
 
   return response.status(200).send(contatinhos);
