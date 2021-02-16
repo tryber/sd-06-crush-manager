1c14d53f817a4303ec0b45409a35bbf8038f4af2
Daniel Moura <darthur.moura@gmail.com>
2021-02-16 20:57:06 -0300

    Futile attempts to pass req 4

--- index.js
+++ index.js
@@ -25,6 +25,51 @@ async function checkPass(pass) {
   return true;
 }
 
+function authenticate(request, response, next) {
+  const { authorization } = request.headers;
+  const msgs = {
+    noToken: 'Token não encontrado',
+    invalidToken: 'Token inválido',
+  };
+
+  if (!authorization) return response.status(401).json(msgs.noToken);
+  if (authorization && authorization.length != 16) return response.status(401).json(msgs.invalidToken);
+
+  next();
+}
+
+function checkNewCrush(request, response, next) {
+  const msgs = {
+    noName: 'O campo "name" é obrigatório',
+    noAge: 'O campo "age" é obrigatório',
+    noDate:
+      'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    invalidName: 'O "name" deve ter pelo menos 3 caracteres',
+    invalidAge: 'O crush deve ser maior de idade',
+    invalidDated: 'O campo "datedAt" deve ter o formato "dd/mm/aaa"',
+    invalidRate: 'O campo "rate" deve ser um inteiro de 1 à 5',
+  };
+
+  const {
+    name,
+    age,
+    date,
+  } = request.body;
+
+  // const regEx = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
+  const validDate = moment(date.datedAt, 'DD/MM/YYYY', true).isValid();
+
+  if (!name) return response.status(400).json(msgs.noName);
+  if (!age) return response.status(400).json(msgs.noAge);
+  if (!date || !date.datedAt || !date.rate) return response.status(400).json(msgs.noDate);
+  if (name.length < 3) return response.status(400).json(msgs.invalidName);
+  if (age < 18) return response.status(400).json(msgs.invalidAge);
+  if (!validDate) return response.status(400).json(msgs.invalidDated);
+  if (date.rate < 1 || date.rate > 5) return response.status(400).json(msgs.invalidRate);
+
+  next();
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
@@ -62,6 +107,15 @@ app.post('/login', async (request, response) => {
   if (!password) return response.status(400).json(msgs.noPassword);
   if (!validPass) return response.status(400).json(msgs.invalidPassword);
   if (!validEmail) return response.status(400).json(msgs.invalidEmail);
+});
+
+app.post('/crush', authenticate, checkNewCrush, async (request, response) => {
+  // const { name, date, age } = request.body;
+  const data = await getCrushes();
+  const newCrush = { ...request.body };
+  data.push(newCrush);
+  fs.writeFileSync('./crush.json', data);
+  response.status(201).json(data);
 });
 
 app.listen(port, () => console.log(`Listening to port ${port}!`));
