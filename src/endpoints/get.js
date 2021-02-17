49596a2a0aa11504842d24c3bdd04f5e79678a31
Usuario <mguimaraes.carneiro@gmail.com>
2021-02-17 15:27:54 -0300

    Requitements 01 and 02

--- src/endpoints/get.js
+++ src/endpoints/get.js
@@ -24,7 +24,7 @@ module.exports = {
     if (selectedCrush.length >= minimumLength) {
       res.status(SUCCESS).send(selectedCrush[firstIndex]);
     } else {
-      res.status(NOT_FOUND).json({ message: 'Crush not found.' });
+        res.status(NOT_FOUND).json({ message: 'Crush não encontrado' });
     }
   },
 };
