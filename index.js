e8e12bca0381e965d40686090d03625130e0cdb1
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-17 14:06:39 -0300

    em post('/crush) 1) o campo date é obrigatório e nenhuma de suas chaves deve vir vazia 2) o campo rate é um inteiro entre 1 e 5 3) o campo datedAt respeita o formato dd/mm/aaaa

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const routes = require('./routes');
 const app = express();
 const SUCCESS = 200;
 
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(SUCCESS).send();
+});
+
 // app.use(); garante que todas as requisições passarão por ele
 // body-parser para leituras de arquivos json (vindos, por exemplo, do body)
 app.use(bodyParser.json());
@@ -16,8 +21,3 @@ app.use(routes);
 // em um caminho fornecido." [https://pt.stackoverflow.com/questions/228944/qual-a-fun%C3%A7%C3%A3o-do-app-listen-no-express]
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Roz, sweet dreams are made... on PORT ${PORT} <3`));
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(SUCCESS).send();
-});
--- routes.js
+++ routes.js
@@ -112,6 +112,31 @@ routes.post('/crush', (request, response) => {
       message: 'O crush deve ser maior de idade',
     });
   }
+
+  // testa se o campo date é obrigatório e nenhuma de suas chaves deve vir vazia
+  if (!date || !date.datedAt || !date.rate) {
+    return response.status(400).send({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  // testa se o numero em rate é um inteiro [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger]
+  // e se é um valor entre 1 e 5
+  if (!Number.isInteger(date.rate) || date.rate < 1 || date.rate > 5) {
+    return response.status(400).send({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+
+  // testa se o campo datedAt respeita o formato dd/mm/aaaa
+  // regex para Datas [https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy]
+  // com algumas adaptações
+  const regexDateFormat = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+  if (!regexDateFormat.test(date.datedAt)) {
+    return response.status(400).send({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
 });
 
 module.exports = routes;
