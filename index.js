ea2d47364564fb77b05ef9ad584a4159053c5ac9
Leandro <leandro.feitoza8018@gmail.com>
2021-02-18 20:19:57 -0300

    requisito 3

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const readFile = require('./manageFiles');
+const geraToken = require('./generateToken');
 
 const app = express();
 const SUCCESS = 200;
 const PORT = 3000;
+
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -22,6 +26,21 @@ app.get('/crush/:id', async (req, res) => {
   const filteredID = result.find((crush) => crush.id === parseInt(id, 10));
   if (filteredID === undefined) return res.status(404).send({ message: 'Crush não encontrado' });
   res.status(200).send(filteredID);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === '' || !email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+
+  const emailMask = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+  if (emailMask.test(email) === false) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+
+  if (password === '' || !password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (password.length < 6) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+
+  const token = geraToken();
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => console.log('Server rolando na porta %s', PORT));
