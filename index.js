458ef8364530c94d0fc0a767e51ee55fbda50103
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-02-17 23:00:39 -0300

    estrutura

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const SUCCESS = 200;
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(SUCCESS).send();
+app.get('/', (_request, res) => {
+  // response.status(SUCCESS).send();
+  res.send('ok');
 });
+
+app.listen(3000);
