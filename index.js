e69d1b807603b926bca8367f579bb6eb9898a232
Leandro <leandro.feitoza8018@gmail.com>
2021-02-18 17:31:06 -0300

    Retomando o projeto do zero.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
-const readFile = require('./manageFiles');
-const geraToken = require('./generateToken');
 
 const app = express();
 const SUCCESS = 200;
@@ -11,36 +9,5 @@ const port = 3000;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
-
-app.get('/crush', async (req, res) => {
-  const result = await readFile();
-  if (result === []) return res.status(200).send('[]');
-  res.status(SUCCESS).send(result);
-});
-
-app.get('/crush/:id', async (req, res) => {
-  const { id } = req.params;
-  const result = await readFile();
-  const filteredID = result.find((crush) => crush.id === parseInt(id, 10));
-  if (filteredID === undefined) return res.status(404).send({ message: 'Crush não encontrado' });
-  res.status(200).send(filteredID);
-});
-
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  if (email === '') return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  const emailMask = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-  if (emailMask.test(email) === false) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  if (password === '') return res.status(400).json({ message: 'O campo "password" é obrigatório' });
-  if (password.length < 6) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
-
-  const token = geraToken();
-
-  res.status(200).json({ token });
-});
-
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 app.listen(port, () => console.log(`Server ouvindo na porta ${port}`));
--- manageFiles.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs').promises;
-const path = require('path');
-
-const readFile = async () => {
-  const fileCrush = await fs.readFile(path.resolve(__dirname, '.', './crush.json'));
-  return JSON.parse(fileCrush.toString('utf-8'));
-};
-
-module.exports = readFile;
