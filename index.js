1ad520118f7f32c55645f0743e4cb77c4fd87ec4
Daniel Moura <darthur.moura@gmail.com>
2021-02-17 21:09:36 -0300

    Req 4: succesfully implemented (local tests)

--- index.js
+++ index.js
@@ -26,28 +26,28 @@ async function checkPass(pass) {
 }
 
 function authenticate(request, response, next) {
-  const { authorization } = request.headers;
+  const auth = request.headers.authorization;
   const msgs = {
-    noToken: 'Token não encontrado',
-    invalidToken: 'Token inválido',
+    noToken: { message: 'Token não encontrado' },
+    invalidToken: { message: 'Token inválido' },
   };
 
-  if (!authorization) return response.status(401).json(msgs.noToken);
-  if (authorization && authorization.length != 16) return response.status(401).json(msgs.invalidToken);
+  if (!auth) return response.status(401).json(msgs.noToken);
+  if (auth && auth.length !== 16) return response.status(401).json(msgs.invalidToken);
 
   next();
 }
 
 function checkNewCrush(request, response, next) {
   const msgs = {
-    noName: 'O campo "name" é obrigatório',
-    noAge: 'O campo "age" é obrigatório',
+    noName: { message: 'O campo "name" é obrigatório' },
+    noAge: { message: 'O campo "age" é obrigatório' },
     noDate:
-      'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
-    invalidName: 'O "name" deve ter pelo menos 3 caracteres',
-    invalidAge: 'O crush deve ser maior de idade',
-    invalidDated: 'O campo "datedAt" deve ter o formato "dd/mm/aaa"',
-    invalidRate: 'O campo "rate" deve ser um inteiro de 1 à 5',
+      { message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' },
+    invalidName: { message: 'O "name" deve ter pelo menos 3 caracteres' },
+    invalidAge: { message: 'O crush deve ser maior de idade' },
+    invalidDated: { message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' },
+    invalidRate: { message: 'O campo "rate" deve ser um inteiro de 1 à 5' },
   };
 
   const {
@@ -56,8 +56,8 @@ function checkNewCrush(request, response, next) {
     date,
   } = request.body;
 
-  // const regEx = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
-  const validDate = moment(date.datedAt, 'DD/MM/YYYY', true).isValid();
+  const regEx = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+  const validDate = date && regEx.test(date.datedAt);
 
   if (!name) return response.status(400).json(msgs.noName);
   if (!age) return response.status(400).json(msgs.noAge);
@@ -92,9 +92,9 @@ app.get('/crush/:id', async (request, response) => {
 
 app.post('/login', async (request, response) => {
   const { email, password } = request.body;
-  console.log(email, password);
   const validEmail = await checkEmail(email);
   const validPass = await checkPass(password);
+  request.body = { token };
   const msgs = {
     noEmail: { message: 'O campo "email" é obrigatório' },
     invalidEmail: { message: 'O "email" deve ter o formato "email@email.com"' },
@@ -102,7 +102,7 @@ app.post('/login', async (request, response) => {
     invalidPassword: { message: 'A "senha" deve ter pelo menos 6 caracteres' },
   };
 
-  if (validEmail && validPass) return response.status(SUCCESS).json({ token });
+  if (validEmail && validPass) return response.status(SUCCESS).json(request.body);
   if (!email) return response.status(400).json(msgs.noEmail);
   if (!password) return response.status(400).json(msgs.noPassword);
   if (!validPass) return response.status(400).json(msgs.invalidPassword);
@@ -110,12 +110,18 @@ app.post('/login', async (request, response) => {
 });
 
 app.post('/crush', authenticate, checkNewCrush, async (request, response) => {
-  // const { name, date, age } = request.body;
-  const data = await getCrushes();
-  const newCrush = { ...request.body };
-  data.push(newCrush);
-  fs.writeFileSync('./crush.json', data);
-  response.status(201).json(data);
+  try {
+    const data = await getCrushes();
+    const ids = await data.map((crush) => crush.id);
+    const maxId = await ids[ids.length - 1];
+    const newCrush = { ...request.body };
+    newCrush.id = maxId + 1;
+    data.push(newCrush);
+    fs.writeFile('./crush.json', JSON.stringify(data));
+    response.status(201).json(newCrush);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 app.listen(port, () => console.log(`Listening to port ${port}!`));
