11a537d6d96b90837ea4372b7dd5e1dda3c5bfb7
Leandro <leandro.feitoza8018@gmail.com>
2021-02-18 19:58:44 -0300

    desafio 2 :/\

--- index.js
+++ index.js
@@ -16,4 +16,12 @@ app.get('/crush', async (req, res) => {
   res.status(200).send(result);
 });
 
+app.get('/crush/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await readFile();
+  const filteredID = result.find((crush) => crush.id === parseInt(id, 10));
+  if (filteredID === undefined) return res.status(404).send({ message: 'Crush não encontrado' });
+  res.status(200).send(filteredID);
+});
+
 app.listen(PORT, () => console.log('Server rolando na porta %s', PORT));
