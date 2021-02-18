07c86340bdb8c132edbc5cb8170501c44244a905
Leandro <leandro.feitoza8018@gmail.com>
2021-02-18 18:02:18 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs').promises;
 // const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,6 +9,12 @@ const port = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', async (_req, res) => {
+  const file = await fs.readFile('./crush.json', 'utf-8');
+  if (file.length === 0) return res.status(200).send('[]');
+  res.status(200).send(file);
 });
 
 app.listen(port, () => console.log(`Server ouvindo na porta ${port}`));
