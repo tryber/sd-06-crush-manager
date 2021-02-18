fca62f6c04e59ae6b7ab2fd4da43b73af8ad50cd
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-18 14:33:44 -0300

    cria endpoint PUT /crush/:id também altera averificação do date.rate para não incluir rates iguais a zero remove console.log da linha 35 Mais uma vez gratidão ao Rafa Ponciano que deu as orientações sem spoilers de como executar esse requisito :balloon:

--- routes.js
+++ routes.js
@@ -32,7 +32,7 @@ routes.get('/crush/:id', async (request, response) => {
   const listaContatinhos = await lerArquivo();
   const neoTheChosenOne = listaContatinhos
     .filter((contatinho) => contatinho.id === parseInt(id, 10));
-  console.log(neoTheChosenOne);
+
   if (neoTheChosenOne.length > 0) {
     return response.status(200).json(neoTheChosenOne[0]);
   }
@@ -128,7 +128,7 @@ routes.post('/crush', async (request, response) => {
   }
 
   // testa se o campo date é obrigatório e nenhuma de suas chaves deve vir vazia
-  if (!date || !date.datedAt || !date.rate) {
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
     return response.status(400).send({
       message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
     });
@@ -157,6 +157,89 @@ routes.post('/crush', async (request, response) => {
   const contatinhoCriado = { id: listaContatinhos.length + 1, name, age, date };
   await escreverArquivo(JSON.stringify([...listaContatinhos, contatinhoCriado], 0, 2));
   return response.status(201).send(contatinhoCriado);
+});
+
+// cria endpoint PUT /crush:id (req5)
+routes.put('/crush/:id', async (request, response) => {
+  const { authorization } = request.headers;
+  const { name, age, date } = request.body;
+  // precisa do id para comparar, encontrar e "alterar" o contatinho
+  const { id } = request.params;
+  const idPut = parseInt(id, 10);
+  console.log('recebe id:', idPut);
+
+  // testa se existe token
+  if (authorization === '' || !authorization) {
+    return response.status(401).send({
+      message: 'Token não encontrado',
+    });
+  }
+  // testa se o token é válido (como o token é válido? Se tiver length === 16)
+  if (authorization.length !== 16) {
+    return response.status(401).send({
+      message: 'Token inválido',
+    });
+  }
+  // testa se o campo name não é passado ou é vazio
+  if (name === '' || !name) {
+    return response.status(400).send({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+
+  // testa se o campo name não tem pelo menso 3 caracteres
+  if (name.length < 3) {
+    return response.status(400).send({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
+  // testa se o campo age não é passado ou é vazio
+  if (age === '' || !age) {
+    return response.status(400).send({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+
+  // testa se o campo age é menor do que 18
+  if (age < 18) {
+    return response.status(400).send({
+      message: 'O crush deve ser maior de idade',
+    });
+  }
+
+  // testa se o campo date é obrigatório e nenhuma de suas chaves deve vir vazia
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
+    return response.status(400).send({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  // testa se o numero em rate é um inteiro [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger]
+  // e se é um valor entre 1 e 5
+  if (!Number.isInteger(date.rate) || date.rate < 1 || date.rate > 5) {
+    return response.status(400).send({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+
+  // testa se o campo datedAt respeita o formato dd/mm/aaaa
+  // regex para Datas [https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy]
+  // com algumas adaptações
+  const regexDateFormat = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+  if (!regexDateFormat.test(date.datedAt)) {
+    return response.status(400).send({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  // caso o name, age, date e token sejam válidos, é necessário alterar o contato
+  // para isso 1) ler o arquivo e encontrar o contato pelo id; 2) "remover" o contato e
+  // 3) inserir os novos dados do contato no index daquele id
+  const listaContatinhos = await lerArquivo();
+  const novosDadosContatinho = { id: idPut, name, age, date };
+  listaContatinhos.splice(idPut, 1, novosDadosContatinho);
+  await escreverArquivo(JSON.stringify([...listaContatinhos], 0, 2));
+  return response.status(200).send(novosDadosContatinho);
 });
 
 module.exports = routes;
