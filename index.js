eea0510263707434de9138560c01e9e72ec27a22
Micaeli Teixeira <luime6@gmail.com>
2021-02-19 16:34:17 -0300

    feat: add challenge 2

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
 const path = require('path');
 
 const dataCrush = path.resolve(__dirname, 'crush.json');
+
+// Errors and port name
 const SUCCESS = 200;
+const NOTFOUND = 404;
 const INTERNALERROR = 500;
 const PORT = 3000;
 
 const app = express();
 app.use(express.json());
+app.use(bodyParser.json());
 
-// não remova esse endpoint, e para o avaliador funcionar
+// Don't remove this endpoint, and for the evaluator to work
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
@@ -26,16 +31,28 @@ function readFile(fileName) {
   });
 }
 
-// Desafio 1 - getAllCrush
-app.get('/crush', rescue(async (_req, res, _next) => {
-  const crushs = await readFile(dataCrush);
-  res.status(SUCCESS).json(JSON.parse(crushs));
-}));
-
 // midlewares error
 app.use((err, _req, res, _next) => {
   console.error(err.message);
   res.status(INTERNALERROR).json({ message: 'Internal Error' });
 });
 
+// Challenge 1 - getAllCrush
+app.get('/crush', rescue(async (_req, res, _next) => {
+  const crushs = await readFile(dataCrush);
+  res.status(SUCCESS).json(JSON.parse(crushs));
+}));
+
+// Challenge 2 - getCrushById
+app.get('/crush/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const crushs = await readFile(dataCrush);
+  const arrayData = JSON.parse(crushs);
+  const crushId = arrayData.find((obj) => obj.id === parseInt(id, 10));
+
+  if (!crushId) return res.status(NOTFOUND).json({ message: 'Crush não encontrado' });
+  return res.status(SUCCESS).json(crushId);
+}));
+
+// Listen to port 3000
 app.listen(PORT);
