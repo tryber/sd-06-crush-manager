ee8bd58c376302b6c5e5b84542d3dcf2644e294a
Pedro Duarte <ikbenpedro@gmail.com>
2021-02-19 19:03:49 -0300

    req3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const request = require('request');
 
 const app = express();
 const SUCCESS = 200;
@@ -11,8 +12,7 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-// Requisito 1 =====================================
-
+// requisito 1
 app.get('/crush', (_req, res) => {
   const content = fs.readFileSync('./crush.json', 'utf-8');
   if (!content) {
@@ -21,10 +21,7 @@ app.get('/crush', (_req, res) => {
   return res.status(200).json(JSON.parse(content));
 });
 
-// ================================================
-
-// Requisito 2 =====================================
-
+// requisito 2
 app.get('/crush/:id', (req, res) => {
   const content = fs.readFileSync('./crush.json', 'utf-8');
   const { id } = req.params;
@@ -35,6 +32,53 @@ app.get('/crush/:id', (req, res) => {
   return res.status(200).json(filteredCrushes);
 });
 
-// ================================================
+// requisito 3
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validatePassword(password) {
+  return password.toLowerCase().toString();
+}
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json(
+      {
+        message: 'O campo "email" é obrigatório',
+      },
+    );
+  }
+
+  if (!validateEmail) {
+    return res.status(400).json(
+      {
+        message: 'O "email" deve ter o formato "email@email.com"',
+      },
+    );
+  }
+
+  if (!password) {
+    return res.status(400).json(
+      {
+        message: 'O campo "password" é obrigatório',
+      },
+    );
+  }
+
+  if (validatePassword(password).length < 6) {
+    return res.status(400).json(
+      {
+        message: 'A "senha" deve ter pelo menos 6 caracteres',
+      },
+    );
+  }
+  return res.status(200).json(
+    {
+      token: '7mqaVRXJSp886CGr',
+    },
+  );
+});
 
 app.listen(3000, () => console.log('ouvindo na porta 3000'));
