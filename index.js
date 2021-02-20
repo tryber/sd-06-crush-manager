e16d21d8fd6c9014cf179ccd1e6a31662ee1ebb0
Bruna Franciele <brunafranciele1902@gmail.com>
2021-02-20 16:15:13 -0300

    fix: req 1

--- index.js
+++ index.js
@@ -4,20 +4,16 @@ const app = express();
 const SUCCESS = 200;
 const port = 3000;
 const fs = require('fs');
-const path = require('path'); 
+const path = require('path');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-
-
 // requisito 1
 
-const crush = './crush.json';
-
-function readFilePromise (fileName) {
+function readFilePromise(fileName) {
   return new Promise((resolve, reject) => {
     fs.readFile(fileName, (err, content) => {
       if (err) return reject(err);
@@ -28,8 +24,7 @@ function readFilePromise (fileName) {
 
 app.get('/crush', async (req, res) => {
   const file = await readFilePromise(path.join(__dirname, '.', 'crush.json'));
-  // console.log(file);
   res.status(200).json(JSON.parse(file));
-  });
+});
 
 app.listen(port);
