f6f2dd67748b8233d24297bb71042db593361680
Bruna Franciele <brunafranciele1902@gmail.com>
2021-02-20 18:15:38 -0300

    feat: requisito 2

--- index.js
+++ index.js
@@ -11,20 +11,30 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-// requisito 1
+// Requisito 1
 
-function readFilePromise(fileName) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(fileName, (err, content) => {
-      if (err) return reject(err);
-      resolve(content);
-    });
+const readFile = (file) => new Promise((resolve, reject) => {
+  fs.readFile(file, 'utf-8', (err, content) => {
+    if (err) return reject(err);
+    resolve(JSON.parse(content));
   });
-}
+});
 
 app.get('/crush', async (req, res) => {
-  const file = await readFilePromise(path.join(__dirname, '.', 'crush.json'));
-  res.status(200).json(JSON.parse(file));
+  const file = await readFile(path.join(__dirname, '.', 'crush.json'));
+  console.log(file);
+  // if (file.length === 0) res.status(200).send([]);
+  res.status(200).json(file);
+});
+
+// Requisito 2
+
+app.get('/crush/:id', async (req, res) => {
+  const file = await readFile(path.join(__dirname, '.', 'crush.json'));
+  const { id } = req.params;
+  const result = file.filter((el) => el.id === +id)[0];
+  if (!result) res.status(404).json({ message: 'Crush não encontrado' });
+  res.status(200).send(result);
 });
 
 app.listen(port);
