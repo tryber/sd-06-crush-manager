f26b1d1d3988d6cbfe877e84c6a0de44ea7975f3
Alessandra <itsalematos@hotmail.com>
2021-02-20 17:15:14 -0400

    Feat: second one

--- index.js
+++ index.js
@@ -15,18 +15,36 @@ app.get('/', (_request, response) => {
 const readFile = (file) => new Promise((resolve, reject) => {
   fs.readFile(file, 'utf-8', (err, content) => {
     if (err) return reject(err);
-    resolve(content);
+    resolve(JSON.parse(content));
   });
 });
 
 app.get('/crush', async (req, res) => {
   const file = await readFile(path.join(__dirname, '.', 'crush.json'));
-  // console.log(file);
-  res.status(200).json(JSON.parse(file));
+  console.log(file);
+  if (file.length === 0) res.status(200).send([]);
+  res.status(200).json(file);
 });
 
 // Requisito 2
 
-// app.get()
+app.get('/crush/:id', async (req, res) => {
+  const file = await readFile(path.join(__dirname, '.', 'crush.json'));
+  const { id } = req.params;
+  const result = file.filter((el) => el.id === +id)[0];
+  if (!result) res.status(404).json({ message: 'Crush não encontrado' });
+  res.status(200).send(result);
+});
+
+// Requisito 3
+
+// app.get('/login', (req, res) => {
+
+// })
+
+// Requisito 4
+
+// app.post('/crush', (req, res) => {
+// });
 
 app.listen(3000, () => console.log('aqui'));
