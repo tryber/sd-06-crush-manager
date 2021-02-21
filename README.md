d760466e3ed21ac81138cc058120f27fe4cad48f
Matheus Dias <matheuscara2000@gmail.com>
2021-02-21 20:30:10 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ Aqui você vai encontrar os detalhes de como estruturar o desenvolvimento do seu
 
 ---
 
+
+
 # Sumário
 
 - [Habilidades](#habilidades)
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const app = express();
 const SUCCESS = 200;
@@ -7,3 +9,25 @@ const SUCCESS = 200;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.use(bodyParser.json());
+
+const getCrush = async () => {
+  const result = await fs.readFileSync('./crush.json', 'utf-8', (erro) => {
+    if(erro) throw new Error(erro);
+
+    console.log(`algo inesperado aconteceu : ${erro}`);
+  })
+  return JSON.parse(result);
+};
+
+app.get('./crush', async (req, res) => {
+  const result = await getCrush();
+  res.send(result)
+})
+
+app.listen(300, () => {
+  console.log('Servidor Online')
+})
+
+
