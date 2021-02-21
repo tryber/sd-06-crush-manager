667b1b60b611c6588fccd4e977a628ccd0dc82e7
paulo-lins <paulolins100@gmail.com>
2021-02-20 21:30:09 -0300

    Quesito2

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
+// REQ-1
 app.get('/crush', (_req, res) => {
   const crushFile = fs.readFileSync(path.join(__dirname, 'crush.json'), 'utf-8');
   if (!crushFile) {
@@ -19,4 +20,16 @@ app.get('/crush', (_req, res) => {
   }
   return res.status(200).json(JSON.parse(crushFile));
 });
+
+// REQ-2
+app.get('/crush/:id', (req, res) => {
+  const crushFile = fs.readFileSync(path.join(__dirname, 'crush.json'), 'utf-8');
+  const { id } = req.params;
+  const getCrush = JSON.parse(crushFile).find((crush) => crush.id === Number(id));
+  if (!getCrush) {
+    return res.status(404).json({ message: 'Crush não encontrado' });
+  }
+  return res.status(200).json(getCrush);
+});
+
 app.listen(3000, () => console.log('rodando'));
