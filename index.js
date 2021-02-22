36381bfa297745a1b296549e7614518412dd6290
paulo-lins <paulolins100@gmail.com>
2021-02-22 18:43:54 -0300

    eveluator-com-problema-?

--- index.js
+++ index.js
@@ -149,20 +149,20 @@ app.put('/crush/:id', (req, res) => {
 });
 
 // REQ-6
-app.delete('/crush/:id', (req, res) => {
-  const { authorization } = req.headers;
-  const { id } = req.params;
-  const file = JSON.parse(fs.readFileSync('./crush.json'));
+// app.delete('/crush/:id', (req, res) => {
+//   const { authorization } = req.headers;
+//   const { id } = req.params;
+//   const file = JSON.parse(fs.readFileSync('./crush.json'));
 
-  if (!authorization) {
-    return res.status(401).json({ message: 'Token não encontrado' });
-  }
-  if (authorization.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido' });
-  }
-  const newFile = file.filter((crush) => crush.id !== Number(id));
-  fs.writeFileSync('crush.json', JSON.stringify(newFile));
-  return res.status(200).json({ message: 'Crush deletado com sucesso' });
-});
+//   if (!authorization) {
+//     return res.status(401).json({ message: 'Token não encontrado' });
+//   }
+//   if (authorization.length !== 16) {
+//     return res.status(401).json({ message: 'Token inválido' });
+//   }
+//   const newFile = file.filter((crush) => crush.id !== Number(id));
+//   fs.writeFileSync('crush.json', JSON.stringify(newFile));
+//   return res.status(200).json({ message: 'Crush deletado com sucesso' });
+// });
 
 app.listen(3000, () => console.log('rodando..'));
