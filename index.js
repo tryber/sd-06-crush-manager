f4b8dbc9e9e6139bed6bc0fc15272d8522162d67
rafaelgelatti <rafael.gelatti@gmail.com>
2021-02-22 10:32:23 -0300

    crush-project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const crushs = require('./crush.json');
 
 const app = express();
 const SUCCESS = 200;
@@ -7,3 +8,19 @@ const SUCCESS = 200;
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', (req, res) => {
+  res.status(SUCCESS).json(crushs);
+});
+
+app.get('/crush/:id', (req, res) => {
+  const { id } = req.params;
+  const crush = crushs.find((e) => e.id == id);
+  if (crush) {
+    return res.status(200).json(crush)
+  } else {
+    return res.status(404).json({ message: "Crush não encontrado" });
+  }
+});
+
+app.listen(3000);
