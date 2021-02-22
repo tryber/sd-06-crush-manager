ba3d7147d4ce0990063595bfbe8306833b4e9d1c
Daniel Jesus <dev.danieljs@gmail.com>
2021-02-22 20:36:08 -0300

    feat: add 2º challenge getCrushById

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const path = require('path');
 const dataCrush = path.resolve(__dirname, 'crush.json');
 
 const SUCCESS = 200;
+const NOTFOUND = 404;
+const INTERNALERROR = 500;
 const PORT = 3000;
 
 const app = express();
@@ -39,5 +41,28 @@ app.get(
     res.status(SUCCESS).json(JSON.parse(crushs));
   }),
 );
+
+// Challenge 2
+
+app.get(
+  '/crush/:id',
+  rescue(async (req, res) => {
+    const { id } = req.params;
+
+    const crushs = await readFile(dataCrush);
+    const arrayData = JSON.parse(crushs);
+    const crushId = arrayData.find((obj) => obj.id === parseInt(id, 10));
+    if (!crushId) return res.status(NOTFOUND).json({ message: 'Crush não encontrado' });
+    return res.status(SUCCESS).json(crushId);
+  }),
+);
+
+// midlewares error
+
+app.use((err, _req, res, _next) => {
+  console.error(err.message);
+
+  res.status(INTERNALERROR).json({ message: 'Internal Error' });
+});
 
 app.listen(PORT, () => console.log(`Rodando servidor na porta: ${PORT}`));
