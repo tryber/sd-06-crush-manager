240805bfb21615bbc378e2fa9e71080394103dd6
Isa Koga <isadorakoga710@gmail.com>
2021-02-22 20:18:45 -0300

    remove console.log

--- deleteCrush.js
+++ deleteCrush.js
@@ -3,15 +3,12 @@ const { getData } = require('./getData');
 
 const deleteCrush = async (req, res) => {
   const id = parseInt(req.params.id, 10);
-  console.log(id);
   const delData = await getData();
   const deletedData = JSON.parse(delData);
-  console.log(deletedData);
   const index = deletedData.findIndex((element) => element.id === id);
   if (index !== -1) {
     deletedData.splice(index, 1);
   }
-  console.log(deletedData);
   fs.writeFile('./crush.json', JSON.stringify(deletedData), (err) => {
     if (err) throw new Error(err);
   });
