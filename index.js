a265124569343f549f4ea0f40e9adf70f25929f0
Daniel Jesus <dev.danieljs@gmail.com>
2021-02-22 20:45:15 -0300

    feat: add 3º challenge login

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const rescue = require('express-rescue');
+const crypto = require('crypto-random-string');
 const path = require('path');
 
 const dataCrush = path.resolve(__dirname, 'crush.json');
 
 const SUCCESS = 200;
+const BADREQUEST = 400;
 const NOTFOUND = 404;
 const INTERNALERROR = 500;
 const PORT = 3000;
@@ -56,6 +58,39 @@ app.get(
     return res.status(SUCCESS).json(crushId);
   }),
 );
+
+// Challenge 3
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  const authentication = crypto({ length: 16, type: 'hex' });
+  const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
+
+  if (!email) {
+    return res
+      .status(BADREQUEST)
+      .json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailRegex.test(email.toLowerCase())) {
+    return res
+      .status(BADREQUEST)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res
+      .status(BADREQUEST)
+      .json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.toString().length < 6) {
+    return res
+      .status(BADREQUEST)
+      .json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+
+  res.status(SUCCESS).json({ token: authentication });
+});
 
 // midlewares error
 
