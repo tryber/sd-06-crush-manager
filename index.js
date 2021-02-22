32624eed7f41164b0c53781382e94ce183676cc4
Anderson Iarrocheski <anderson.iarrocheski@fohat.co>
2021-02-21 21:53:02 -0300

    Requisitos 1 a 3, testando no avaliador

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const express = require('express');
+const fs = require('fs')
+const crypto = require('crypto');
 
 const app = express();
 const SUCCESS = 200;
 
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
+// Necessário para o avaliador
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
+});
+
+app.get('/crush', async (req, res) => {
+  if (res.statusCode === SUCCESS) {
+    res.send(JSON.parse(fs.readFileSync('./crush.json')));
+  }
+});
+
+app.get('/crush/:id', async (req, res) => {
+  const { id: paramID } = req.params;
+  const file = await fs.readFileSync('./crush.json');
+  const checkIfTheUserExists = file.some((id) => id === Number(paramID));
+
+  if (!checkIfTheUserExists) res.status(404).json({ message: 'Crush não encontrado' });
+
+  const response = JSON.parse(file).find((people) => people.id === Number(paramID));
+  res.status(200).json(response);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
+
+  if (!email) {
+    return res.status(404).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!emailFormat.test(email)) {
+    return res.status(404).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    return res.status(404).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res.status(404).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  }
+
+  return new Promise((fulfill, reject) => {
+    crypto.randomBytes(8, (error, buf) => {
+      if (error) {
+        reject(error);
+      } else {
+        const GeneratedToken = buf.toString('hex');
+        fulfill(GeneratedToken);
+        res.status(200).json({ token: GeneratedToken });
+      }
+    });
+  });
 });
