c0ab07b465bb79a07b65e35fccbc1cf2d2897825
Bruna Franciele <brunafranciele1902@gmail.com>
2021-02-22 10:18:01 -0300

    fix: usando código da Alê

--- index.js
+++ index.js
@@ -1,63 +1,52 @@
 const express = require('express');
-
-const app = express();
-const SUCCESS = 200;
-const port = 3000;
 const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
+const SUCCESS = 200;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
-
 // Requisito 1
-
 const readFile = (file) => new Promise((resolve, reject) => {
   fs.readFile(file, 'utf-8', (err, content) => {
     if (err) return reject(err);
     resolve(JSON.parse(content));
   });
 });
-
 app.get('/crush', async (req, res) => {
   const file = await readFile(path.join(__dirname, '.', 'crush.json'));
   console.log(file);
-  // if (file.length === 0) res.status(200).send([]);
-  res.status(200).json(file);
+  if (file.length === 0) return res.status(200).send([]);
+  return res.status(200).json(file);
 });
-
-// Requisito 21
-
+// Requisito 2
 app.get('/crush/:id', async (req, res) => {
   const file = await readFile(path.join(__dirname, '.', 'crush.json'));
   const { id } = req.params;
   const result = file.filter((el) => el.id === +id)[0];
-  if (!result) res.status(404).json({ message: 'Crush não encontrado' });
-  res.status(200).send(result);
+  if (!result) return res.status(404).json({ message: 'Crush não encontrado' });
+  return res.status(200).send(result);
 });
-
 // Requisito 3
-app.use(bodyParser.json());
-
 const isValid = (email, password) => {
-  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(email);
+  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/;
   if (!email) return 'O campo "email" é obrigatório';
-  if (!regexEmail) return 'O "email" deve ter o formato "email@email.com"';
+  if (!regexEmail.test(email)) return 'O "email" deve ter o formato "email@email.com"';
   if (!password) return 'O campo "password" é obrigatório';
   if (password.length < 6) return 'A "senha" deve ter pelo menos 6 caracteres';
   return false;
 };
 app.post('/login', (req, res) => {
   const randomToken = crypto.randomBytes(8).toString('hex');
-  // console.log(randomToken);
   const { email, password } = req.body;
-  // console.log(email);
   const result = isValid(email, password);
-  if (result) res.status(400).json({ message: result });
-  res.status(200).json({ token: randomToken });
+  if (result) return res.status(400).json({ message: result });
+  return res.status(200).json({ token: randomToken });
 });
-
-app.listen(port);
+// Requisito 4
+app.listen(3000, () => console.log('aqui'));
