035d5dadb294170e599e60050f02d10ae3084a47
deko possas <andrepossaster@gmail.com>
2021-02-23 07:04:48 -0300

    troquei o texto de validacao

--- controle/login.js
+++ controle/login.js
@@ -7,7 +7,7 @@ const login = (_req, res) => {
   if (!validandoEmail) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
 
   if (!password) return res.status(400).json({ message: 'O campo password é obrigatorio' });
-  if (!validandoPassword) return res.status(400).json({ message: 'O password ter pelo menos 6 caracteres' });
+  if (!validandoPassword) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
 
   res.status(200).json({ token: gerandoToken() });
 };
