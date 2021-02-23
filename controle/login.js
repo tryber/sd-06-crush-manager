0c0baf3e5e640e23ed9e5422e60c05d4fab27ce3
deko possas <andrepossaster@gmail.com>
2021-02-23 06:17:30 -0300

    importei as funcoes de validacao e tratei erro de obrigacao de email

---
+++ controle/login.js
@@ -0,0 +1,7 @@
+const { gerandoToken, validandoEmail, validandoPassword } = require('../servicos');
+
+const login = (_req, res) => {
+  const {email, password} = _req.body;
+
+  if(!email) return res.status(400).json({message: 'O campo email é obrigatorio'});
+};
