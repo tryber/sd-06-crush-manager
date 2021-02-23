3a54bb05d4ffd0795c5b4b7d75c545bcc77a5d4a
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-02-23 10:18:18 -0300

    :bug: Corrigindo erros

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { request } = require('express');
 const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
-const data = require('./crushs');
+const data = require('./crush.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -17,12 +16,13 @@ app.get('/crush', async (_request, response) => {
 app.get('/crush/:id', async (request, response) => {
   const { id } = request.params;
   const messsage = {
-    "message": "Crush não encontrado"
+    message: 'Crush não encontrado',
+  };
+  const ator = data.find((el) => id === el.id);
+  if (!ator) {
+    return response.status(404).json(messsage);
   }
-  if(!data[id-1]) {
-    return response.status(404).json(messsage)
-  }
-  await response.status(200).send(data[id-1]);
+  await response.status(200).send(ator);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
