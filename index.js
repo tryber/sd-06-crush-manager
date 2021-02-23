0cd4eec904ff5897f78353508b0a44560ce1dfce
Cássio Rubens <cassiorubensadv@gmail.com>
2021-02-23 13:54:39 -0300

    FIX: Crie o endpoint DELETE /crush/:id

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');//
-const fs = require('fs');
 const { readFile } = require('./utils/manageFiles');//
 
 const app = express();
