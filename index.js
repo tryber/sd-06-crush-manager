5e5ae9fdcc3951d4f3fa86a79658895d4f26a095
flaviannymaciel <flaviannymaciel@gmail.com>
2021-02-22 21:45:24 -0300

    req2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 
 const app = express();
 const SUCCESS = 200;
-const port = 3000;
+const port = 3456;
 app.use(express.json()); // mesma funcionalidade que o bodyParser
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -11,18 +11,33 @@ app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-// READ
+// readFile
+const read = async () => {
+  const crush = await fs.readFile('./crush.json', 'utf-8');
+  console.log(crush);
+  return JSON.parse(crush);
+};
+
+// read rota crush
 app.get('/crush', async (_req, res) => {
   try {
-    const readFile = await fs.readFile('./crush.json', 'utf-8');
-    // console.log(readFile);
-    return res.status(200).json(JSON.parse(readFile));
+    const crushes = await read();
+    return res.status(200).send(crushes);
   } catch (error) {
-    // console.error(error.message);
-    res.status(200).json([]);
+    console.error(error.message);
+    return res.status(200).json([]);
   }
 });
 
+app.get('/crush/:id', async (req, res) => {
+  const { id } = req.params;
+  const crushList = await read();
+  const paramsId = crushList.filter((crush) => crush.id === parseInt(id, 10));
+  if (paramsId.length) {
+    return res.status(200).json(paramsId[0]);
+  }
+  return res.status(404).json({ message: 'Crush não encontrado' });
+});
 app.listen(port, () => {
   console.log('Ouvindo na porta:', port);
 });
