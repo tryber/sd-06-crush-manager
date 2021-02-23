4685de452b86b2f645a5697d28f5976260ef4c3b
deko possas <andrepossaster@gmail.com>
2021-02-23 06:22:25 -0300

    tratamento de erro da validacao

--- controle/login.js
+++ controle/login.js
@@ -3,5 +3,6 @@ const { gerandoToken, validandoEmail, validandoPassword } = require('../servicos
 const login = (_req, res) => {
   const {email, password} = _req.body;
 
-  if(!email) return res.status(400).json({message: 'O campo email é obrigatorio'});
+  if(!email) return res.status(400).json({ message: 'O campo email é obrigatorio' });
+  if(!validandoEmail) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
 };
