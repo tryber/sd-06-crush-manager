c4f49890721932ca56580fd63c16ada87d44ff44
deko possas <andrepossaster@gmail.com>
2021-02-23 07:28:11 -0300

    index completo

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { pegandoCrushs } = require('./servicos');
+const { pegandoTodosOsCrushs } = require('./controle/pegandoTodosOsCrushs');
 const { pegandoCrushId } = require('./controle/pegandoPorId');
 const { login } = require('./controle/login');
+const { validandoCrush } = require('./controle/validandoCrush');
+const { criandoCrush } = require('./controle/criandoCrush');
+const { checandoToken } = require('./servicos');
+const { editandoCrush } = require('./controle/editandoCrush');
+const { deletandoCrush } = require('./controle/deletandoCrush');
+const { procurandoCrush } = require('./controle/procurandoCrush');
 
 const app = express();
 const SUCCESS = 200;
@@ -17,12 +23,12 @@ app.use(bodyParser.json());
 
 app.post('/login', login);
 
-app.get('/crush', async (req, res) => {
-  const response = await pegandoCrushs();
-  res.status(200).send(response);
-});
-
+app.get('/crush', pegandoTodosOsCrushs);
+app.get('/crush/search', checandoToken, procurandoCrush);
 app.get('/crush/:id', pegandoCrushId);
+app.post('/crush', checandoToken, validandoCrush, criandoCrush);
+app.put('/crush/:id', checandoToken, validandoCrush, editandoCrush);
+app.delete('/crush/:id', checandoToken, deletandoCrush);
 
 app.listen(3000, () => {
   console.log('trabalhando');
