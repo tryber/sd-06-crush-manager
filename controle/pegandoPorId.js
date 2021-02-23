45ac8838eae7df0cf41383a1606d4d8a63137e45
deko possas <andrepossaster@gmail.com>
2021-02-23 06:57:30 -0300

    voltei pra pegandoCrushs

--- controle/pegandoPorId.js
+++ controle/pegandoPorId.js
@@ -1,8 +1,8 @@
-const { pegandoTodosOsCrushs } = require('./pegandoTodosOsCrushs');
+const { pegandoCrushs } = require('../servicos');
 
 const pegandoCrushId = async (_req, res) => {
   const id = parseInt(_req.params.id, 10);
-  const crushArr = await pegandoTodosOsCrushs();
+  const crushArr = await pegandoCrushs();
   const crushEncontrado = crushArr.find((crush) => crush.id === id);
   if (!crushEncontrado) res.status(404).json({ message: 'Crush não encontrado' });
 
