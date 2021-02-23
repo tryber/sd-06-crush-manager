539ce45debc3624f9a16f86a15d5057c1679dd87
deko possas <andrepossaster@gmail.com>
2021-02-23 06:26:58 -0300

    tratamento de sucesso e export de login

--- controle/login.js
+++ controle/login.js
@@ -1,8 +1,15 @@
 const { gerandoToken, validandoEmail, validandoPassword } = require('../servicos');
 
 const login = (_req, res) => {
-  const {email, password} = _req.body;
+  const { email, password } = _req.body;
 
-  if(!email) return res.status(400).json({ message: 'O campo email é obrigatorio' });
-  if(!validandoEmail) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
+  if (!email) return res.status(400).json({ message: 'O campo email é obrigatorio' });
+  if (!validandoEmail) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
+
+  if (!password) return res.status(400).json({ message: 'O campo password é obrigatorio' });
+  if (!validandoPassword) return res.status(400).json({ message: 'O password ter pelo menos 6 caracteres' });
+
+  res.status(200).json({ token: gerandoToken() });
 };
+
+module.exports = { login };
