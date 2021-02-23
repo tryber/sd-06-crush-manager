ac7baea09b0f72a7ba874497ecccbc7cf2d24d7c
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-02-23 11:09:09 -0300

    req2

--- index.js
+++ index.js
@@ -16,9 +16,21 @@ const read = async (arquivo) => {
   return JSON.parse(response);
 };
 
+const write = async (arquivo, obj) => {
+  await fs.writeFile(path.resolve(path.join(__dirname, arquivo)), obj, 'utf-8');
+  return true;
+};
+
 app.get('/crush', async (_request, response) => {
   const fun = await read('./crush.json');
   response.status(200).json(fun);
+});
+
+app.get('/crush/:id', async (request, response) => {
+  const { id } = request.params;
+  const fun = await read('./crush.json');
+  const ator = fun.find((el) => el.id === +id);
+  response.status(200).json(ator);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
