3ce6e7080150652ad59b02ed5d881044f8969ac3
flaviannymaciel <flaviannymaciel@gmail.com>
2021-02-23 18:57:51 -0300

    req4

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const read = async () => {
   return JSON.parse(crush);
 };
 // read();
+// writeFile
+const write = async (newCrush) => {
+  await fs.writeFile('./crush.json', newCrush, 'utf-8');
+  return true;
+};
 
 // req 1 rota crush
 app.get('/crush', async (_req, res) => {
@@ -69,19 +74,70 @@ app.post('/login', (req, res) => {
   // validate password
   const rgxPassword = /^\d{6,}$/gm;
   if (!rgxPassword.test(password)) {
-    return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+    return res
+      .status(400)
+      .json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
 
   return res.status(200).json({ token });
 });
 
 // req4
-// app.post('/crush', (req, res) => {
-//   const newCrush = req.body;
-//   const crushList = read();
-//   const newList = crushList.push(newCrush);
-//   res.status(200).json(newList);
-// });
+app.post('/crush', async (req, res) => {
+  const crushList = await read();
+  // console.log(crushList);
+  const { name, age, date } = req.body;
+  // console.log(name, age, date);
+  const tokenAuthorization = req.headers.authorization;
+  // console.log(tokenAuthorization);
+  if (!tokenAuthorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (tokenAuthorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res
+      .status(400)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
+    return res.status(400).send({
+      message:
+        'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }
+  // Number.isInteger verifica se o numero é inteiro
+  if (!Number.isInteger(date.rate) || date.rate < 1 || date.rate > 5) {
+    return res.status(400).send({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  // regex para datas xx/xx/xxxx
+  const rgxDate = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+  if (!rgxDate.test(date.datedAt)) {
+    return res.status(400).send({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+
+  const newCrush = { id: crushList.length + 1, ...req.body };
+  await write(JSON.stringify([...crushList, newCrush], 0, 2));
+  return res.status(201).send(newCrush);
+});
 
 const port = 3000;
 app.listen(port, () => {
