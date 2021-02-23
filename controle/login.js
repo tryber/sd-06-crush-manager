6821e2f1e7fe688697ac9e7eea35cd238433aec6
deko possas <andrepossaster@gmail.com>
2021-02-23 07:08:46 -0300

    tinha esquecido os parametros

--- controle/login.js
+++ controle/login.js
@@ -4,10 +4,10 @@ const login = (_req, res) => {
   const { email, password } = _req.body;
 
   if (!email) return res.status(400).json({ message: 'O campo email é obrigatorio' });
-  if (!validandoEmail) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
+  if (!validandoEmail(email)) return res.status(400).json({ message: 'O email deve ter o formato "email@email.com"' });
 
   if (!password) return res.status(400).json({ message: 'O campo password é obrigatorio' });
-  if (!validandoPassword) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+  if (!validandoPassword(password)) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
 
   res.status(200).json({ token: gerandoToken() });
 };
