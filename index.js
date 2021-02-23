d2b0ef0be0d6e5719e6f095bd6c7c0f78ba87e1d
Matheus Dias <matheuscara2000@gmail.com>
2021-02-22 21:49:13 -0300

    Lint

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const fs = require('fs').promises;
+
 const app = express();
 const SUCCESS = 200;
 const port = 3000;
+
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+function validEmail(email) {
+  return /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{3,8})?$/.test(email);
+}
+function validPassword(password) {
+  return /[0-9]{6}/.test(password);
+}
 
 app.use(express.json());
 
@@ -18,7 +29,6 @@ app.get('/crush', async (_req, res) => {
   res.status(SUCCESS).send(JSON.parse(data));
 });
 
-
 // task 2
 app.get('/crush/:id', async (req, res) => {
   const data = JSON.parse(await fs.readFile('./crush.json', 'utf-8'));
@@ -31,13 +41,12 @@ app.get('/crush/:id', async (req, res) => {
 // task 3
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  if (email === undefined) return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
-  if (validEmail(email) === false) return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
-  if (password === undefined) return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
-  if (validPassword(password) === false) return res.status(400).json({ message: "A \"senha\" deve ter pelo menos 6 caracteres" });
+  if (email === undefined) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (validEmail(email) === false) return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  if (password === undefined) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (validPassword(password) === false) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   const token = generateToken();
   return res.send({ token });
 });
-
 
 app.listen(port, () => console.log('Server On'));
