a2b02c7debe69723fd64f6af68b35cf4f0d57972
deko possas <andrepossaster@gmail.com>
2021-02-23 08:38:38 -0300

    procurando crushs

---
+++ controle/procurandoCrush.js
@@ -0,0 +1,11 @@
+const { pegandoCrushs } = require('../servicos');
+
+const procurandoCrush = async (req, res) => {
+  const crushData = await pegandoCrushs();
+  const searchText = req.query.q;
+  if (!searchText) res.status(200).json(crushData);
+  const crushFound = crushData.Filter((crush) => crush.name.includes(searchText));
+  res.status(200).json(crushFound);
+};
+
+module.exports = { procurandoCrush };
