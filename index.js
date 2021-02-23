99dd77ee97b6a81a85df38eef573b1ebe81d1578
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-02-23 09:01:41 -0300

    req1

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 const SUCCESS = 200;
+const data = require('./crushs');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
+
+app.get('/crush', async (_request, response) => {
+  await response.status(200).send(data);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
