3a58b50446c239988942b39736a544cb256ca738
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-02-23 10:45:30 -0300

    req1

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 const SUCCESS = 200;
-const data = require('./crush.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.get('/crush', async (_request, response) => {
-  await response.status(200).send(data);
-});
+const read = async (arquivo) => {
+  const response = await fs.readFile(path.resolve(path.join(__dirname, arquivo)), 'utf-8');
+  console.log(response);
+  return response;
+};
 
-app.get('/crush/:id', async (request, response) => {
-  const { id } = request.params;
-  const messsage = {
-    message: 'Crush não encontrado',
-  };
-  const ator = data.find((el) => id === el.id);
-  if (!ator) {
-    return response.status(404).json(messsage);
-  }
-  await response.status(200).send(ator);
+app.get('/crush', async (_request, response) => {
+  const fun = await read('./crush.json');
+  response.status(200).send(fun);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
