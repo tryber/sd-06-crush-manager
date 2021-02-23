6e38d02454036d89877680b79a13b23f39246621
Matheus Dias <matheuscara2000@gmail.com>
2021-02-22 21:44:00 -0300

    Task 1 - 3

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-
+const fs = require('fs').promises;
 const app = express();
 const SUCCESS = 200;
+const port = 3000;
+
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.use(bodyParser.json());
-
-const getCrush = async () => {
-  const result = await fs.readFileSync('./crush.json', 'utf-8', (erro) => {
-    if (erro) throw new Error(erro);
-
-    console.log(`algo inesperado aconteceu : ${erro}`);
-  });
-  return JSON.parse(result);
-};
-
-app.get('./crush', async (req, res) => {
-  const result = await getCrush();
-  res.send(result);
+// task 1
+app.get('/crush', async (_req, res) => {
+  const data = await fs.readFile('./crush.json', 'utf-8');
+  if (data.length < 1) res.status(SUCCESS).send([]);
+  res.status(SUCCESS).send(JSON.parse(data));
 });
 
-app.listen(300, () => {
-  console.log('Servidor Online');
+
+// task 2
+app.get('/crush/:id', async (req, res) => {
+  const data = JSON.parse(await fs.readFile('./crush.json', 'utf-8'));
+  const { id } = req.params;
+  const result = data.find((el) => el.id === Number(id));
+  if (result === undefined) res.status(404).send({ message: 'Crush não encontrado' });
+  res.status(SUCCESS).send(result);
 });
+
+// task 3
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (email === undefined) return res.status(400).json({ message: "O campo \"email\" é obrigatório" });
+  if (validEmail(email) === false) return res.status(400).json({ message: "O \"email\" deve ter o formato \"email@email.com\"" });
+  if (password === undefined) return res.status(400).json({ message: "O campo \"password\" é obrigatório" });
+  if (validPassword(password) === false) return res.status(400).json({ message: "A \"senha\" deve ter pelo menos 6 caracteres" });
+  const token = generateToken();
+  return res.send({ token });
+});
+
+
+app.listen(port, () => console.log('Server On'));
