870c05457864aeee61c0db51897674da8c6334f3
Matheus Dias <matheuscara2000@gmail.com>
2021-02-23 18:13:17 -0300

    Projeto nao passa

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 const SUCCESS = 200;
 const port = 3000;
 
+app.use(express.json());
+
 function generateToken() {
   return crypto.randomBytes(8).toString('hex');
 }
@@ -18,8 +20,6 @@ function validPassword(password) {
 function validDate(date) {
   return /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/.test(date);
 }
-
-app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -50,30 +50,46 @@ app.post('/login', async (req, res) => {
   if (password === undefined) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   if (validPassword(password) === false) return res.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   const token = generateToken();
+
   return res.send({ token });
 });
 
-// desafio 4
+
+
+// task 4
 app.post('/crush', async (req, res) => {
-  const crushRead = await fs.readFile('./crush.json', 'utf-8');
-  const dataJson = JSON.parse(crushRead);
+  const data = await fs.readFile('./crush.json', 'utf-8');
+
   const { name, age, date } = req.body;
-  const newCrush = JSON.stringify(dataJson.concat({ name, age, date }));
+
+  const dataJson = JSON.parse(data);
+
+  const id = dataJson.length + 1;
+
+  const corpoValidate = { name, age, id, date };
+
+  const novoCrush = JSON.stringify(dataJson.concat(corpoValidate));
+
   const { authorization } = req.headers;
 
   if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
   if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+
   if (!name || name === '') return res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.length < 3) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  if (!age || age === '' || typeof (age) !== 'number') return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (name < 3) return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+
+  if (!age || age === '') return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   if (age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+
   if (!date || date === '' || !date.rate || date.datedAt === '' || !date.datedAt) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
-  if ((date.rate) < 1 || (date.rate) > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+
+  if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+
   if (validDate(date.datedAt) === false) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
 
-  await fs.writeFile('./crush.json', newCrush, 'utf-8');
+  await fs.writeFile('./crush.json', novoCrush, 'utf-8');
 
-  res.status(201).send({ name, age, date });
+  res.status(201).send(corpoValidate);
 });
 
 app.listen(port, () => console.log('Server On'));
