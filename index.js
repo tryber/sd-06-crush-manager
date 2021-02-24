21692ff9ddb40320f669cf48b077f26fc7f482e4
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-02-24 08:44:35 -0300

    req3

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const SUCCESS = 200;
+
+app.use(bodyParser.json());
+app.post('/teste', (req, res) => res.status(200).json(req.body));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -36,6 +40,55 @@ app.get('/crush/:id', async (request, response) => {
     });
   }
   response.status(200).json(ator);
+});
+
+const confereEmail = (email) => {
+  const regex = /^[a-zA-Z0-9]+@[a-z]+\.com$/;
+  if (!email) return null;
+  return regex.test(email);
+};
+
+const geradordeToken = () => {
+  const token = Math.random()
+    .toString(36).substring(2, 20);
+  return token;
+};
+
+const confereSenha = (senha) => {
+  if (senha.toString().length === 0) {
+    return null;
+  }
+  if (senha.toString().length >= 6) {
+    return true;
+  }
+};
+
+app.post('/login', async (request, response) => {
+  console.log(request);
+  console.log(request.body);
+  const { email, password } = request.body;
+
+  if (confereEmail(email) === false) {
+    response.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  if (confereEmail(email) === null) {
+    response.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  if (confereSenha(password) === false) {
+    response.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  if (confereSenha(password) === null) {
+    response.status(400).json({
+      message: 'A "senha" deve ter pelo menos 6 caracteres',
+    });
+  }
+  return response.status(200).json({ token: geradordeToken() });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
