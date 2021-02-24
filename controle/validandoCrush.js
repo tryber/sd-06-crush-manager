afb0349c3671a1be9763110cc33f734b701263b2
deko possas <andrepossaster@gmail.com>
2021-02-24 16:46:23 -0300

    troquei o nome das variaveis

--- controle/validandoCrush.js
+++ controle/validandoCrush.js
@@ -1,29 +1,29 @@
-const chegandoData = (data) => {
+const chegandoDate = (date) => {
   const validacao = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
-  return validacao.test(data);
+  return validacao.test(date);
 };
 
 const validandoCrush = (req, res, next) => {
-  const { nome, idade, data } = req.body;
-  if (!nome || nome === '') {
+  const { name, age, date } = req.body;
+  if (!name || name === '') {
     return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   }
-  if (nome.length < 3) {
+  if (name.length < 3) {
     return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
-  if (!idade || idade === '') {
+  if (!age || age === '') {
     return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   }
-  if (idade < 18) {
+  if (age < 18) {
     return res.status(400).json({ message: 'O crush deve ser maior de idade' });
   }
-  if (!data || !data.datedAt || (!data.rate && data.rate !== 0)) {
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
     return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
   }
-  if (!chegandoData(data.datedAt)) {
+  if (!chegandoDate(date.datedAt)) {
     return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
   }
-  if (data.rate < 1 || data.rate > 5) {
+  if (date.rate < 1 || date.rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
