1f77b67251763b06ef9c254a698349bca58b9269
ilanribeiro <ilanribeiro@gmail.com>
2021-02-24 10:23:49 -0300

    [ Feature ] requisito 5 ok. Passa nos testes locais.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const crushes = require('./crush.json');
 
 const app = express();
 const SUCCESS = 200;
+const CREATED = 201;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 const NOTFOUND = 404;
 const PORT = 3000;
 
@@ -47,7 +50,7 @@ const verifyEmail = (email, response) => {
   const isValidEmail = regex.test(email);
 
   if (!email) {
-    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
+    return response.status(BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!isValidEmail) {
@@ -59,18 +62,18 @@ const verifyPassword = (password, response) => {
   const six = 6;
 
   if (!password) {
-    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return response.status(BAD_REQUEST).json({ message: 'O campo "password" é obrigatório' });
   }
 
   if (password.length < six) {
-    return response.status(400).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
+    return response.status(BAD_REQUEST).json({ message: 'A "senha" deve ter pelo menos 6 caracteres' });
   }
 };
 
 const tokenExists = (authorization, response) => {
   const authorizationNotExists = !authorization || authorization === '';
   if (authorizationNotExists) {
-    return responseError(401, 'Token não encontrado', response);
+    return responseError(UNAUTHORIZED, 'Token não encontrado', response);
   }
 };
 
@@ -78,14 +81,14 @@ const validToken = (authorization, response) => {
   const tokenLength = authorization.length;
   const validLength = 16;
   if (tokenLength !== validLength) {
-    return responseError(401, 'Token inválido', response);
+    return responseError(UNAUTHORIZED, 'Token inválido', response);
   }
 };
 
 const nameExists = (name, response) => {
   const nameNotExists = !name || name === '';
   if (nameNotExists) {
-    return responseError(400, 'O campo "name" é obrigatório', response);
+    return responseError(BAD_REQUEST, 'O campo "name" é obrigatório', response);
   }
 };
 
@@ -93,35 +96,35 @@ const nameLength = (name, response) => {
   const nameSize = name.length;
   const minCaracteres = 3;
   if (nameSize < minCaracteres) {
-    return responseError(400, 'O "name" deve ter pelo menos 3 caracteres', response);
+    return responseError(BAD_REQUEST, 'O "name" deve ter pelo menos 3 caracteres', response);
   }
 };
 
 const ageExists = (age, response) => {
   const ageNotExists = !age || age === '';
   if (ageNotExists) {
-    return responseError(400, 'O campo "age" é obrigatório', response);
+    return responseError(BAD_REQUEST, 'O campo "age" é obrigatório', response);
   }
 };
 
 const allowedAge = (age, response) => {
   const permittedAge = 18;
   if (age < permittedAge) {
-    return responseError(400, 'O crush deve ser maior de idade', response);
+    return responseError(BAD_REQUEST, 'O crush deve ser maior de idade', response);
   }
 };
 
 const dateIsRequired = (date, response) => {
   const dateNotExists = !date || !date.datedAt || (!date.rate && date.rate !== 0);
   if (dateNotExists) {
-    return responseError(400, 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios', response);
+    return responseError(BAD_REQUEST, 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios', response);
   }
 };
 
 const dateRateWithinRange = (rate, response) => {
   const rateOutRange = !Number.isInteger(rate) || rate < 1 || rate > 5;
   if (rateOutRange) {
-    return responseError(400, 'O campo "rate" deve ser um inteiro de 1 à 5', response);
+    return responseError(BAD_REQUEST, 'O campo "rate" deve ser um inteiro de 1 à 5', response);
   }
 };
 
@@ -130,7 +133,7 @@ const formatDatedAt = (datedAt, response) => {
   const regexDateFormat = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
   const incorrectFormatDate = !regexDateFormat.test(datedAt);
   if (incorrectFormatDate) {
-    return responseError(400, 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"', response);
+    return responseError(BAD_REQUEST, 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"', response);
   }
 };
 
@@ -155,18 +158,16 @@ app.get('/crush/:id', (request, response) => {
 app.post('/login', (request, response) => {
   const { email, password } = request.body;
   const token = buildToken();
-  // console.log(password.length);
 
   verifyEmail(email, response);
   verifyPassword(password, response);
 
-  response.status(200).json({ token });
+  response.status(SUCCESS).json({ token });
 });
 
 app.post('/crush', (request, response) => {
   const { authorization } = request.headers;
   const { name, age, date } = request.body;
-  console.log(authorization);
 
   tokenExists(authorization, response);
   validToken(authorization, response);
@@ -184,7 +185,40 @@ app.post('/crush', (request, response) => {
   const allCrushes = getArrayOfCrushes();
   const createdCrush = { id: allCrushes.length + 1, name, age, date };
   writeInFile(JSON.stringify([...allCrushes, createdCrush], 0, 2));
-  return response.status(201).send(createdCrush);
+  return response.status(CREATED).send(createdCrush);
+});
+
+app.put('/crush/:id', (request, response) => {
+  const { authorization } = request.headers;
+  const { name, age, date } = request.body;
+  const { id } = request.params;
+  const idNumber = Number(id);
+
+  tokenExists(authorization, response);
+  validToken(authorization, response);
+
+  nameExists(name, response);
+  nameLength(name, response);
+
+  ageExists(age, response);
+  allowedAge(age, response);
+
+  dateIsRequired(date, response);
+  dateRateWithinRange(date.rate, response);
+  formatDatedAt(date.datedAt, response);
+
+  const allCrushes = getArrayOfCrushes();
+  const updatedCrush = { id: idNumber, name, age, date };
+
+  const replaceCrush = (crush, index) => {
+    if (crush.id === idNumber) {
+      allCrushes.splice(index, 1, updatedCrush);
+    }
+  };
+
+  allCrushes.map(replaceCrush);
+  writeInFile(JSON.stringify([...allCrushes], 0, 2));
+  return response.status(SUCCESS).send(updatedCrush);
 });
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
