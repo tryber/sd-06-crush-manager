e996877ccb8fac2bcb522757101fbbe119caae0e
flaviannymaciel <flaviannymaciel@gmail.com>
2021-02-24 12:11:03 -0300

    req5

--- index.js
+++ index.js
@@ -139,6 +139,59 @@ app.post('/crush', async (req, res) => {
   return res.status(201).send(newCrush);
 });
 
+app.put('/crush/:id', async (req, res) => {
+  const token = req.headers.authorization;
+  const { name, age, date } = req.body;
+  const { id } = req.params;
+  const idInt = parseInt(id, 10);
+
+  // validações
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  }
+
+  if (!date || !date.datedAt || (!date.rate && date.rate !== 0)) {
+    return res.status(400).send({
+      message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios',
+    });
+  }
+  if (!Number.isInteger(date.rate) || date.rate < 1 || date.rate > 5) {
+    return res.status(400).send({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  const rgxDate = /^(?:(?:31(\/)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+  if (!rgxDate.test(date.datedAt)) {
+    return res.status(400).send({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+
+  const crushList = await read();
+  const newCrush = { id: idInt, name, age, date };
+  crushList.splice(idInt, 1, newCrush);
+  await write(JSON.stringify([...crushList], 0, 2));
+  return res.status(200).json(newCrush);
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Executando na ${port}`);
