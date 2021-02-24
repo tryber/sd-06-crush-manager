905ef55b6bb397a949229fc1493bd3da51010aa1
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-02-24 13:29:16 -0300

    req3

--- index.js
+++ index.js
@@ -65,30 +65,67 @@ app.post('/login', async (request, response) => {
   const { email, password } = request.body;
 
   if (confereEmail(email) === false) {
-    response.status(400).json({
+    return response.status(400).json({
       message: 'O "email" deve ter o formato "email@email.com"',
     });
   }
   if (confereEmail(email) === null) {
-    response.status(400).json({
+    return response.status(400).json({
       message: 'O campo "email" é obrigatório',
     });
   }
   if (confereSenha(password) === null) {
-    response.status(400).json({
+    return response.status(400).json({
       message: 'O campo "password" é obrigatório',
     });
   }
   if (confereSenha(password) === false) {
-    response.status(400).json({
+    return response.status(400).json({
       message: 'A "senha" deve ter pelo menos 6 caracteres',
     });
   }
   return response.status(200).json({ token: geradordeToken(16) });
 });
 
-// app.post('/crush', async (request, response) => {
-
-// });
+app.post('/crush', async (request, response) => {
+  const { name, age, date } = request.body;
+  const { authorization } = request.headers;
+  if (!authorization) {
+    return response.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+  if (authorization.length !== 16) {
+    return response.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+  if (name.length === 0) {
+    return response.status(400).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+  if (name.length < 3) {
+    return response.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+  if (age.length === 0) {
+    return response.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+  if (age < 18) {
+    return response.status(400).json({
+      message: 'O crush deve ser maior de idade',
+    });
+  }
+  if (date.datedAt) {
+    return response.status(400).json({
+      message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  return response.status(200).json(request.body);
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
