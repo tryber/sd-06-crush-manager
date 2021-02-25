97d15567591a5812d1f3668c57b56f9706a3dd4d
Matheus Dias <matheuscara2000@gmail.com>
2021-02-24 21:58:16 -0300

    Lint

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.delete('/crush/:id', async (req, res) => {
 
   if (!authorization) return res.status(401).json({ message: 'Token não encontrado' });
   if (authorization.length !== 16) return res.status(401).json({ message: 'Token inválido' });
-  
+
   const data = JSON.parse(await fs.readFile('./crush.json'));
   const { id } = req.params;
 
