5c1f4fe475f2b726d1cb502f5e0217f4f567bc04
Igor-leao <igorleao.eng@gmail.com>
2021-02-28 09:30:02 -0300

    requisitos 4 e 5 revisando

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const express = require('express');
-const fs = require('fs').promises;
+const bodyParser = require('body-parser');
+
 const app = express();
 const SUCCESS = 200;
-const port = 3000;
-const crushArray = 'crush.json';
+const PORT = 3000;
+
+// Routers
+const crush = require('./routers/crush');
+const login = require('./routers/login');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
-// Requisito -1 obs: '_' p/ quando um parãmetro não é usado
-// res com todos os crushs cadastrados ou um array vazio e o status 200.
-app.get('/crush', async (_req, res) => {
-  const crushes = await fs.readFile(crushArray);
-  if (!crushes) {
-    return res.status(200).json([]);
-  }
-  return res.status(200).json(JSON.parse(crushes));
+
+app.use(bodyParser.json());
+
+app.use('/login', login);
+app.use('/crush', crush);
+
+app.use((err, req, res, _next) => {
+  console.log(`${req.method} ${req.url} ${res.statusCode}, error: ${err}`);
+  return res.json({ message: err.toString() });
 });
 
-app.listen(3000, () => console.log(`Using port: ${port}`));
-
-// Requisito-2 obs: id da url vem formato string, por isso o Number(id)
-// res com o crush doo id da rota ou { "message": "Crush não encontrado" } e o status 404
-app.get('/crush/:id', async (req, res) => {
-  const crushes = await fs.readFile(crushArray);
-  const crushesJson = JSON.parse(crushes);
-  const { id } = req.params;
-  const crush = crushesJson.find((crushJson) => crushJson.id === Number(id));
-  if (!crush) {
-    return res.status(404).json({ message: 'Crush não encontrado' });
-  }
-  return res.status(200).json(crush);
-});
+app.listen(PORT, () => console.log(`Crush Manager server listening on port ${PORT}`));
