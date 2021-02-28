6f3e462a8b2defc243ef7eb847c9d88ce17347ab
Igor-leao <igorleao.eng@gmail.com>
2021-02-28 10:31:45 -0300

    resolvendo middleware

--- index.js
+++ index.js
@@ -1,18 +1,81 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+const middleware = require('./middleware');
+const services = require('./services');
 
 const app = express();
 const SUCCESS = 200;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
 
-app.use(bodyParser.json());
+app.post('/login', middleware.login, services.login);
 
-app.use(routes);
+app.get('/crush/search', async (request, response) => {
+  if (!request.headers.authorization) {
+    return response.status(401).send({ message: 'Token não encontrado' });
+  }
+  if (request.headers.authorization && request.headers.authorization.length !== 16) {
+    return response.status(401).send({ message: 'Token inválido' });
+  }
 
-app.listen(3000, () => console.log('Running'));
+  if (!request.query.q) {
+    return response.status(401).send([]);
+  }
 
-app.use((error, _request, response, _next) => response.status(error.status || 500).send(`Error, error message: ${error.message}`));
+  const name = request.query.q;
+  fs.readFile('./crush.json', 'utf8', (err, data) => {
+    if (err) throw new Error('Error');
+
+    const crushs = JSON.parse(data);
+
+    if (name.length === 0) {
+      return response.status(401).send(crushs);
+    }
+
+    const filteredCrushs = crushs.filter((crush) => crush.name.includes(name));
+    return response.status(SUCCESS).json(filteredCrushs);
+  });
+});
+
+app.get('/crush/:id', async (request, response) => {
+  const id = request.params.id - 1;
+
+  await fs.readFile('./crush.json', (err, data) => {
+    if (err) throw new Error('Error');
+
+    if (!JSON.parse(data)[id]) {
+      return response.status(404).send({ message: 'Crush não encontrado' });
+    }
+
+    return response.status(SUCCESS).send(JSON.parse(data)[id]);
+  });
+});
+
+app.get('/crush', (_request, response) => {
+  fs.readFile('./crush.json', (err, data) => {
+    if (err) throw new Error('Error');
+    return response.status(SUCCESS).send(JSON.parse(data));
+  });
+});
+
+app.post('/crush', middleware.crush, services.createCrush);
+
+app.put('/crush/:id', middleware.crush, services.editCrush);
+
+app.delete('/crush/:id', (request, response, next) => {
+  if (!request.headers.authorization) {
+    return response.status(401).send({ message: 'Token não encontrado' });
+  }
+  if (request.headers.authorization && request.headers.authorization.length !== 16) {
+    return response.status(401).send({ message: 'Token inválido' });
+  }
+  next();
+}, services.deleteCrush);
+
+app.listen(3000, () => console.log('poooorta 3000 ta on'));
---
+++ services/index.js
@@ -0,0 +1,11 @@
+const login = require('./login');
+const createCrush = require('./createCrush');
+const editCrush = require('./editCrush');
+const deleteCrush = require('./deleteCrush');
+
+module.exports = {
+  login,
+  createCrush,
+  editCrush,
+  deleteCrush,
+};
