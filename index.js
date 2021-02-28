85eb9672c1971bf35a159c9b9a998dbecd3ccc7d
Igor-leao <igorleao.eng@gmail.com>
2021-02-28 09:24:28 -0300

    requisito 2

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const fs = require('fs').promises;
-
 const app = express();
 const SUCCESS = 200;
 const port = 3000;
 const crushArray = 'crush.json';
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(SUCCESS).send();
 });
-
 // Requisito -1 obs: '_' p/ quando um parãmetro não é usado
 // res com todos os crushs cadastrados ou um array vazio e o status 200.
 app.get('/crush', async (_req, res) => {
@@ -22,3 +19,16 @@ app.get('/crush', async (_req, res) => {
 });
 
 app.listen(3000, () => console.log(`Using port: ${port}`));
+
+// Requisito-2 obs: id da url vem formato string, por isso o Number(id)
+// res com o crush doo id da rota ou { "message": "Crush não encontrado" } e o status 404
+app.get('/crush/:id', async (req, res) => {
+  const crushes = await fs.readFile(crushArray);
+  const crushesJson = JSON.parse(crushes);
+  const { id } = req.params;
+  const crush = crushesJson.find((crushJson) => crushJson.id === Number(id));
+  if (!crush) {
+    return res.status(404).json({ message: 'Crush não encontrado' });
+  }
+  return res.status(200).json(crush);
+});
