ab6966424cb1abd54164a89a44f1f8d921db78f6
Isabela Rosa <isabelarosa954@gmail.com>
2021-02-28 22:12:51 -0300

    requisito 3: rota '/login' - teste 2: mudando texto de mensagem do json retornado

--- login.js
+++ login.js
@@ -21,7 +21,7 @@ router.post('/', (req, res) => {
   const regexValidateEmail = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!regexValidateEmail.test(email)) {
     return res.status(400).json({
-      message: 'O "email" deve ter o formato \'email@email.com\'',
+      message: 'O "email" deve ter o formato "email@email.com"',
     });
   }
   if (!password) {
