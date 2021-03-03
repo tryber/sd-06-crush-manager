5aa00355add541e77a0ba39b8a10c3b06c3e83ef
Isabela Rosa <isabelarosa954@gmail.com>
2021-03-03 00:43:38 -0300

    requisito 4: nenhum teste está passando no momento, embora esteja tudo funcionando

--- routerCrush.js
+++ routerCrush.js
@@ -55,20 +55,22 @@ router.post('/', validateToken, async (req, res) => {
 
   const regexData = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
 
-  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.length < 3) return res.status(400).json({ message: 'O "nome" deve ter pelo menos 3 caracteres' });
+  if (!name) { return res.status(400).json({ message: 'O campo "name" é obrigatório' }); }
+  if (name.length < 3) { return res.status(400).json({ message: 'O "nome" deve ter pelo menos 3 caracteres' }); }
 
   if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  if (+age < 18) return res.status(400).json({ message: 'O crush deve ser maior de idade' });
+  if (+age < 18) { return res.status(400).json({ message: 'O crush deve ser maior de idade' }); }
 
-  if (!date || !date.datedAt || !date.rate) return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' });
+  if (!date || !date.datedAt || !date.rate) { return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' }); }
 
-  if (!regexData.test(date.datedAt)) return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' });
+  if (!regexData.test(date.datedAt)) { return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' }); }
 
-  if (date.rate < 1 || date.rate > 5) return res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro de 1 à 5' });
+  if (date.rate < 1 || date.rate > 5) { return res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro de 1 à 5' }); }
 
   addNewCrush(newCrush);
   return res.status(201).json(newCrush);
 });
 
 module.exports = router;
+
+// comentário teste
