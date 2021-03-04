5c060a3c9d40169638d186a831a9af37fb190681
Isabela Rosa <isabelarosa954@gmail.com>
2021-03-04 12:09:23 -0300

    construindo middlewares separados para as validações de nome, idade e data

--- routerCrush.js
+++ routerCrush.js
@@ -5,6 +5,7 @@ const fs = require('fs').promises;
 const path = require('path');
 const bodyParser = require('body-parser');
 const validateToken = require('./middlewareValidateToken');
+const { validateName, validateAge, validateDate } = require('./validationMiddleware');
 
 router.use(bodyParser.json());
 
@@ -47,30 +48,14 @@ const addNewCrush = async (crush) => {
   await WriteCrushJson(newCrushJson);
 };
 
-router.post('/', validateToken, async (req, res) => {
+router.post('/', validateToken, validateName, validateAge, validateDate, async (req, res) => {
   const { name, age, date } = req.body;
   const arrayCrush = await readCrushJson();
   const id = arrayCrush.length + 1;
   const newCrush = { id, name, age, date };
-
-  const regexData = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
-
-  if (!name) { return res.status(400).json({ message: 'O campo "name" é obrigatório' }); }
-  if (name.length < 3) { return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); }
-
-  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  if (+age < 18) { return res.status(400).json({ message: 'O crush deve ser maior de idade' }); }
-
-  if (!date || !date.datedAt || !date.rate) { return res.status(400).json({ message: 'O campo "date" é obrigatório e "datedAt" e "rate" não podem ser vazios' }); }
-
-  if (!regexData.test(date.datedAt)) { return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' }); }
-
-  if (date.rate < 1 || date.rate > 5) { return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' }); }
 
   addNewCrush(newCrush);
   return res.status(201).json(newCrush);
 });
 
 module.exports = router;
-
-// comentário teste
