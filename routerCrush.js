5dc9e3304c851c18ee460159348a3505e4a39545
Isabela Rosa <isabelarosa954@gmail.com>
2021-03-04 10:17:19 -0300

    requisito 4: finalmente os teste locais passaram, graças ao Gargani! Gratidão

--- routerCrush.js
+++ routerCrush.js
@@ -56,7 +56,7 @@ router.post('/', validateToken, async (req, res) => {
   const regexData = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
 
   if (!name) { return res.status(400).json({ message: 'O campo "name" é obrigatório' }); }
-  if (name.length < 3) { return res.status(400).json({ message: 'O "nome" deve ter pelo menos 3 caracteres' }); }
+  if (name.length < 3) { return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); }
 
   if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   if (+age < 18) { return res.status(400).json({ message: 'O crush deve ser maior de idade' }); }
@@ -65,7 +65,7 @@ router.post('/', validateToken, async (req, res) => {
 
   if (!regexData.test(date.datedAt)) { return res.status(400).json({ message: 'O campo "datedAt" deve ter o formato "dd/mm/aaaa"' }); }
 
-  if (date.rate < 1 || date.rate > 5) { return res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro de 1 à 5' }); }
+  if (date.rate < 1 || date.rate > 5) { return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' }); }
 
   addNewCrush(newCrush);
   return res.status(201).json(newCrush);
